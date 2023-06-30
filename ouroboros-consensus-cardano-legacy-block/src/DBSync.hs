d3653deec5cfe1f21322c84845eae6ba9d2bc135
Javier Sagredo <javier.sagredo@iohk.io>
2023-06-30 11:12:30 +0200

    Add conversion functions for more Legacy types

--- ouroboros-consensus-cardano-legacy-block/src/DBSync.hs
+++ ouroboros-consensus-cardano-legacy-block/src/DBSync.hs
@@ -1,15 +1,17 @@
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE PolyKinds             #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeApplications      #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE DataKinds                #-}
+{-# LANGUAGE FlexibleContexts         #-}
+{-# LANGUAGE FlexibleInstances        #-}
+{-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE LambdaCase               #-}
+{-# LANGUAGE MultiParamTypeClasses    #-}
+{-# LANGUAGE NamedFieldPuns           #-}
+{-# LANGUAGE PolyKinds                #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications         #-}
+{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE TypeOperators            #-}
+{-# LANGUAGE UndecidableInstances     #-}
 
 module DBSync (
     initLedger
@@ -17,23 +19,33 @@ module DBSync (
     -- * Conversion
   , Convert (..)
   , Convert'
+  , convertBlock
   , convertExtLedgerState
   , convertExtLedgerState'
+  , convertLedgerConfig
+  , convertLedgerError
+  , convertLedgerResult
   , convertLedgerState
   , convertLedgerState'
   ) where
 
-import           Cardano.Ledger.Crypto
 import           Data.Coerce
+import           Data.Kind
+import           Data.SOP.Counting
 import           Data.SOP.Functors
 import           Data.SOP.Strict
+import           Legacy.Byron.Ledger ()
 import           Legacy.Cardano.Block
 import           Legacy.Cardano.CanHardFork
 import           Legacy.LegacyBlock
+import           Legacy.Shelley.Ledger ()
 import           Ouroboros.Consensus.Block.Abstract
-import           Ouroboros.Consensus.Cardano
+import           Ouroboros.Consensus.Cardano.Block
 import           Ouroboros.Consensus.HardFork.Combinator.AcrossEras
 import           Ouroboros.Consensus.HardFork.Combinator.Basics
+import           Ouroboros.Consensus.HardFork.Combinator.Ledger
+import           Ouroboros.Consensus.HardFork.Combinator.PartialConfig
+import           Ouroboros.Consensus.HardFork.History.Summary
 import           Ouroboros.Consensus.HeaderValidation
 import           Ouroboros.Consensus.Ledger.Abstract
 import           Ouroboros.Consensus.Ledger.Extended
@@ -41,6 +53,7 @@ import           Ouroboros.Consensus.Ledger.Tables.Utils
 import           Ouroboros.Consensus.Node.ProtocolInfo
 import           Ouroboros.Consensus.Protocol.Abstract
 import           Ouroboros.Consensus.TypeFamilyWrappers
+import           Unsafe.Coerce
 
 initLedger ::
      ProtocolInfo IO (LegacyCardanoBlock StandardCrypto)
@@ -65,6 +78,19 @@ reapplyBlock cfg block lsb =
 {-------------------------------------------------------------------------------
   Conversion
 -------------------------------------------------------------------------------}
+
+convertBlock ::
+     CardanoBlock StandardCrypto
+     -> LegacyCardanoBlock StandardCrypto
+convertBlock = LegacyBlock
+             . HardForkBlock
+             . OneEraBlock
+             . htrans (Proxy @Convert) transOne
+             . getOneEraBlock
+             . getHardForkBlock
+    where
+      transOne :: I x -> I (LegacyBlock x)
+      transOne = I . LegacyBlock . unI
 
 convertLedgerState ::
      LedgerState (CardanoBlock StandardCrypto) EmptyMK
@@ -239,3 +265,101 @@ instance Convert x (LegacyBlock x) where
   mkLegacyLedgerState = LegacyLedgerState
   unmkLegacyBlock = getLegacyBlock
   unmkLegacyLedgerState = getLegacyLedgerState
+
+convertLedgerConfig ::
+     LedgerConfig (CardanoBlock StandardCrypto)
+  -> LedgerConfig (LegacyCardanoBlock StandardCrypto)
+convertLedgerConfig cfg =
+  HardForkLedgerConfig {
+      hardForkLedgerConfigShape =
+        Shape
+      . Exactly
+      . htrans (Proxy @Top2) (K . unK)
+      . getExactly
+      . getShape
+      $ hardForkLedgerConfigShape
+    , hardForkLedgerConfigPerEra =
+      PerEraLedgerConfig
+      . htrans (Proxy @Convert) (
+          WrapPartialLedgerConfig
+        . unwrapPartialLedgerConfig
+          )
+      . getPerEraLedgerConfig
+      $ hardForkLedgerConfigPerEra
+  }
+  where
+    HardForkLedgerConfig {
+        hardForkLedgerConfigShape
+      , hardForkLedgerConfigPerEra
+      } = cfg
+
+
+class (Top x, Top y) => Top2 x y
+instance (Top x, Top y) => Top2 x y
+
+class ( Convert' x y
+      , LedgerErr (LedgerState (LegacyBlock y)) ~ LedgerErr (LedgerState y)
+      , AuxLedgerEvent (LedgerState (LegacyBlock y)) ~ AuxLedgerEvent (LedgerState y)
+      ) => Convert'' x y
+instance ( Convert' x y
+         , LedgerErr (LedgerState (LegacyBlock y)) ~ LedgerErr (LedgerState y)
+         , AuxLedgerEvent (LedgerState (LegacyBlock y)) ~ AuxLedgerEvent (LedgerState y)
+         ) => Convert'' x y
+
+convertLedgerError ::
+     LedgerErr (LedgerState (LegacyCardanoBlock StandardCrypto))
+  -> LedgerErr (LedgerState (CardanoBlock StandardCrypto))
+convertLedgerError (HardForkLedgerErrorFromEra oe) =
+    HardForkLedgerErrorFromEra
+  . OneEraLedgerError
+  . htrans (Proxy @Convert'') coerce
+  . getOneEraLedgerError
+  $ oe
+
+convertLedgerError (HardForkLedgerErrorWrongEra we) =
+    HardForkLedgerErrorWrongEra
+  . MismatchEraInfo
+  . unsafeCoerce
+    -- This should not be problematic because the LedgerEraInfo and
+    -- SingleEraInfo are just wrappers over a Text, so everything should be
+    -- safely coerced, but SOP complains about not being able to resolve the Top
+    -- constraint for every type.
+  . getMismatchEraInfo
+  $ we
+
+  -- where
+  --   convertMismatch :: forall xxs x xs . (xxs ~ x ': xs) =>
+  --        Mismatch SingleEraInfo LedgerEraInfo (Map LegacyBlock xxs)
+  --     -> Mismatch SingleEraInfo LedgerEraInfo xxs
+  --   convertMismatch (ML (SingleEraInfo sei) nslei) =
+  --                    ML (SingleEraInfo sei)
+  --                       (htrans (Proxy @Top2) coerce nslei)
+  --   convertMismatch (MR nssei (LedgerEraInfo (SingleEraInfo lei))) =
+  --                    MR (htrans (Proxy @Top2) coerce nssei)
+  --                       (LedgerEraInfo (SingleEraInfo lei))
+  --   convertMismatch (MS n) = MS $ convertMismatch n
+
+type Map :: (Type -> Type) -> [Type] -> [Type]
+type family Map f xs where
+  Map f '[] = '[]
+  Map f (x ': xs) = f x ': Map f xs
+
+convertLedgerResult ::
+     LedgerResult
+      (LedgerState (LegacyCardanoBlock StandardCrypto))
+      (LedgerState (LegacyCardanoBlock StandardCrypto) DiffMK)
+  -> LedgerResult
+      (LedgerState (CardanoBlock StandardCrypto))
+      (LedgerState (CardanoBlock StandardCrypto) EmptyMK)
+convertLedgerResult le =
+     LedgerResult {
+      lrEvents = map ( OneEraLedgerEvent
+                     . htrans (Proxy @Convert'') coerce
+                     . getOneEraLedgerEvent
+                     ) lrEvents
+    , lrResult = convertLedgerState' $ convertMapKind lrResult
+    }
+ where
+   LedgerResult { lrEvents
+                , lrResult
+                } = le
