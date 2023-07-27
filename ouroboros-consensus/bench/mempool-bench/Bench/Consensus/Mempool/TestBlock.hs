ae89e366f0e104e0b0619663ae4dbee2d3dba8ce
Javier Sagredo <javier.sagredo@iohk.io>
2023-07-27 14:31:20 +0200

    BackingStore bench and adapt Mempool bench

--- ouroboros-consensus/bench/mempool-bench/Bench/Consensus/Mempool/TestBlock.hs
+++ ouroboros-consensus/bench/mempool-bench/Bench/Consensus/Mempool/TestBlock.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns             #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE TypeFamilies               #-}
 
@@ -11,38 +12,45 @@
 module Bench.Consensus.Mempool.TestBlock (
     -- * Test block
     TestBlock
-    -- * Initial parameters
-  , initialLedgerState
-  , sampleLedgerConfig
-  , sampleMempoolAndModelParams
+    -- * Payload semantics
+  , Ledger.GenTx (TestBlockGenTx, unGenTx)
+  , PayloadDependentState (TestPLDS)
     -- * Transactions
   , Token (Token)
-  , Tx (Tx)
+  , Tx (Tx, consumed, produced)
+  , mkSimpleGenesisTx
+  , mkSimpleTx
   , mkTx
   , txSize
   ) where
 
-import           Bench.Consensus.MempoolWithMockedLedgerItf
-import qualified Cardano.Slotting.Time as Time
-import           Codec.Serialise (Serialise)
+import           Cardano.Binary (FromCBOR (..), ToCBOR (..))
+import           Codec.Serialise (Serialise (..))
 import           Control.DeepSeq (NFData)
 import           Control.Monad.Trans.Except (except)
-import           Data.Set (Set, (\\))
+import           Data.Map.Diff.Strict (Diff)
+import qualified Data.Map.Diff.Strict as Diff
+import qualified Data.Map.Strict as Map
+import           Data.Set (Set)
 import qualified Data.Set as Set
 import           Data.TreeDiff (ToExpr)
-import           Data.Word (Word8)
 import           GHC.Generics (Generic)
 import           NoThunks.Class (NoThunks)
 import qualified Ouroboros.Consensus.Block as Block
-import           Ouroboros.Consensus.Config.SecurityParam as Consensus
-import qualified Ouroboros.Consensus.HardFork.History as HardFork
 import qualified Ouroboros.Consensus.Ledger.Basics as Ledger
 import qualified Ouroboros.Consensus.Ledger.SupportsMempool as Ledger
+import           Ouroboros.Consensus.Ledger.Tables (CanSerializeLedgerTables,
+                     CanStowLedgerTables, DiffMK (..), EmptyMK, HasLedgerTables,
+                     IsMapKind (..), Key, KeysMK (..), LedgerTables (..), Value,
+                     ValuesMK (..))
+import qualified Ouroboros.Consensus.Ledger.Tables.Utils as Ledger
+                     (rawAttachAndApplyDiffs)
 import qualified Ouroboros.Consensus.Mempool as Mempool
 import           Test.Util.TestBlock (LedgerState (TestLedger),
-                     PayloadSemantics (PayloadDependentError, PayloadDependentState, applyPayload),
-                     TestBlockWith, applyDirectlyToPayloadDependentState,
-                     lastAppliedPoint, payloadDependentState)
+                     PayloadSemantics (PayloadDependentError, PayloadDependentState, applyPayload, getPayloadKeySets),
+                     TestBlockWith, Ticked1 (TickedTestLedger),
+                     applyDirectlyToPayloadDependentState,
+                     payloadDependentState)
 
 {-------------------------------------------------------------------------------
   MempoolTestBlock
@@ -57,59 +65,79 @@ data Tx = Tx {
   deriving stock (Eq, Ord, Generic, Show)
   deriving anyclass (NoThunks, NFData)
 
-newtype Token = Token { unToken :: Word8  }
+newtype Token = Token { unToken :: Int  }
   deriving stock (Show, Eq, Ord, Generic)
+  deriving newtype (ToCBOR, FromCBOR, Num, Enum)
   deriving anyclass (NoThunks, ToExpr, Serialise, NFData)
 
-{-------------------------------------------------------------------------------
-  Initial parameters
--------------------------------------------------------------------------------}
+mkTx ::
+     [Token]
+     -- ^ Consumed
+  -> [Token]
+     -- ^ Produced
+  -> Tx
+mkTx cons prod = Tx {
+    consumed = Set.fromList cons
+  , produced = Set.fromList prod
+  }
 
-initialLedgerState :: LedgerState (TestBlockWith Tx)
-initialLedgerState = TestLedger {
-      lastAppliedPoint      = Block.GenesisPoint
-    , payloadDependentState = TestLedgerState {
-          availableTokens = Set.empty :: Set Token
-        }
-    }
+-- | Create a 'Tx' that consumes and produces exactly one 'Token'.
+mkSimpleTx :: Token -> Token -> Tx
+mkSimpleTx x y = mkTx [x] [y]
 
-sampleLedgerConfig :: Ledger.LedgerConfig TestBlock
-sampleLedgerConfig =
-  HardFork.defaultEraParams (Consensus.SecurityParam 10) (Time.slotLengthFromSec 2)
-
-sampleMempoolAndModelParams :: InitialMempoolAndModelParams TestBlock
-sampleMempoolAndModelParams = MempoolAndModelParams {
-      immpInitialState = initialLedgerState
-    , immpLedgerConfig = sampleLedgerConfig
-    }
+-- | Create a 'Tx' that consumes nothing, and produces only the given 'Token'.
+mkSimpleGenesisTx :: Token -> Tx
+mkSimpleGenesisTx y = mkTx [] [y]
 
 {-------------------------------------------------------------------------------
   Payload semantics
 -------------------------------------------------------------------------------}
 
-data TestLedgerState = TestLedgerState {
-    availableTokens :: Set Token
-  }
-  deriving stock (Generic, Eq, Show)
-  deriving anyclass (NoThunks, ToExpr, Serialise)
-
-data TxApplicationError =
+newtype TxApplicationError =
     -- | The transaction could not be applied due to the given unavailable tokens.
     TxApplicationError { unavailable :: Set Token }
   deriving stock (Generic, Eq, Show)
   deriving anyclass (NoThunks, ToExpr, Serialise)
 
 instance PayloadSemantics Tx where
-  type PayloadDependentState Tx = TestLedgerState
+  newtype instance PayloadDependentState Tx mk = TestPLDS {
+      getTestPLDS :: mk Token ()
+    }
+    deriving stock Generic
 
   type PayloadDependentError Tx = TxApplicationError
 
-  applyPayload st@TestLedgerState { availableTokens } Tx { consumed, produced } =
-    let
-      notFound = Set.filter (not . (`Set.member` availableTokens)) consumed
-    in if Set.null notFound
-       then Right $ st{ availableTokens = availableTokens \\ consumed <> produced }
-       else Left  $ TxApplicationError notFound
+  applyPayload plds tx =
+      let
+        notFound = Set.filter (not . (`Map.member` tokMap)) consumed
+      in if Set.null notFound
+        then Right $ TestPLDS (Ledger.rawAttachAndApplyDiffs fullDiff toks)
+        else Left  $ TxApplicationError notFound
+    where
+      TestPLDS toks@(ValuesMK tokMap) = plds
+      Tx {consumed, produced}         = tx
+
+      consumedDiff, producedDiff :: Diff Token ()
+      consumedDiff = Diff.fromListDeletes [(t, ()) | t <- Set.toList consumed]
+      producedDiff = Diff.fromListInserts [(t, ()) | t <- Set.toList produced]
+
+      fullDiff :: DiffMK Token ()
+      fullDiff = DiffMK $ consumedDiff <> producedDiff
+
+  getPayloadKeySets tx = LedgerTables $ KeysMK $ consumed <> produced
+    where
+      Tx {consumed, produced} = tx
+
+deriving stock instance (IsMapKind mk)
+                        => Eq (PayloadDependentState Tx mk)
+deriving stock instance (IsMapKind mk)
+                        => Show (PayloadDependentState Tx mk)
+deriving anyclass instance (IsMapKind mk)
+                        => NoThunks (PayloadDependentState Tx mk)
+
+instance Serialise (PayloadDependentState Tx EmptyMK) where
+  encode = error "unused: encode"
+  decode = error "unused: decode"
 
 -- | TODO: for the time being 'TestBlock' does not have any codec config
 data instance Block.CodecConfig TestBlock = TestBlockCodecConfig
@@ -118,6 +146,36 @@ data instance Block.CodecConfig TestBlock = TestBlockCodecConfig
 -- | TODO: for the time being 'TestBlock' does not have any storage config
 data instance Block.StorageConfig TestBlock = TestBlockStorageConfig
   deriving (Show, Generic, NoThunks)
+
+{-------------------------------------------------------------------------------
+  Ledger tables
+-------------------------------------------------------------------------------}
+
+type instance Key   (LedgerState TestBlock) = Token
+type instance Value (LedgerState TestBlock) = ()
+
+instance HasLedgerTables (LedgerState TestBlock) where
+  projectLedgerTables st =
+    LedgerTables $ getTestPLDS $ payloadDependentState st
+  withLedgerTables st table = st {
+        payloadDependentState = plds {
+            getTestPLDS = Ledger.getLedgerTables table
+          }
+      }
+    where
+      TestLedger { payloadDependentState = plds } = st
+
+instance HasLedgerTables (Ticked1 (LedgerState TestBlock)) where
+  projectLedgerTables (TickedTestLedger st) = Ledger.castLedgerTables $
+    Ledger.projectLedgerTables st
+  withLedgerTables (TickedTestLedger st) tables =
+    TickedTestLedger $ Ledger.withLedgerTables st $ Ledger.castLedgerTables tables
+
+instance CanSerializeLedgerTables (LedgerState TestBlock)
+
+instance CanStowLedgerTables (LedgerState TestBlock) where
+  stowLedgerTables     = error "unused: stowLedgerTables"
+  unstowLedgerTables   = error "unused: unstowLedgerTables"
 
 {-------------------------------------------------------------------------------
   Mempool support
@@ -131,16 +189,6 @@ newtype instance Ledger.GenTx TestBlock = TestBlockGenTx { unGenTx :: Tx }
 -- the actual size of the transaction in bytes.
 txSize :: Ledger.GenTx TestBlock -> Mempool.TxSizeInBytes
 txSize (TestBlockGenTx tx) = fromIntegral $ 1 + length (consumed tx) + length (produced tx)
-
-mkTx ::
-     [Token]
-     -- ^ Consumed
-  -> [Token]
-     -- ^ Produced
-  -> Ledger.GenTx TestBlock
-mkTx cons prod = TestBlockGenTx $ Tx { consumed = Set.fromList cons
-                                     , produced = Set.fromList prod
-                                     }
 
 instance Ledger.LedgerSupportsMempool TestBlock where
   applyTx _cfg _shouldIntervene _slot (TestBlockGenTx tx) tickedSt =
@@ -158,6 +206,9 @@ instance Ledger.LedgerSupportsMempool TestBlock where
   txInBlockSize = txSize
 
   txForgetValidated (ValidatedGenTx tx) = tx
+
+  getTransactionKeySets (TestBlockGenTx tx) = LedgerTables $
+    KeysMK $ consumed tx
 
 newtype instance Ledger.TxId (Ledger.GenTx TestBlock) = TestBlockTxId Tx
   deriving stock (Generic)
