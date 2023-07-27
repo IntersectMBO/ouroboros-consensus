9843f8ff5a3663ccc64c5bca264d3cd3b5c5955e
Javier Sagredo <javier.sagredo@iohk.io>
2023-07-27 14:41:09 +0200

    Implement new compat package o-c-card-legacy-block
    Adds a compatibility wrapper that downstream clients can use to rule out
    the UTxO-HD implementation.

---
+++ ouroboros-consensus-cardano-legacy-block/src/Legacy/Cardano/Block.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds                #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeOperators            #-}
+
+module Legacy.Cardano.Block (
+    LegacyCardanoBlock
+  , LegacyCardanoEras
+  , LegacyCardanoShelleyEras
+  ) where
+
+import           Data.Kind
+import           Legacy.LegacyBlock
+import           Ouroboros.Consensus.Byron.Ledger.Block
+import           Ouroboros.Consensus.Cardano.Block
+import           Ouroboros.Consensus.Protocol.Praos
+import           Ouroboros.Consensus.Protocol.TPraos
+import           Ouroboros.Consensus.Shelley.Ledger
+
+type LegacyCardanoEras :: Type -> [Type]
+type LegacyCardanoEras c =  LegacyBlock ByronBlock
+                         ': LegacyCardanoShelleyEras c
+
+type LegacyCardanoShelleyEras :: Type -> [Type]
+type LegacyCardanoShelleyEras c =
+  '[ LegacyBlock (ShelleyBlock (TPraos c) (ShelleyEra c))
+   , LegacyBlock (ShelleyBlock (TPraos c) (AllegraEra c))
+   , LegacyBlock (ShelleyBlock (TPraos c) (MaryEra c))
+   , LegacyBlock (ShelleyBlock (TPraos c) (AlonzoEra c))
+   , LegacyBlock (ShelleyBlock (Praos c)  (BabbageEra c))
+   , LegacyBlock (ShelleyBlock (Praos c)  (ConwayEra c))
+   ]
+
+type LegacyCardanoBlock c = LegacyBlock (HardForkBlock (LegacyCardanoEras c))
