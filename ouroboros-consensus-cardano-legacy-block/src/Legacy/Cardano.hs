39083b1694875555f8aa36304f63512f5d2d21e6
Javier Sagredo <javier.sagredo@iohk.io>
2023-07-27 14:41:09 +0200

    Implement new compat package o-c-card-legacy-block
    Adds a compatibility wrapper that downstream clients can use to rule out
    the UTxO-HD implementation.

---
+++ ouroboros-consensus-cardano-legacy-block/src/Legacy/Cardano.hs
@@ -0,0 +1,12 @@
+module Legacy.Cardano (
+    LegacyCardanoBlock
+  , LegacyCardanoEras
+  , LegacyCardanoHardForkConstraints
+  , LegacyCardanoShelleyEras
+  ) where
+
+import           Legacy.Byron.Ledger ()
+import           Legacy.Cardano.Block
+import           Legacy.Cardano.CanHardFork
+import           Legacy.Cardano.Ledger ()
+import           Legacy.Shelley.Ledger ()
