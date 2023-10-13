41920cb8e3fc8f14b4be98911525e2bc54c5509a
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-10-13 11:04:51 +0000

    Remove the example for classifiers

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Setup/Classifiers.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Setup/Classifiers.hs
@@ -17,35 +17,18 @@ import           Test.Consensus.Network.AnchoredFragment.Extras (slotLength)
 import           Test.Consensus.PointSchedule
 import           Test.Util.Orphans.IOLike ()
 
-{- | Interesting categories to classify test inputs
-
-Knowing if there is a Genesis window after the intersection is important because
-otherwise the Genesis node has no chance to advance the immutable tip past
-the Limit on Eagerness.
-
-It is possible that, at the end of a test, the selection might be dishonest.
-Consider the following example, where k = 5 and scg = 12:
-
-    slots:  0  1  2  3  4  5  6  7  8  9  10  11  12
-    trunk:  0──1──2──3──4──5──6──7──8──────9──10──11
-                     ╰──4──5──6──7─────────8───9──10
-
-Because of the Limit on Eagerness, the selection is stuck at slot 8 (k-th
-block after the intersection).
-Since the test chains only have 7 and 8 blocks after the intersection, the Genesis node
-will not be able to make a final decision, since it needs to have enough information
-to determine that one of the fragments is definitely denser within the 12 slots after
-the intersection (at slot 16), and here both chains still have the chance to be
-the densest.
-This means that a test will terminate while the selection contains an arbitrary fragment
-whose tip is the block in slot 8.
--}
+-- | Interesting categories to classify test inputs
 data Classifiers =
   Classifiers {
     -- | There are more than k blocks in the alternative chain after the intersection
     existsSelectableAdversary      :: Bool,
     -- | There are at least scg slots after the intesection on both the honest
     -- and the alternative chain
+    --
+    -- Knowing if there is a Genesis window after the intersection is important because
+    -- otherwise the Genesis node has no chance to advance the immutable tip past
+    -- the Limit on Eagerness.
+    --
     genesisWindowAfterIntersection :: Bool
   }
 
