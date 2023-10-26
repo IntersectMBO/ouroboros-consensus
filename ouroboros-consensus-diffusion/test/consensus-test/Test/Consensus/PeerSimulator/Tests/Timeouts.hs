84d7a1c637498c8572cd90e524508414e9a822f3
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-10-26 14:26:47 +0200

    Add a variant of the timeout test using dynamic point schedules

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/Tests/Timeouts.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/Tests/Timeouts.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Test.Consensus.PeerSimulator.Tests.Timeouts (tests) where
@@ -28,37 +27,43 @@ import           Test.Tasty.QuickCheck
 import           Test.Util.Orphans.IOLike ()
 
 tests :: TestTree
-tests = testProperty "timeouts" prop_timeouts
+tests = testGroup "timeouts" [
+    testProperty "static" prop_timeouts_static,
+    testProperty "dynamic" prop_timeouts_dynamic
+  ]
 
-prop_timeouts :: QC.Gen QC.Property
-prop_timeouts = do
+stateViewProperty :: StateView -> QC.Property
+stateViewProperty stateView =
+  case svChainSyncExceptions stateView of
+    [] ->
+      counterexample ("result: " ++ condense (svSelectedChain stateView)) False
+    [exn] ->
+      case fromException $ cseException exn of
+        Just (ExceededTimeLimit _) -> property True
+        _ -> counterexample ("exception: " ++ show exn) False
+    exns ->
+      counterexample ("exceptions: " ++ show exns) False
+
+-- | Static variant of the test. We count exactly how many ticks are going to be
+-- necessary to make the test reach a timeout and we create a schedule that
+-- exactly that amount of ticks.
+prop_timeouts_static :: QC.Gen QC.Property
+prop_timeouts_static = do
   genesisTest <- genChains 0
 
   -- Use higher tick duration to avoid the test taking really long
   let scSchedule = PointScheduleConfig {pscTickDuration = 1}
-
       schedulerConfig = defaultSchedulerConfig scSchedule (gtHonestAsc genesisTest)
-
       schedule =
         dullSchedule
           scSchedule
           (fromJust $ mustReplyTimeout (scChainSyncTimeouts schedulerConfig))
           (btTrunk $ gtBlockTree genesisTest)
 
-  pure $ withMaxSuccess 10 $ runSimOrThrow $
-    runTest schedulerConfig genesisTest schedule $ \stateView ->
-      case svChainSyncExceptions stateView of
-        [] ->
-          counterexample ("result: " ++ condense (svSelectedChain stateView)) False
-        [exn] ->
-          case fromException $ cseException exn of
-            Just (ExceededTimeLimit _) -> property True
-            _ -> counterexample ("exception: " ++ show exn) False
-        exns ->
-          counterexample ("exceptions: " ++ show exns) False
+  pure $ runSimOrThrow $
+    runTest schedulerConfig genesisTest schedule stateViewProperty
 
   where
-
     -- A schedule that advertises all the points of the chain from the start but
     -- contains just one too many ticks, therefore reaching the timeouts.
     dullSchedule :: PointScheduleConfig -> DiffTime -> TestFrag -> PointSchedule
@@ -72,3 +77,39 @@ prop_timeouts = do
           maximumNumberOfTicks = round $ timeout / pscTickDuration scheduleConfig
       in
       PointSchedule (tick :| replicate maximumNumberOfTicks tick)
+
+-- | Dynamic variant of the test. We start with one tick but provide an extender
+-- that systematically adds another tick. If no exception is every triggered,
+-- then this test hangs. However, it is conceptually simpler and requires less
+-- inputs than its static counterpart.
+prop_timeouts_dynamic :: QC.Gen QC.Property
+prop_timeouts_dynamic = do
+  genesisTest <- genChains 0
+  let schedule = oneshotSchedule (btTrunk $ gtBlockTree genesisTest)
+
+  -- Use higher tick duration to avoid the test taking really long. Add an
+  -- extender that adds the same tick over and over again until we get a
+  -- ChainSyncException.
+  let scSchedule = PointScheduleConfig {pscTickDuration = 1}
+      schedulerConfig =
+        (defaultSchedulerConfig scSchedule (gtHonestAsc genesisTest)) {
+          scExtender = \stateView ->
+              case svChainSyncExceptions stateView of
+                [] -> Just schedule
+                _  -> Nothing
+          }
+
+  pure $ runSimOrThrow $
+    runTest schedulerConfig genesisTest schedule stateViewProperty
+
+  where
+    -- A schedule that advertises all the points of the chain in one tick.
+    oneshotSchedule :: TestFrag -> PointSchedule
+    oneshotSchedule (AF.Empty _) = error "requires a non-empty block tree"
+    oneshotSchedule (_ AF.:> tipBlock) =
+      let tipPoint = TipPoint $ tipFromHeader tipBlock
+          headerPoint = HeaderPoint $ getHeader tipBlock
+          blockPoint = BlockPoint tipBlock
+          state = Peer HonestPeer $ NodeOnline $ AdvertisedPoints tipPoint headerPoint blockPoint
+          tick = Tick { active = state, peers = Peers state Map.empty }
+      in PointSchedule (tick :| [])
