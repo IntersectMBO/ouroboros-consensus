500ac40d7c62f880c0fe85fadceeb1050af6f174
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-10-26 11:34:10 +0200

    Document `StateView`

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/StateView.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/StateView.hs
@@ -14,7 +14,16 @@ data ChainSyncException = ChainSyncException
        }
     deriving Show
 
+-- | A record that helps inspecting the current state of the simulation. This
+-- contains both information on the peer simulator and on the node under test
+-- (and in particular its ChainDB).
 data StateView = StateView {
+    -- | The chain currently selected by the ChainDB.
     svSelectedChain       :: TestFragH,
+
+    -- | A list of exception raised in threads running scheduled ChainSync
+    -- servers.
+    --
+    -- REVIEW: Only the server part?
     svChainSyncExceptions :: [ChainSyncException]
   }
