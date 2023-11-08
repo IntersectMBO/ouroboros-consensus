a67d10ec07ec9171fe149b595d84da17e96a3d47
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-11-08 14:54:20 +0000

    Add a helper `adjustQuickCheckTests`

--- ouroboros-consensus/src/unstable-consensus-testlib/Test/Util/TestEnv.hs
+++ ouroboros-consensus/src/unstable-consensus-testlib/Test/Util/TestEnv.hs
@@ -3,6 +3,7 @@
 -- | A @tasty@ command-line option for enabling nightly tests
 module Test.Util.TestEnv (
     TestEnv (..)
+  , adjustQuickCheckTests
   , askTestEnv
   , defaultMainWithTestEnv
   , defaultTestEnvConfig
@@ -66,3 +67,9 @@ instance IsOption TestEnv where
 
   -- Set of choices for test environment
   optionCLParser = mkOptionCLParser $ metavar "nightly|ci|dev"
+
+-- | Locally adjust the number of QuickCheck tests for the given test subtree.
+adjustQuickCheckTests :: (Int -> Int) -> TestTree -> TestTree
+adjustQuickCheckTests f =
+  adjustOption $ \(QuickCheckTests n) ->
+    QuickCheckTests $ if n == 0 then 0 else max 1 (f n)
