db2d0ab4862ef47389491ce5f2b3680fb6a250c4
Damian Nadales <damian.only@gmail.com>
2023-11-14 18:22:35 +0100

    Add an option to output data points as JSON
    To this end this patch also:
    
    - Abstracts away the `DataPoint` file writing operations. The block
      stats are now inside the `DataPoint` to facilitate this abstraction.
    - Implements `ToJSON` instance for `SlotDataPoint`.
    - Adds support for selecting the `benchmark-ledger-ops` output format
      based on the file path extension of the output file passed to the
      benchmark ledger ops analysis.
    - Adds support for printing the analysis metadata when the JSON output
      is chosen.
    - Encapsulates the file writing aspect of `BenchmarkLedgerOps`
    - Adds minor GCs and allocated bytes to `DataPoint`.

---
+++ ouroboros-consensus-cardano/src/unstable-cardano-tools/Cardano/Tools/DBAnalyser/Analysis/BenchmarkLedgerOps/Metadata.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+-- | Functions related to obtaining information about the 'db-analyser' run.
+--
+-- Metadata includes information such as:
+--
+-- - RTS flags.
+-- - Compiler version.
+-- - OS and architecture.
+--
+-- See 'Metadata' and 'getMetadata' for more details.
+--
+module Cardano.Tools.DBAnalyser.Analysis.BenchmarkLedgerOps.Metadata (
+    Metadata (..)
+  , getMetadata
+  ) where
+
+import           Cardano.Tools.GitRev (gitRev)
+import           Data.Aeson (ToJSON)
+import qualified Data.Aeson as Aeson
+import qualified Data.Text as T
+import qualified Data.Version
+import           Data.Word (Word32, Word64)
+import           GHC.Generics (Generic)
+import qualified GHC.RTS.Flags as RTS
+import qualified System.Info
+
+data Metadata = Metadata {
+    rtsGCMaxStkSize             :: Word32
+  , rtsGCMaxHeapSize            :: Word32
+  , rtsConcurrentCtxtSwitchTime :: Word64
+  , rtsParNCapabilities         :: Word32
+  , compilerVersion             :: String
+  , compilerName                :: String
+  , operatingSystem             :: String
+  , machineArchitecture         :: String
+  , gitRevison                  :: String
+  } deriving (Generic, Show, Eq)
+
+instance ToJSON Metadata where
+  toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
+
+getMetadata :: IO Metadata
+getMetadata = do
+  rtsFlags <- RTS.getRTSFlags
+  pure $ Metadata {
+      rtsGCMaxStkSize             = RTS.maxStkSize     $ RTS.gcFlags rtsFlags
+    , rtsGCMaxHeapSize            = RTS.maxHeapSize    $ RTS.gcFlags rtsFlags
+    , rtsConcurrentCtxtSwitchTime = RTS.ctxtSwitchTime $ RTS.concurrentFlags rtsFlags
+    , rtsParNCapabilities         = RTS.nCapabilities  $ RTS.parFlags rtsFlags
+    , compilerVersion             = Data.Version.showVersion System.Info.compilerVersion
+    , compilerName                = System.Info.compilerName
+    , operatingSystem             = System.Info.os
+    , machineArchitecture         = System.Info.arch
+    , gitRevison                  = T.unpack gitRev
+    }
