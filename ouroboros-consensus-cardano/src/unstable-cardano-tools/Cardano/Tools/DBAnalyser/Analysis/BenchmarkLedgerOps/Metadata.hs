2588100c585c1913717a64e078e3cb042afc9455
Damian Nadales <damian.only@gmail.com>
2023-11-21 17:30:54 +0100

    Extract metadata and related functions and instances to a separate module

---
+++ ouroboros-consensus-cardano/src/unstable-cardano-tools/Cardano/Tools/DBAnalyser/Analysis/BenchmarkLedgerOps/Metadata.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DeriveGeneric       #-}
+
+-- | Functions related to obtaining information about the 'db-analyser' run.
+--
+-- Metadata includes information such as:
+--
+-- - RTS flags.
+-- - Compiler version.
+-- - OS and architecture.
+--
+-- See 'Metadata' and 'getMetadata' for more details.
+--
+module Cardano.Tools.DBAnalyser.Analysis.BenchmarkLedgerOps.Metadata (
+    Metadata (..)
+  , getMetadata
+  ) where
+
+import           Data.Word (Word32, Word64)
+import           Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Aeson as Aeson
+import qualified GHC.RTS.Flags as RTS
+import qualified System.Info
+import           GHC.Generics (Generic)
+import qualified Data.Version
+
+data Metadata = Metadata {
+    rtsGCMaxStkSize             :: Word32
+  , rtsGCMaxHeapSize            :: Word32
+  , rtsConcurrentCtxtSwitchTime :: Word64
+  , rtsParNCapabilities         :: Word32
+  , compilerVersion             :: String
+  , compilerName                :: String
+  , operatingSystem             :: String
+  , machineArchitecture         :: String
+  } deriving (Generic, Show)
+
+instance ToJSON Metadata where
+  toEncoding = Aeson.genericToEncoding Aeson.defaultOptions
+
+instance FromJSON Metadata
+
+getMetadata :: IO Metadata
+getMetadata = do
+  rtsFlags <- RTS.getRTSFlags
+  pure $ Metadata {
+      rtsGCMaxStkSize             = RTS.maxStkSize     $ RTS.gcFlags rtsFlags
+    , rtsGCMaxHeapSize            = RTS.maxHeapSize    $ RTS.gcFlags rtsFlags
+    , rtsConcurrentCtxtSwitchTime = RTS.ctxtSwitchTime $ RTS.concurrentFlags rtsFlags
+    , rtsParNCapabilities         = RTS.nCapabilities  $ RTS.parFlags rtsFlags
+    , compilerVersion             = Data.Version.showVersion System.Info.compilerVersion
+    , compilerName                = System.Info.compilerName
+    , operatingSystem             = System.Info.os
+    , machineArchitecture         = System.Info.arch
+    }
