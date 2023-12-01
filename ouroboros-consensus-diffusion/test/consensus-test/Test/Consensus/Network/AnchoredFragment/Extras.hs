cb489e2d26eed793232e7d440cd1d4249a05d438
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-12-01 17:49:47 +0000

    Edit slotLength documentation as requested in #434

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Network/AnchoredFragment/Extras.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Network/AnchoredFragment/Extras.hs
@@ -4,7 +4,7 @@ import           Cardano.Slotting.Slot (SlotNo (unSlotNo), withOrigin)
 import           Ouroboros.Network.AnchoredFragment (AnchoredFragment,
                      HasHeader, anchor, anchorToSlotNo, headAnchor)
 
--- | Number of slots on which the fragment spans. This is different from the
+-- | The number of slots the fragment spans. This is different from the
 -- 'length' which is the number of blocks in the fragment.
 slotLength :: HasHeader blk => AnchoredFragment blk -> Int
 slotLength fragment =
