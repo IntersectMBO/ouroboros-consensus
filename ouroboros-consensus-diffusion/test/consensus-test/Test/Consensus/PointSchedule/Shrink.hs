8edfe2252a5bd9d7c0ac6eee2126d92220dfc67e
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-12-12 15:01:47 +0000

    Add a `trimBlockTree` shrinking helper

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PointSchedule/Shrink.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PointSchedule/Shrink.hs
@@ -7,15 +7,19 @@ module Test.Consensus.PointSchedule.Shrink (
   ) where
 
 import           Data.Function ((&))
+import           Data.List (isPrefixOf, isSuffixOf, sortOn)
 import           Data.List.NonEmpty (toList)
 import           Data.Maybe (mapMaybe)
-import           Ouroboros.Network.AnchoredFragment (headPoint)
+import           Ouroboros.Consensus.Block (blockHash, blockSlot)
+import           Ouroboros.Network.AnchoredFragment (AnchoredFragment,
+                     AnchoredSeq (..), headPoint)
 import           Ouroboros.Network.Block (Point)
-import           Test.Consensus.BlockTree (BlockTree (btTrunk))
+import           Test.Consensus.BlockTree (BlockTree (..), addBranch',
+                     btbSuffix, mkTrunk)
 import           Test.Consensus.PeerSimulator.StateView (StateView)
 import           Test.Consensus.PointSchedule
 import           Test.QuickCheck (shrinkList)
-import           Test.Util.TestBlock (TestBlock)
+import           Test.Util.TestBlock (TestBlock, TestHash (unTestHash))
 
 --------------------------------------------------------------------------------
 -- Public interface
@@ -83,3 +87,53 @@ shrinkTicksList _bt PointSchedule{ticks, peerIds} =
 --------------------------------------------------------------------------------
 -- Smart shrinkers
 --------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- Other utilities
+--------------------------------------------------------------------------------
+
+-- | Remove blocks from the given block tree that are not necessary for the
+-- given point schedule. If entire branches are unused, they are removed. If the
+-- trunk is unused, then it remains as an empty anchored fragment.
+trimBlockTree :: BlockTree TestBlock -> PointSchedule -> BlockTree TestBlock
+trimBlockTree bt ps =
+    let youngest = selectYoungest (reverse (pointScheduleBlocks ps))
+        trunk = trimFragment youngest (btTrunk bt)
+        branches = mapMaybe (fragmentToMaybe . trimFragment youngest . btbSuffix) (btBranches bt)
+     in foldr addBranch' (mkTrunk trunk) branches
+  where
+    fragmentToMaybe (Empty _) = Nothing
+    fragmentToMaybe fragment  = Just fragment
+
+    -- | Given a list of blocks and a fragment, cut the fragment such that it
+    -- contains only blocks that are ancestors of blocks in the list.
+    trimFragment :: [TestBlock] -> AnchoredFragment TestBlock -> AnchoredFragment TestBlock
+    trimFragment _ fragment@(Empty _) = fragment
+    trimFragment youngest (fragment :> block)
+      | any (block `isOlderThan`) youngest = fragment :> block
+      | otherwise = trimFragment youngest fragment
+
+    -- | Return a subset of the given block containing youngest elements. It is
+    -- not guaranteed that this set is minimal. It is however guaranteed that
+    -- any block in the input list is an ancestor of a block in the output list.
+    selectYoungest :: [TestBlock] -> [TestBlock]
+    selectYoungest = go [] . sortOn blockSlot
+      where
+        go youngest [] = youngest
+        go youngest (block : blocks)
+          | any (`isYoungerThan` block) youngest = go youngest blocks
+          | otherwise = go (block : youngest) blocks
+
+    -- | Partial comparison of blocks. A block is older than another block if it
+    -- is its ancestor. For test blocks, this can be seen in the hash.
+    isOlderThan :: TestBlock -> TestBlock -> Bool
+    isOlderThan b1 b2 =
+      toList (unTestHash (blockHash b1))
+        `isPrefixOf`
+      toList (unTestHash (blockHash b2))
+
+    isYoungerThan :: TestBlock -> TestBlock -> Bool
+    isYoungerThan b1 b2 =
+      toList (unTestHash (blockHash b1))
+        `isSuffixOf`
+      toList (unTestHash (blockHash b2))
