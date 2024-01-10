f7e9dde7319b83d28fc0f0993dfc5c7dd6ed5489
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-01-10 12:24:04 +0000

    Partially revert "Uniform property relies on a `PointSchedule`"
    This partially reverts commit cd82d765018cdb07de88cd76271d5afe6b62d6f2
    using the newly introduced `forAllGenesisTest'` helper.

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/Uniform.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/Uniform.hs
@@ -56,7 +56,7 @@ tests =
 
 theProperty ::
   GenesisTest ->
-  PointSchedule ->
+  Peers PeerSchedule ->
   StateView ->
   Property
 theProperty genesisTest schedule stateView@StateView{svSelectedChain} =
@@ -75,7 +75,7 @@ theProperty genesisTest schedule stateView@StateView{svSelectedChain} =
     immutableTipIsRecent
   ]
   where
-    advCount = length (peerIds schedule) - 1
+    advCount = Map.size (others schedule)
 
     immutableTipIsRecent =
       counterexample ("Age of the immutable tip: " ++ show immutableTipAge) $
@@ -109,7 +109,7 @@ theProperty genesisTest schedule stateView@StateView{svSelectedChain} =
       [] -> "No peers were killed"
       peers -> "Some peers were killed: " ++ intercalate ", " (condense <$> peers)
 
-    honestTipSlot = tbSlot <$> lastHonestBlockPoint schedule
+    honestTipSlot = At $ tbSlot $ snd $ last $ mapMaybe fromBlockPoint $ value $ honest schedule
 
     GenesisTest {gtBlockTree, gtGenesisWindow = GenesisWindow s, gtDelay = Delta d} = genesisTest
 
@@ -123,10 +123,10 @@ fromBlockPoint _                          = Nothing
 -- adversarial peers serving adversarial branches.
 prop_serveAdversarialBranches :: Property
 prop_serveAdversarialBranches =
-  expectFailure $ forAllGenesisTest
+  expectFailure $ forAllGenesisTest'
 
     (do gt <- genChains (QC.choose (1, 4))
-        ps <- fromSchedulePoints <$> genUniformSchedulePoints gt
+        ps <- genUniformSchedulePoints gt
         pure (gt, ps))
 
     ((noTimeoutsSchedulerConfig defaultPointScheduleConfig)
@@ -165,10 +165,10 @@ genUniformSchedulePoints gt = stToGen (uniformPoints (gtBlockTree gt))
 -- yet.
 prop_leashingAttackStalling :: Property
 prop_leashingAttackStalling =
-  expectFailure $ forAllGenesisTest
+  expectFailure $ forAllGenesisTest'
 
     (do gt <- genChains (QC.choose (1, 4))
-        ps <- fromSchedulePoints <$> genLeashingSchedule gt
+        ps <- genLeashingSchedule gt
         pure (gt, ps))
 
     ((noTimeoutsSchedulerConfig defaultPointScheduleConfig)
@@ -210,10 +210,10 @@ prop_leashingAttackStalling =
 -- See Note [Leashing attacks]
 prop_leashingAttackTimeLimited :: Property
 prop_leashingAttackTimeLimited =
-  expectFailure $ forAllGenesisTest
+  expectFailure $ forAllGenesisTest'
 
     (do gt <- genChains (QC.choose (1, 4))
-        ps <- fromSchedulePoints <$> genTimeLimitedSchedule gt
+        ps <- genTimeLimitedSchedule gt
         pure (gt, ps))
 
     ((noTimeoutsSchedulerConfig defaultPointScheduleConfig)
