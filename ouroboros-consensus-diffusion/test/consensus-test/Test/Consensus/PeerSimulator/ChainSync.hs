15973dee3435aba5ff6dbcdfe1a93a01a65f1bf8
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-03-04 11:53:20 +0000

    Expose mini-protocol threads at a higher level

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/ChainSync.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/ChainSync.hs
@@ -4,14 +4,21 @@
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Test.Consensus.PeerSimulator.ChainSync (runChainSyncClient) where
+module Test.Consensus.PeerSimulator.ChainSync (
+    chainSyncNoSizeLimits
+  , chainSyncNoTimeouts
+  , runChainSyncClient
+  , runChainSyncServer
+  ) where
 
 import           Control.Exception (AsyncException (ThreadKilled))
 import           Control.Monad.Class.MonadTimer.SI (MonadTimer)
 import           Control.Tracer (Tracer (Tracer), nullTracer, traceWith)
+import           Data.Functor (void)
 import           Data.Map.Strict (Map)
 import           Data.Proxy (Proxy (..))
 import qualified Data.Set as Set
+import           Network.TypedProtocol.Codec (AnyMessage)
 import           Ouroboros.Consensus.Block (Header, Point)
 import           Ouroboros.Consensus.Config (TopLevelConfig (..))
 import           Ouroboros.Consensus.Ledger.SupportsProtocol
@@ -26,22 +33,24 @@ import           Ouroboros.Consensus.Util.IOLike (Exception (fromException),
                      IOLike, MonadCatch (try), StrictTVar, uncheckedNewTVarM)
 import           Ouroboros.Network.AnchoredFragment (AnchoredFragment)
 import           Ouroboros.Network.Block (Tip)
-import           Ouroboros.Network.Channel (createConnectedChannels)
+import           Ouroboros.Network.Channel (Channel)
 import           Ouroboros.Network.ControlMessage (ControlMessage (..))
+import           Ouroboros.Network.Driver (runPeer)
 import           Ouroboros.Network.Driver.Limits
-                     (ProtocolLimitFailure (ExceededSizeLimit, ExceededTimeLimit))
+                     (ProtocolLimitFailure (ExceededSizeLimit, ExceededTimeLimit),
+                     runPipelinedPeerWithLimits)
 import           Ouroboros.Network.NodeToNode.Version (NodeToNodeVersion)
 import           Ouroboros.Network.Protocol.ChainSync.ClientPipelined
                      (ChainSyncClientPipelined, chainSyncClientPeerPipelined)
-import           Ouroboros.Network.Protocol.ChainSync.Codec (ChainSyncTimeout,
+import           Ouroboros.Network.Protocol.ChainSync.Codec
+                     (ChainSyncTimeout (..), byteLimitsChainSync,
                      codecChainSyncId, timeLimitsChainSync)
 import           Ouroboros.Network.Protocol.ChainSync.PipelineDecision
                      (pipelineDecisionLowHighMark)
 import           Ouroboros.Network.Protocol.ChainSync.Server (ChainSyncServer,
                      chainSyncServerPeer)
-import           Test.Consensus.Network.Driver.Limits.Extras
-                     (chainSyncNoSizeLimits,
-                     runConnectedPeersPipelinedWithLimits)
+import           Ouroboros.Network.Protocol.ChainSync.Type (ChainSync)
+import           Ouroboros.Network.Protocol.Limits (ProtocolSizeLimits (..))
 import           Test.Consensus.PeerSimulator.StateView
                      (ChainSyncException (ChainSyncException),
                      StateViewTracers (StateViewTracers, svtChainSyncExceptionsTracer))
@@ -99,53 +108,80 @@ runChainSyncClient ::
   TopLevelConfig blk ->
   ChainDbView m blk ->
   PeerId ->
-  ChainSyncServer (Header blk) (Point blk) (Tip blk) m () ->
   ChainSyncTimeout ->
   ChainSyncLoPBucketConfig ->
   StateViewTracers m ->
   StrictTVar m (Map PeerId (StrictTVar m (AnchoredFragment (Header blk)))) ->
+  Channel m (AnyMessage (ChainSync (Header blk) (Point blk) (Tip blk))) ->
   m ()
 runChainSyncClient
   tracer
   cfg
   chainDbView
   peerId
-  server
   chainSyncTimeouts
   lopBucketConfig
   StateViewTracers {svtChainSyncExceptionsTracer}
   varCandidates
-  = do
+  channel = do
     -- We don't need this shared Set yet. If we need it at some point,
     -- it ought to be passed to `runChainSyncClient`.
     varIdling <- uncheckedNewTVarM $ Set.empty
-    bracketChainSyncClient nullTracer chainDbView varCandidates varIdling peerId ntnVersion lopBucketConfig $ \ varCandidate idleManagers lopBucket -> do
-      res <- try $ runConnectedPeersPipelinedWithLimits
-        createConnectedChannels
-        nullTracer
-        codecChainSyncId
-        chainSyncNoSizeLimits
-        (timeLimitsChainSync chainSyncTimeouts)
-        (chainSyncClientPeerPipelined (basicChainSyncClient peerId tracer cfg chainDbView varCandidate idleManagers lopBucket))
-        (chainSyncServerPeer server)
-      case res of
-        Right _ -> pure ()
-        Left exn -> do
-          traceWith svtChainSyncExceptionsTracer $ ChainSyncException peerId exn
-          case fromException exn of
-            Just (ExceededSizeLimit _) ->
-              traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceExceededSizeLimit
-            Just (ExceededTimeLimit _) ->
-              traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceExceededTimeLimit
-            Nothing -> pure ()
-          case fromException exn of
-            Just ThreadKilled ->
-              traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceTerminatedByGDDGovernor
-            _                 -> pure ()
-          case fromException exn of
-            Just CSClient.EmptyBucket ->
-              traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceTerminatedByLoP
-            _ -> pure ()
-  where
-    ntnVersion :: NodeToNodeVersion
-    ntnVersion = maxBound
+    bracketChainSyncClient
+      nullTracer
+      chainDbView
+      varCandidates
+      varIdling
+      peerId
+      (maxBound :: NodeToNodeVersion)
+      lopBucketConfig
+      $ \varCandidate idleManagers lopBucket -> do
+        res <-
+          try $
+            runPipelinedPeerWithLimits
+              nullTracer
+              codecChainSyncId
+              chainSyncNoSizeLimits
+              (timeLimitsChainSync chainSyncTimeouts)
+              channel
+              (chainSyncClientPeerPipelined (basicChainSyncClient peerId tracer cfg chainDbView varCandidate idleManagers lopBucket))
+        case res of
+          Right _  -> pure ()
+          Left exn -> traceException exn
+    where
+      traceException exn = do
+        traceWith svtChainSyncExceptionsTracer $ ChainSyncException peerId exn
+        case fromException exn of
+          Just (ExceededSizeLimit _) ->
+            traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceExceededSizeLimit
+          Just (ExceededTimeLimit _) ->
+            traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceExceededTimeLimit
+          Nothing -> pure ()
+        case fromException exn of
+          Just ThreadKilled ->
+            traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceTerminatedByGDDGovernor
+          _ -> pure ()
+        case fromException exn of
+          Just CSClient.EmptyBucket ->
+            traceWith tracer $ TraceChainSyncClientTerminationEvent peerId TraceTerminatedByLoP
+          _ -> pure ()
+
+chainSyncNoSizeLimits :: ProtocolSizeLimits (ChainSync header point tip) bytes
+chainSyncNoSizeLimits = byteLimitsChainSync (const 0)
+
+chainSyncNoTimeouts :: ChainSyncTimeout
+chainSyncNoTimeouts =
+  ChainSyncTimeout
+    { canAwaitTimeout = Nothing,
+      intersectTimeout = Nothing,
+      mustReplyTimeout = Nothing,
+      idleTimeout = Nothing
+    }
+
+runChainSyncServer ::
+  (IOLike m, ShowProxy blk, ShowProxy (Header blk)) =>
+  ChainSyncServer (Header blk) (Point blk) (Tip blk) m () ->
+  Channel m (AnyMessage (ChainSync (Header blk) (Point blk) (Tip blk))) ->
+  m ()
+runChainSyncServer server channel =
+  void $ runPeer nullTracer codecChainSyncId channel (chainSyncServerPeer server)
