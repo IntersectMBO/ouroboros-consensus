19e2f7185bc0ecb7cf720a306076b2cdb58f1e80
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-03-08 18:31:30 +0000

    Fix alignment of fields of `ScheduledBlockFetchServer`

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/ScheduledBlockFetchServer.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/ScheduledBlockFetchServer.hs
@@ -33,8 +33,8 @@ data BlockFetch =
 -- | Resources used by a BlockFetch server mock.
 data ScheduledBlockFetchServer m a =
   ScheduledBlockFetchServer {
-    sbfsServer :: ScheduledServer m a,
-    sbfsHandler       :: ChainRange (Point TestBlock) -> a -> STM m (Maybe BlockFetch, [String])
+    sbfsServer  :: ScheduledServer m a,
+    sbfsHandler :: ChainRange (Point TestBlock) -> a -> STM m (Maybe BlockFetch, [String])
   }
 
 scheduledBlockFetchServer ::
