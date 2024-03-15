7efd67df46f9f25e22bae14132205d902892845e
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-03-15 21:14:56 +0000

    Print the idle timeout in the trace output

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PointSchedule.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PointSchedule.hs
@@ -354,6 +354,7 @@ prettyGenesisTest prettySchedule genesisTest =
   , "    canAwait = " ++ show canAwaitTimeout
   , "    intersect = " ++ show intersectTimeout
   , "    mustReply = " ++ show mustReplyTimeout
+  , "    idle = " ++ show idleTimeout
   , "  gtBlockFetchTimeouts: "
   , "    busy = " ++ show busyTimeout
   , "    streaming = " ++ show streamingTimeout
@@ -372,7 +373,8 @@ prettyGenesisTest prettySchedule genesisTest =
       , gtForecastRange
       , gtDelay = Delta delta
       , gtBlockTree
-      , gtChainSyncTimeouts = ChainSyncTimeout{canAwaitTimeout, intersectTimeout, mustReplyTimeout}
+      , gtChainSyncTimeouts =
+          ChainSyncTimeout{canAwaitTimeout, intersectTimeout, mustReplyTimeout, idleTimeout}
       , gtBlockFetchTimeouts = BlockFetchTimeout{busyTimeout, streamingTimeout}
       , gtLoPBucketParams = LoPBucketParams{lbpCapacity, lbpRate}
       , gtSlotLength
