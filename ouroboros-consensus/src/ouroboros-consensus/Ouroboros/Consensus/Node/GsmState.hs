30da3243d94797462e53e4c66e6358a9fe49d0e3
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-03-19 10:51:12 +0000

    Make the LoP follow the GSM states
    This also involves some fairly important changes to the leaky bucket and
    to the leaky bucket API. These changes make the leaky bucket
    significantly more robust.
    
    Co-authored-by: Alexander Esgen <alexander.esgen@iohk.io>

---
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Node/GsmState.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+
+-- | This module contains the definition of a state in the Genesis State Machine
+-- (GSM). The GSM itself is defined in 'ouroboros-consensus-diffusion', but the
+-- ChainSync client relies on its state.
+module Ouroboros.Consensus.Node.GsmState (GsmState (..)) where
+
+import           GHC.Generics (Generic)
+import           NoThunks.Class (NoThunks)
+
+-- | Current state of the Genesis State Machine
+data GsmState =
+    PreSyncing
+    -- ^ We are syncing, and the Honest Availability Assumption is not
+    -- satisfied.
+  |
+    Syncing
+    -- ^ We are syncing, and the Honest Availability Assumption is satisfied.
+  |
+    CaughtUp
+    -- ^ We are caught-up.
+  deriving (Eq, Show, Read, Generic, NoThunks)
