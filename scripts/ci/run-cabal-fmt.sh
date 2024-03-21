bb1f3e2445577ce0dd90450adb74488b217baca2
Damian Nadales <damian.nadales@iohk.io>
2024-03-21 15:30:01 +0100

    Adapt formatting scripts to work with Ubuntu installs of `fd`.

--- scripts/ci/run-cabal-fmt.sh
+++ scripts/ci/run-cabal-fmt.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
-fd --full-path "$(pwd)/(ouroboros-consensus|sop-extras|strict-sop-core)" -e cabal -x cabal-fmt -i
+# First, try to find the 'fd' command
+fdcmd="fd"
+if ! command -v "$fdcmd" &> /dev/null; then
+    # In Ubuntu systems the fd command is called fdfind.
+    # If 'fd' is not found, try 'fdfind'
+    fdcmd="fdfind"
+    if ! command -v "$fdcmd" &> /dev/null; then
+        echo "Error: Neither 'fd' nor 'fdfind' command found." >&2
+        exit 1
+    fi
+fi
+
+$fdcmd --full-path "$(pwd)/(ouroboros-consensus|sop-extras|strict-sop-core)" -e cabal -x cabal-fmt -i
