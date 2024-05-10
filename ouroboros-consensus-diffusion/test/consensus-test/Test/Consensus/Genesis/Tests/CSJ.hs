29c90f2a3b97d7394f2ae20a519222905a2b056d
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-05-10 12:08:21 +0000

    Add some adversaries to the “happy path” test

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/CSJ.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/CSJ.hs
@@ -3,10 +3,10 @@
 
 module Test.Consensus.Genesis.Tests.CSJ (tests) where
 
-import           Data.Containers.ListUtils (nubOrd)
 import           Data.List (nub)
+import qualified Data.Map.Strict as Map
 import           Data.Maybe (mapMaybe)
-import           Ouroboros.Consensus.Block (blockSlot, succWithOrigin)
+import           Ouroboros.Consensus.Block (Header, blockSlot, succWithOrigin)
 import           Ouroboros.Consensus.MiniProtocol.ChainSync.Client
                      (TraceChainSyncClientEvent (..))
 import           Ouroboros.Consensus.Util.Condense (PaddingDirection (..),
@@ -20,51 +20,66 @@ import           Test.Consensus.PeerSimulator.Run (SchedulerConfig (..),
 import           Test.Consensus.PeerSimulator.StateView (StateView (..))
 import           Test.Consensus.PeerSimulator.Trace (TraceEvent (..))
 import           Test.Consensus.PointSchedule
+import qualified Test.Consensus.PointSchedule.Peers as Peers
 import           Test.Consensus.PointSchedule.Peers (Peers (..), peers')
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 import           Test.Util.Orphans.IOLike ()
 import           Test.Util.PartialAccessors
-import           Test.Util.TestBlock (Header, TestBlock)
+import           Test.Util.TestBlock (TestBlock)
 import           Test.Util.TestEnv (adjustQuickCheckMaxSize)
 
 tests :: TestTree
 tests =
   adjustQuickCheckMaxSize (`div` 5) $
-  testGroup
-    "CSJ"
-    [ testGroup "Happy Path"
-      [ testProperty "synchronous" $ prop_happyPath True
-      , testProperty "asynchronous" $ prop_happyPath False
+    testGroup
+      "CSJ"
+      [ testGroup
+          "Happy Path"
+          [ testProperty "honest peers are synchronised" $ prop_CSJ True True,
+            testProperty "honest peers do their own thing" $ prop_CSJ True False
+          ],
+        testGroup
+          "With some adversaries"
+          [ testProperty "honest peers are synchronised" $ prop_CSJ False True,
+            testProperty "honest peers do their own thing" $ prop_CSJ False False
+          ]
       ]
-    ]
 
--- | Test of the “happy path” scenario of ChainSync Jumping (CSJ).
+-- | Test of ChainSync Jumping (CSJ).
 --
--- This test features one chain (ie. a block tree that is only trunk) and only
--- honest peers and syncs the chain in question with CSJ enabled. What we expect
--- to observe is that one of the honest peers becomes the dynamo while the
--- others become jumpers. Because the jumpers will agree to all the jumps, the
--- whole syncing should happen with CSJ without objectors.
+-- This test features several peers the all sync the “honest” chain (ie. the
+-- trunk of the block tree) with CSJ enabled. What we expect to observe is that
+-- one of the honest peers becomes the dynamo while the others become jumpers.
+-- Because the jumpers will agree to all the jumps, the whole syncing should
+-- happen with CSJ.
 --
--- The final property is that headers should only ever be downloaded once and
--- only from one peer (the dynamo). This is true except when almost caught-up:
--- when the dynamo is caught-up, it gets disengaged and one of the jumpers takes
--- its place and starts serving headers. This might lead to duplication of
--- headers, but only in a window of @jumpSize@ slots near the tip of the chain.
+-- There are two variants of this test: the “happy path” variant features no
+-- adversaries. As such, everything should happen with one dynamo and no
+-- objector. Another variant adds adversaries, so we expect to see some
+-- dynamo-vs-objector action.
 --
--- The boolean differentiates between “synchronous” and “asynchronous”
--- scenarios. In a synchronous scenario, all the honest peers have the same
--- schedule: they serve the chain exactly in the same way. In the asynchronous
--- scenario, a random schedule is generated for each peer (but they still serve
--- the same chain).
-prop_happyPath :: Bool -> Property
-prop_happyPath synchronized =
+-- Regardless, the final property is that “honest” headers should only ever be
+-- downloaded at most once from honest peers. They may however be downloaded
+-- several times from adversaries. This is true except when almost caught-up:
+-- when the dynamo or objector is caught-up, it gets disengaged and one of the
+-- jumpers takes its place and starts serving headers. This might lead to
+-- duplication of headers, but only in a window of @jumpSize@ slots near the tip
+-- of the chain.
+--
+-- The first boolean differentiates between the “happy path” variant and the
+-- variant with adversaries; the second boolean differentiates between
+-- “synchronous” and “asynchronous” scenarios. In a synchronous scenario, all
+-- the honest peers have the same schedule: they serve the chain exactly in the
+-- same way. In the asynchronous scenario, a random schedule is generated for
+-- each peer (but they still serve the same chain).
+prop_CSJ :: Bool -> Bool -> Property
+prop_CSJ happy synchronized =
   forAllGenesisTest
     ( if synchronized
-        then genChainsWithExtraHonestPeers (choose (2, 4)) (pure 0)
+        then genChainsWithExtraHonestPeers (choose (2, 4)) (if happy then pure 0 else choose (2, 4))
           `enrichedWith` genUniformSchedulePoints
-        else genChains (pure 0)
+        else genChains (if happy then pure 0 else choose (2, 4))
           `enrichedWith` genDuplicatedHonestSchedule
     )
     ( defaultSchedulerConfig
@@ -80,43 +95,46 @@ prop_happyPath synchronized =
     )
     ( \gt StateView{svTrace} ->
         let
-          headerDownloadEvents =
+          headerHonestDownloadEvents =
             mapMaybe
               (\case
                 TraceChainSyncClientEvent pid (TraceDownloadedHeader hdr)
                   | not (isNewerThanJumpSizeFromTip gt hdr)
+                  , Peers.HonestPeer _ <- pid
                   -> Just (pid, hdr)
                 _ -> Nothing
               )
               svTrace
-          receivedHeadersOnlyOnce = length (nub $ snd <$> headerDownloadEvents) == length headerDownloadEvents
-          -- NOTE: If all the headers are newer than jumpSize from the tip, then
-          -- 'headerDownloadEvents' is empty and the following condition would
-          -- violated if we used @==@.
-          receivedHeadersFromOnlyOnePeer = length (nubOrd $ fst <$> headerDownloadEvents) <= 1
+          receivedHeadersAtMostOnceFromHonestPeers =
+            length (nub $ snd <$> headerHonestDownloadEvents) == length headerHonestDownloadEvents
         in
           tabulate ""
-            [ if headerDownloadEvents == []
-                then "All headers may be downloaded twice"
+            [ if headerHonestDownloadEvents == []
+                then "All headers are within the last jump window"
                 else "There exist headers that have to be downloaded exactly once"
             ] $
           counterexample
           ("Downloaded headers (except jumpSize slots near the tip):\n" ++
             ( unlines $ fmap ("  " ++) $ zipWith
               (\peer header -> peer ++ " | " ++ header)
-              (condenseListWithPadding PadRight $ fst <$> headerDownloadEvents)
-              (condenseListWithPadding PadRight $ snd <$> headerDownloadEvents)
+              (condenseListWithPadding PadRight $ fst <$> headerHonestDownloadEvents)
+              (condenseListWithPadding PadRight $ snd <$> headerHonestDownloadEvents)
             )
           )
-          (receivedHeadersOnlyOnce && receivedHeadersFromOnlyOnePeer)
+          receivedHeadersAtMostOnceFromHonestPeers
     )
   where
     genDuplicatedHonestSchedule :: GenesisTest TestBlock () -> Gen (PeersSchedule TestBlock)
-    genDuplicatedHonestSchedule gt@GenesisTest{gtExtraHonestPeers} = do
-      Peers {honestPeers} <- genUniformSchedulePoints gt
-      pure $ peers'
-        (replicate (fromIntegral gtExtraHonestPeers + 1) (getHonestPeer honestPeers))
-        []
+    genDuplicatedHonestSchedule gt@GenesisTest {gtExtraHonestPeers} = do
+      Peers {honestPeers, adversarialPeers} <- genUniformSchedulePoints gt
+      pure $
+        Peers.unionWithKey
+          (\_ _ _ -> error "should not happen")
+          ( peers'
+              (replicate (fromIntegral gtExtraHonestPeers + 1) (getHonestPeer honestPeers))
+              []
+          )
+          (Peers Map.empty adversarialPeers)
 
     isNewerThanJumpSizeFromTip :: GenesisTestFull TestBlock -> Header TestBlock -> Bool
     isNewerThanJumpSizeFromTip gt hdr =
