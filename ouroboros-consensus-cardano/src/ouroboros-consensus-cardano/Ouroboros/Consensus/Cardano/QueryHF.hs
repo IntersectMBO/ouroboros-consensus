3193e7dfa808016e9a4c961d333b101b544a02f1
Javier Sagredo <javier.sagredo@iohk.io>
2024-06-04 16:13:18 +0200

    Remove toNS/fromNS

--- ouroboros-consensus-cardano/src/ouroboros-consensus-cardano/Ouroboros/Consensus/Cardano/QueryHF.hs
+++ ouroboros-consensus-cardano/src/ouroboros-consensus-cardano/Ouroboros/Consensus/Cardano/QueryHF.hs
@@ -25,7 +25,6 @@
 module Ouroboros.Consensus.Cardano.QueryHF () where
 
 import           Data.SOP.Index
-import           Data.SOP.Strict
 import           Ouroboros.Consensus.Byron.Ledger
 import           Ouroboros.Consensus.Byron.Node ()
 import           Ouroboros.Consensus.Cardano.Block
@@ -37,7 +36,6 @@ import           Ouroboros.Consensus.Shelley.HFEras ()
 import           Ouroboros.Consensus.Shelley.Ledger
 import           Ouroboros.Consensus.Shelley.Node ()
 import           Ouroboros.Consensus.Shelley.Protocol.Praos ()
-import           Ouroboros.Consensus.TypeFamilyWrappers
 
 instance CardanoHardForkConstraints c => BlockSupportsHFLedgerQuery (CardanoEras c) where
   answerBlockQueryHFLookup IZ _cfg (q :: BlockQuery ByronBlock QFLookupTables result) _dlv =
@@ -74,71 +72,70 @@ instance CardanoHardForkConstraints c => BlockSupportsHFLedgerQuery (CardanoEras
   answerBlockQueryHFTraverse (IS (IS (IS (IS (IS (IS (IS idx))))))) _cfg _q _dlv =
     case idx of {}
 
-  -- TODO: remove the indrection through the NS
   queryLedgerGetTraversingFilter IZ (q :: BlockQuery ByronBlock QFTraverseTables result) = case q of {}
   queryLedgerGetTraversingFilter idx@(IS IZ) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
   queryLedgerGetTraversingFilter idx@(IS (IS IZ)) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
   queryLedgerGetTraversingFilter idx@(IS (IS (IS IZ))) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
   queryLedgerGetTraversingFilter idx@(IS (IS (IS (IS IZ)))) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
   queryLedgerGetTraversingFilter idx@(IS (IS (IS (IS (IS IZ))))) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
   queryLedgerGetTraversingFilter idx@(IS (IS (IS (IS (IS (IS IZ)))))) q = case q of
-    GetUTxOByAddress addrs -> \txout -> case toNS txout of
-      S (Z (WrapTxOut x)) -> filterGetUTxOByAddressOne addrs x
-      S (S (Z (WrapTxOut x))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (Z (WrapTxOut x)))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (Z (WrapTxOut x))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (Z (WrapTxOut x)))))) -> filterGetUTxOByAddressOne addrs x
-      S (S (S (S (S (S (Z (WrapTxOut x))))))) -> filterGetUTxOByAddressOne addrs x
+    GetUTxOByAddress addrs -> \case
+      ShelleyTxOut x -> filterGetUTxOByAddressOne addrs x
+      AllegraTxOut x -> filterGetUTxOByAddressOne addrs x
+      MaryTxOut x -> filterGetUTxOByAddressOne addrs x
+      AlonzoTxOut x -> filterGetUTxOByAddressOne addrs x
+      BabbageTxOut x -> filterGetUTxOByAddressOne addrs x
+      ConwayTxOut x -> filterGetUTxOByAddressOne addrs x
     GetUTxOWhole ->
       const True
     GetCBOR q' -> queryLedgerGetTraversingFilter idx q'
