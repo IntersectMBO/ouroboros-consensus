4706d20899ff1e07a530168e68f09b96a66b9427
Alexander Esgen <alexander.esgen@iohk.io>
2024-06-24 20:09:05 +0200

    n2n-pg: init

---
+++ ouroboros-consensus-cardano/app/n2n-pg.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE ApplicativeDo  #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Main (main) where
+
+import           Cardano.Crypto.Init (cryptoInit)
+import           Cardano.Tools.N2NPG.Run (Opts (..))
+import qualified Cardano.Tools.N2NPG.Run as N2NPG
+import           Main.Utf8 (withStdTerminalHandles)
+import qualified Network.Socket as Socket
+import           Options.Applicative
+import           Ouroboros.Consensus.Block
+
+main :: IO ()
+main = withStdTerminalHandles $ do
+    cryptoInit
+    N2NPG.run =<< execParser optsParser
+
+optsParser :: ParserInfo Opts
+optsParser =
+    info (helper <*> parse) $ fullDesc <> progDesc desc
+  where
+    desc = "Download headers and blocks via the N2N protocol"
+
+    parse = do
+      configFile <- strOption $ mconcat
+        [ long "config"
+        , help "Path to config file, in the same format as for the node or db-analyser"
+        , metavar "PATH"
+        ]
+      immutableDBDir <- strOption $ mconcat
+        [ long "db"
+        , help "Path to the ImmutableDB, only used for hash lookups"
+        , metavar "PATH"
+        ]
+      -- TODO don't hardcode
+      let serverAddr = Socket.SockAddrInet 3001 ip
+            where
+              -- some IOG relay
+              ip = Socket.tupleToHostAddress (18, 136, 216, 144)
+      startSlot <- fmap withOriginFromMaybe $ optional $ option (SlotNo <$> auto) $ mconcat
+        [ long "start-from"
+        , metavar "SLOT_NUMBER"
+        , help "Start downloading from this slot (must be in the ImmutableDB)"
+        ]
+      numBlocks <- option auto $ mconcat
+        [ long "num-blocks"
+        , metavar "NUM_BLOCKS"
+        , help "Number of blocks to download"
+        ]
+      pure Opts {
+          configFile
+        , immutableDBDir
+        , serverAddr
+        , startSlot
+        , numBlocks
+        }
