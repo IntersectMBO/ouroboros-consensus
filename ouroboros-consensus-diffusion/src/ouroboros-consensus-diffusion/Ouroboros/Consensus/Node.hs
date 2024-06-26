896a52d101cc2eefdf60bcb210d2a63520c2f78d
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-06-26 15:47:13 +0200

    Accomodate for changes to BlockFetch
    * Addition of ChainSyncClientHandleCollection, grace period, and starvation event in BlockFetch
    * Plug `rotateDynamo` into `BlockFetchConsensusInterface`
    * Removal of `bfcMaxConcurrencyBulkSync`
    * Changes in blockfetch decision tracing

--- ouroboros-consensus-diffusion/src/ouroboros-consensus-diffusion/Ouroboros/Consensus/Node.hs
+++ ouroboros-consensus-diffusion/src/ouroboros-consensus-diffusion/Ouroboros/Consensus/Node.hs
@@ -317,8 +317,7 @@ nonImmutableDbPath (MultipleDbPaths _ vol) = vol
 --
 -- See 'stdLowLevelRunNodeArgsIO'.
 data StdRunNodeArgs m blk (p2p :: Diffusion.P2P) = StdRunNodeArgs
-  { srnBfcMaxConcurrencyBulkSync    :: Maybe Word
-  , srnBfcMaxConcurrencyDeadline    :: Maybe Word
+  { srnBfcMaxConcurrencyDeadline    :: Maybe Word
   , srnChainDbValidateOverride      :: Bool
     -- ^ If @True@, validate the ChainDB on init no matter what
   , srnDiskPolicyArgs               :: DiskPolicyArgs
@@ -982,9 +981,6 @@ stdLowLevelRunNodeArgsIO RunNodeArgs{ rnProtocolInfo
             maybe id
               (\mc bfc -> bfc { bfcMaxConcurrencyDeadline = mc })
               srnBfcMaxConcurrencyDeadline
-          . maybe id
-              (\mc bfc -> bfc { bfcMaxConcurrencyBulkSync = mc })
-              srnBfcMaxConcurrencyBulkSync
         modifyMempoolCapacityOverride =
             maybe id
               (\mc nka -> nka { mempoolCapacityOverride = mc })
