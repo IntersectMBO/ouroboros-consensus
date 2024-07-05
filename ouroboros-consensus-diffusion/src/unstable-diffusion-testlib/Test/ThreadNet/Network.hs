0b1e973837583c270b199ad3ea8eeb74bae87447
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-07-05 14:29:48 +0200

    Follow configurable grace period

--- ouroboros-consensus-diffusion/src/unstable-diffusion-testlib/Test/ThreadNet/Network.hs
+++ ouroboros-consensus-diffusion/src/unstable-diffusion-testlib/Test/ThreadNet/Network.hs
@@ -1021,6 +1021,7 @@ runThreadNetwork systemTime ThreadNetworkArgs
                                                   -- interval which doesn't play nice with
                                                   -- blockfetch descision interval.
                 , bfcSalt                   = 0
+                , bfcBulkSyncGracePeriod    = 10 -- seconds
                 }
             , gsmArgs                 = GSM.GsmNodeKernelArgs {
                   gsmAntiThunderingHerd  = kaRng
