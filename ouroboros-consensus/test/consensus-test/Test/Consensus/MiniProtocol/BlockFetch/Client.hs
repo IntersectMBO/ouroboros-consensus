affbc03ceb1fa4f54101c971d645ecab8257c580
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-07-11 19:00:37 +0000

    Accomodate for the addition of ChainSyncClientHandleCollection and grace period and starvation event in BlockFetch

--- ouroboros-consensus/test/consensus-test/Test/Consensus/MiniProtocol/BlockFetch/Client.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/MiniProtocol/BlockFetch/Client.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeApplications           #-}
 
 -- | A test for the consensus-specific parts of the BlockFetch client.
 --
@@ -52,7 +53,7 @@ import           Ouroboros.Consensus.Util.STM (blockUntilJust,
 import           Ouroboros.Network.AnchoredFragment (AnchoredFragment)
 import qualified Ouroboros.Network.AnchoredFragment as AF
 import           Ouroboros.Network.BlockFetch (BlockFetchConfiguration (..), GenesisBlockFetchConfiguration (..),
-                     BlockFetchConsensusInterface, FetchMode (..),
+                     BlockFetchConsensusInterface (..), FetchMode (..),
                      blockFetchLogic, bracketFetchClient,
                      bracketKeepAliveClient, bracketSyncWithFetchClient,
                      newFetchClientRegistry)
@@ -257,10 +258,11 @@ runBlockFetchTest BlockFetchClientTestSetup{..} = withRegistry \registry -> do
 
         let -- Always return the empty chain such that the BlockFetch logic
             -- downloads all chains.
-            getCurrentChain           = pure $ AF.Empty AF.AnchorGenesis
-            getIsFetched              = ChainDB.getIsFetched chainDB
-            getMaxSlotNo              = ChainDB.getMaxSlotNo chainDB
-            addBlockWaitWrittenToDisk = ChainDB.addBlockWaitWrittenToDisk chainDB
+            getCurrentChain       = pure $ AF.Empty AF.AnchorGenesis
+            getIsFetched          = ChainDB.getIsFetched chainDB
+            getMaxSlotNo          = ChainDB.getMaxSlotNo chainDB
+            addBlockAsync         = ChainDB.addBlockAsync chainDB
+            getChainSelStarvation = ChainDB.getChainSelStarvation chainDB
         pure BlockFetchClientInterface.ChainDbView {..}
       where
         -- Needs to be larger than any chain length in this test, to ensure that
@@ -279,14 +281,17 @@ runBlockFetchTest BlockFetchClientTestSetup{..} = withRegistry \registry -> do
       -> BlockFetchClientInterface.ChainDbView m TestBlock
       -> BlockFetchConsensusInterface PeerId (Header TestBlock) TestBlock m
     mkTestBlockFetchConsensusInterface getCandidates chainDbView =
-        BlockFetchClientInterface.mkBlockFetchConsensusInterface
+        (BlockFetchClientInterface.mkBlockFetchConsensusInterface @m @PeerId
           nullTracer
           (TestBlockConfig numCoreNodes)
           chainDbView
-          getCandidates
+          (error "ChainSyncClientHandleCollection not provided to mkBlockFetchConsensusInterface")
           (\_hdr -> 1000) -- header size, only used for peer prioritization
           slotForgeTime
-          (pure blockFetchMode)
+          (pure blockFetchMode))
+            { readCandidateChains = getCandidates
+            , demoteCSJDynamo = const (pure ())
+            }
       where
         -- Bogus implementation; this is fine as this is only used for
         -- enriching tracing information ATM.
@@ -360,6 +365,7 @@ instance Arbitrary BlockFetchClientTestSetup where
             -- logic iterations in case the monitored state vars change too
             -- fast, which we don't have to worry about in this test.
             bfcDecisionLoopInterval   = 0
+            bfcBulkSyncGracePeriod = 10
         bfcMaxRequestsInflight <- chooseEnum (2, 10)
         bfcSalt                <- arbitrary
         gbfcBulkSyncGracePeriod <- fromIntegral <$> chooseInteger (5, 60)
