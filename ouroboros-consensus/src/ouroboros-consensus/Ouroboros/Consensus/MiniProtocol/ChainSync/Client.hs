65c17d17941aa64118dc39480390753e327735b2
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2024-07-16 19:04:29 +0200

    Update Genesis configuration
    * Move Genesis-specific BlockFetch config to GenesisConfig
    * Introduce GenesisConfigFlags for interaction with config files/CLI
    * Add missing instances for Genesis configuration

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ChainSync/Client.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ChainSync/Client.hs
@@ -167,7 +167,7 @@ data ChainSyncLoPBucketEnabledConfig = ChainSyncLoPBucketEnabledConfig {
     csbcCapacity :: Integer,
     -- | The rate of the bucket (think tokens per second).
     csbcRate     :: Rational
-  }
+  } deriving stock (Eq, Generic, Show)
 
 -- | Configuration of the leaky bucket.
 data ChainSyncLoPBucketConfig
@@ -178,6 +178,7 @@ data ChainSyncLoPBucketConfig
   |
     -- | Enable the leaky bucket.
     ChainSyncLoPBucketEnabled ChainSyncLoPBucketEnabledConfig
+  deriving stock (Eq, Generic, Show)
 
 -- | Configuration of ChainSync Jumping
 data CSJConfig
@@ -188,6 +189,7 @@ data CSJConfig
   |
     -- | Enable ChainSync Jumping
     CSJEnabled CSJEnabledConfig
+  deriving stock (Eq, Generic, Show)
 
 newtype CSJEnabledConfig = CSJEnabledConfig {
   -- | The _ideal_ size for ChainSync jumps. Note that the algorithm
@@ -207,7 +209,7 @@ newtype CSJEnabledConfig = CSJEnabledConfig {
   -- window has a higher change that dishonest peers can delay syncing by a
   -- small margin (around 2 minutes per dishonest peer with mainnet parameters).
   csjcJumpSize       :: SlotNo
-}
+} deriving stock (Eq, Generic, Show)
 
 defaultChainDbView ::
      (IOLike m, LedgerSupportsProtocol blk)
