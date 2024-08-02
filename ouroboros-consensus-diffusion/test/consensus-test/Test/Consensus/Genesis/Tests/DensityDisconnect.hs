b9fc000102db6e4f135034eb5fb87494a19a40bd
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-08-02 21:53:45 +0000

    Expect CandidateTooSparse in gdd tests

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/DensityDisconnect.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/DensityDisconnect.hs
@@ -31,7 +31,7 @@ import           Ouroboros.Consensus.Config.SecurityParam
 import           Ouroboros.Consensus.Genesis.Governor (DensityBounds,
                      densityDisconnect, sharedCandidatePrefix)
 import           Ouroboros.Consensus.MiniProtocol.ChainSync.Client
-                     (ChainSyncClientException (DensityTooLow),
+                     (ChainSyncClientException (..),
                      ChainSyncState (..))
 import           Ouroboros.Consensus.Util.Condense (condense)
 import           Ouroboros.Network.AnchoredFragment (AnchoredFragment)
@@ -475,6 +475,7 @@ prop_densityDisconnectTriggersChainSel =
           othersCount = Map.size (adversarialPeers $ psSchedule gtSchedule)
           exnCorrect = case exceptionsByComponent ChainSyncClient stateView of
             [fromException -> Just DensityTooLow] -> True
+            [fromException -> Just CandidateTooSparse{}] -> True
             []                 | othersCount == 0 -> True
             _                                     -> False
           tipPointCorrect = Just (getTrunkTip gtBlockTree) == svTipBlock
