c862d346631fc2d20dc791396975279370d4a1af
Alexander Esgen <alexander.esgen@iohk.io>
2024-08-02 08:31:05 +0200

    db-truncater: make `--truncate-after-slot` more lenient again
    This is convenient eg for truncating all blocks after an epoch without having to
    find out the exact slot of the last block in the epoch just before.

--- ouroboros-consensus-cardano/src/unstable-cardano-tools/Cardano/Tools/DBTruncater/Types.hs
+++ ouroboros-consensus-cardano/src/unstable-cardano-tools/Cardano/Tools/DBTruncater/Types.hs
@@ -13,10 +13,8 @@ data DBTruncaterConfig = DBTruncaterConfig {
 
 -- | Where to truncate the ImmutableDB.
 data TruncateAfter
-    -- | Truncate after the given slot number, such that the new tip has this
-    -- exact slot number. Fail if this is not possible, ie no block has this
-    -- slot number. If there are two blocks with the same slot number (due to
-    -- EBBs), the tip will be the non-EBB.
+    -- | Truncate after the given slot number, deleting all blocks with a higher
+    -- slot number.
   = TruncateAfterSlot SlotNo
     -- | Truncate after the given block number (such that the new tip has this
     -- block number).
