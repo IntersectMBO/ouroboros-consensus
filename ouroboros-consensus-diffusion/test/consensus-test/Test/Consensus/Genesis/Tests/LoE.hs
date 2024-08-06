17ca1f77671f477e8a3a4abdf2ea4f0ed2896d0c
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-08-06 16:53:31 +0200

    Document all tests that did not have documentation

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/LoE.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/LoE.hs
@@ -41,7 +41,14 @@ tests =
     ]
 
 -- | Tests that the selection advances in presence of the LoE when a peer is
--- killed by something that is not LoE-aware, eg. the timeouts.
+-- killed by something that is not LoE-aware, eg. the timeouts. This test
+-- features an honest peer behaving normally and an adversarial peer behaving
+-- such that it will get killed by timeouts. We check that, after the adversary
+-- gets disconnected, the LoE gets updated to stop taking it into account. There
+-- are two variants of the test: one with timeouts enabled, and one without. In
+-- the case where timeouts are disabled, we check that we do in fact remain
+-- stuck at the intersection between trunk and other chain.
+--
 -- NOTE: Same as 'LoP.prop_delayAttack' with timeouts instead of LoP.
 prop_adversaryHitsTimeouts :: Bool -> Property
 prop_adversaryHitsTimeouts timeoutsEnabled =
