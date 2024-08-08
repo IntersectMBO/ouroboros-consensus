2470b114b6077d3dc49e212521b13294ca412f1a
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-08-08 17:42:50 +0200

    Document prop_blockFetchLeashingAttack

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/Uniform.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/Tests/Uniform.hs
@@ -421,6 +421,11 @@ prop_downtime = forAllGenesisTest
       , pgpDowntime = DowntimeWithSecurityParam (gtSecurityParam gt)
       }
 
+-- | Test that the block fetch leashing attack does not delay the immutable tip.
+-- This leashing attack consists in having adversarial peers that behave
+-- honestly when it comes to ChainSync but refuse to send blocks. A proper node
+-- under test should detect those behaviours as adversarial and find a way to
+-- make progress.
 prop_blockFetchLeashingAttack :: Property
 prop_blockFetchLeashingAttack =
   forAllGenesisTest
@@ -435,6 +440,9 @@ prop_blockFetchLeashingAttack =
   where
     genBlockFetchLeashingSchedule :: GenesisTest TestBlock () -> QC.Gen (PointSchedule TestBlock)
     genBlockFetchLeashingSchedule genesisTest = do
+      -- A schedule with several honest peers and no adversaries. We will then
+      -- keep one of those as honest and remove the block points from the
+      -- others, hence producing one honest peer and several adversaries.
       PointSchedule {psSchedule} <-
         stToGen $
           uniformPoints
