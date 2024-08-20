ddab705b092525af085288af618e0cee6733a61e
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-08-20 21:00:13 +0000

    Update configuration after recovering BulkSync in ouroboros-network

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/BlockFetch/ClientInterface.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/BlockFetch/ClientInterface.hs
@@ -46,7 +46,8 @@ import           Ouroboros.Network.Block (MaxSlotNo)
 import           Ouroboros.Network.BlockFetch.ConsensusInterface
                      (BlockFetchConsensusInterface (..),
                      ChainSelStarvation (..), FetchMode (..),
-                     FromConsensus (..))
+                     FromConsensus (..), PraosFetchMode (..), mkReadFetchMode)
+import           Ouroboros.Network.ConsensusMode (ConsensusMode)
 import           Ouroboros.Network.PeerSelection.Bootstrap (UseBootstrapPeers,
                      requiresBootstrapPeers)
 import           Ouroboros.Network.PeerSelection.LedgerPeers.Type
@@ -142,37 +143,41 @@ initSlotForgeTimeOracle cfg chainDB = do
 
 readFetchModeDefault ::
      (MonadSTM m, HasHeader blk)
-  => BlockchainTime m
+  => ConsensusMode
+  -> BlockchainTime m
   -> STM m (AnchoredFragment blk)
   -> STM m UseBootstrapPeers
   -> STM m LedgerStateJudgement
   -> STM m FetchMode
-readFetchModeDefault btime getCurrentChain
-                     getUseBootstrapPeers getLedgerStateJudgement = do
-    mCurSlot <- getCurrentSlot btime
-    usingBootstrapPeers <- requiresBootstrapPeers <$> getUseBootstrapPeers
-                                                  <*> getLedgerStateJudgement
+readFetchModeDefault consensusMode btime getCurrentChain
+                     getUseBootstrapPeers getLedgerStateJudgement =
+    mkReadFetchMode consensusMode getLedgerStateJudgement praosFetchMode
+  where
+    praosFetchMode = do
+      mCurSlot <- getCurrentSlot btime
+      usingBootstrapPeers <- requiresBootstrapPeers <$> getUseBootstrapPeers
+                                                    <*> getLedgerStateJudgement
 
-    -- This logic means that when the node is using bootstrap peers and is in
-    -- TooOld state it will always return BulkSync. Otherwise if the node
-    -- isn't using bootstrap peers (i.e. has them disabled it will use the old
-    -- logic of returning BulkSync if behind 1000 slots
-    case (usingBootstrapPeers, mCurSlot) of
-      (True, _)                    -> return FetchModeBulkSync
-      (False, CurrentSlotUnknown)  -> return FetchModeBulkSync
-      (False, CurrentSlot curSlot) -> do
-        curChainSlot <- AF.headSlot <$> getCurrentChain
-        let slotsBehind = case curChainSlot of
-              -- There's nothing in the chain. If the current slot is 0, then
-              -- we're 1 slot behind.
-              Origin         -> unSlotNo curSlot + 1
-              NotOrigin slot -> unSlotNo curSlot - unSlotNo slot
-            maxSlotsBehind = 1000
-        return $ if slotsBehind < maxSlotsBehind
-          -- When the current chain is near to "now", use deadline mode,
-          -- when it is far away, use bulk sync mode.
-          then FetchModeDeadline
-          else FetchModeBulkSync
+      -- This logic means that when the node is using bootstrap peers and is in
+      -- TooOld state it will always return BulkSync. Otherwise if the node
+      -- isn't using bootstrap peers (i.e. has them disabled it will use the old
+      -- logic of returning BulkSync if behind 1000 slots
+      case (usingBootstrapPeers, mCurSlot) of
+        (True, _)                    -> return FetchModeBulkSync
+        (False, CurrentSlotUnknown)  -> return FetchModeBulkSync
+        (False, CurrentSlot curSlot) -> do
+          curChainSlot <- AF.headSlot <$> getCurrentChain
+          let slotsBehind = case curChainSlot of
+                -- There's nothing in the chain. If the current slot is 0, then
+                -- we're 1 slot behind.
+                Origin         -> unSlotNo curSlot + 1
+                NotOrigin slot -> unSlotNo curSlot - unSlotNo slot
+              maxSlotsBehind = 1000
+          return $ if slotsBehind < maxSlotsBehind
+            -- When the current chain is near to "now", use deadline mode,
+            -- when it is far away, use bulk sync mode.
+            then FetchModeDeadline
+            else FetchModeBulkSync
 
 mkBlockFetchConsensusInterface ::
      forall m peer blk.
--- ouroboros-consensus/test/consensus-test/Test/Consensus/MiniProtocol/BlockFetch/Client.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/MiniProtocol/BlockFetch/Client.hs
@@ -57,6 +57,8 @@ import           Ouroboros.Network.BlockFetch (BlockFetchConfiguration (..),
                      bracketFetchClient, bracketKeepAliveClient,
                      bracketSyncWithFetchClient, newFetchClientRegistry)
 import           Ouroboros.Network.BlockFetch.Client (blockFetchClient)
+import           Ouroboros.Network.BlockFetch.ConsensusInterface
+                     (PraosFetchMode (..))
 import           Ouroboros.Network.ControlMessage (ControlMessage (..))
 import           Ouroboros.Network.Mock.Chain (Chain)
 import qualified Ouroboros.Network.Mock.Chain as Chain
@@ -97,8 +99,9 @@ prop_blockFetch bfcts@BlockFetchClientTestSetup{..} =
       [ Map.keysSet bfcoBlockFetchResults === Map.keysSet peerUpdates
       , counterexample ("Fetched blocks per peer: " <> condense bfcoFetchedBlocks) $
         property $ case blockFetchMode of
-          FetchModeDeadline -> all (> 0) bfcoFetchedBlocks
-          FetchModeBulkSync -> any (> 0) bfcoFetchedBlocks
+          PraosFetchMode FetchModeDeadline -> all (> 0) bfcoFetchedBlocks
+          PraosFetchMode FetchModeBulkSync -> all (> 0) bfcoFetchedBlocks
+          FetchModeGenesis                 -> any (> 0) bfcoFetchedBlocks
       ]
   where
     BlockFetchClientOutcome{..} = runSimOrThrow $ runBlockFetchTest bfcts
@@ -361,18 +364,23 @@ instance Arbitrary BlockFetchClientTestSetup where
       peerUpdates <-
             Map.fromList . zip peerIds
         <$> replicateM numPeers (genUpdateSchedule blockFetchPipelining)
-      blockFetchMode <- elements [FetchModeBulkSync, FetchModeDeadline]
+      blockFetchMode <- elements
+        [ PraosFetchMode FetchModeBulkSync
+        , PraosFetchMode FetchModeDeadline
+        , FetchModeGenesis
+        ]
       blockFetchCfg  <- do
         let -- ensure that we can download blocks from all peers
+            bfcMaxConcurrencyBulkSync = fromIntegral numPeers
             bfcMaxConcurrencyDeadline = fromIntegral numPeers
             -- This is used to introduce a minimal delay between BlockFetch
             -- logic iterations in case the monitored state vars change too
             -- fast, which we don't have to worry about in this test.
-            bfcDecisionLoopIntervalBulkSync = 0
-            bfcDecisionLoopIntervalDeadline = 0
+            bfcDecisionLoopIntervalGenesis = 0
+            bfcDecisionLoopIntervalPraos = 0
         bfcMaxRequestsInflight <- chooseEnum (2, 10)
         bfcSalt                <- arbitrary
-        gbfcBulkSyncGracePeriod <- fromIntegral <$> chooseInteger (5, 60)
+        gbfcGracePeriod <- fromIntegral <$> chooseInteger (5, 60)
         let bfcGenesisBFConfig = GenesisBlockFetchConfiguration {..}
         pure BlockFetchConfiguration {..}
       pure BlockFetchClientTestSetup {..}
