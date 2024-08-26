b67443dd82021e0c9164de99f8ca3809ee188e30
Nicolas Frisby <nick.frisby@iohk.io>
2024-08-26 13:43:54 -0700

    TOSQUASH fixup some Haddock

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/TxSeq.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/TxSeq.hs
@@ -202,10 +202,10 @@ splitAfterTicketNo (TxSeq txs) n =
     case FingerTree.split (\m -> mMaxTicket m > n) txs of
       (l, r) -> (TxSeq l, TxSeq r)
 
--- | \( O(\log(n)) \). Split the sequence of transactions into two parts
--- based on the given @sz@. The first part has transactions whose
--- summed '' is less than or equal to the given 'TxSizeInBytes',
--- and the second part has the remaining transactions in the sequence.
+-- | \( O(\log(n)) \). Split the sequence of transactions into two parts based
+-- on the given @sz@. The first part has transactions whose summed @sz@ is less
+-- than or equal to the given @sz@, and the second part has the remaining
+-- transactions in the sequence.
 --
 splitAfterTxSize ::
      Measure sz
