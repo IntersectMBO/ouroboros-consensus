98314004076fda31a2502e1899b27f3ea5e631a0
Marcin Szamotulski <coot@coot.me>
2024-09-25 11:55:33 +0200

    Updated to use typed-protocols-0.3.0.0

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ChainSync/Client.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ChainSync/Client.hs
@@ -88,7 +88,7 @@ import           Data.Typeable
 import           Data.Word (Word64)
 import           GHC.Generics (Generic)
 import           GHC.Stack (HasCallStack)
-import           Network.TypedProtocol.Pipelined
+import           Network.TypedProtocol.Core
 import           NoThunks.Class (unsafeNoThunks)
 import           Ouroboros.Consensus.Block
 import           Ouroboros.Consensus.BlockchainTime (RelativeTime)
