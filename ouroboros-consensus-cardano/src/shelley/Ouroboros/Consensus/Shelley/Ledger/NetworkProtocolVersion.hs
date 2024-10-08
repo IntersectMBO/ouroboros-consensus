3568dece3dd9ab8d2c86717554e213cb35b36cf5
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2024-10-08 19:30:09 +0200

    Remove support for NodeToNodeV_12 and earlier

--- ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/NetworkProtocolVersion.hs
+++ ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/NetworkProtocolVersion.hs
@@ -49,13 +49,7 @@ instance HasNetworkProtocolVersion (ShelleyBlock proto era) where
 -- TODO #2668 make this era-specific
 instance SupportedNetworkProtocolVersion (ShelleyBlock proto era) where
   supportedNodeToNodeVersions   _ = Map.fromList [
-        (NodeToNodeV_7,  ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_8,  ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_9,  ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_10, ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_11, ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_12, ShelleyNodeToNodeVersion1)
-      , (NodeToNodeV_13, ShelleyNodeToNodeVersion1)
+      (NodeToNodeV_13, ShelleyNodeToNodeVersion1)
       ]
   supportedNodeToClientVersions _ = Map.fromList [
         (NodeToClientV_9,  ShelleyNodeToClientVersion5)
@@ -67,6 +61,7 @@ instance SupportedNetworkProtocolVersion (ShelleyBlock proto era) where
       , (NodeToClientV_15, ShelleyNodeToClientVersion7)
       , (NodeToClientV_16, ShelleyNodeToClientVersion8)
       , (NodeToClientV_17, ShelleyNodeToClientVersion9)
+      -- , (NodeToClientV_18, ShelleyNodeToClientVersion10)
       ]
 
   latestReleasedNodeVersion = latestReleasedNodeVersionDefault
