9913ef54b1846f6678f4cd0bcd8984bbc42cce24
Javier Sagredo <jasataco@gmail.com>
2024-10-22 16:54:59 +0200

    Use hackage `rawlock`

--- ouroboros-consensus/test/consensus-test/Main.hs
+++ ouroboros-consensus/test/consensus-test/Main.hs
@@ -12,7 +12,6 @@ import qualified Test.Consensus.MiniProtocol.ChainSync.Client (tests)
 import qualified Test.Consensus.MiniProtocol.LocalStateQuery.Server (tests)
 import qualified Test.Consensus.ResourceRegistry (tests)
 import qualified Test.Consensus.Util.MonadSTM.NormalForm (tests)
-import qualified Test.Consensus.Util.MonadSTM.RAWLock (tests)
 import qualified Test.Consensus.Util.Versioned (tests)
 import           Test.Tasty
 import           Test.Util.TestEnv (defaultMainWithTestEnv,
@@ -32,7 +31,6 @@ tests =
   , Test.Consensus.Mempool.tests
   , Test.Consensus.Mempool.Fairness.tests
   , Test.Consensus.ResourceRegistry.tests
-  , Test.Consensus.Util.MonadSTM.RAWLock.tests
   , Test.Consensus.Util.MonadSTM.NormalForm.tests
   , Test.Consensus.Util.Versioned.tests
   , testGroup "HardFork" [
