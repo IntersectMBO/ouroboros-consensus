9302544e072f5e0f82fc4de7973de6904c67ccf9
Javier Díaz <javier.diaz@iohk.io>
2024-12-05 19:33:12 +0000

    Generate Haskell code for `UpdateNonce`

--- docs/agda-spec/src/Spec/Foreign/HSConsensus.agda
+++ docs/agda-spec/src/Spec/Foreign/HSConsensus.agda
@@ -1,4 +1,4 @@
 module Spec.Foreign.HSConsensus where
 
 open import Spec.Foreign.HSConsensus.TickNonce public
-
+open import Spec.Foreign.HSConsensus.UpdateNonce public
---
+++ docs/agda-spec/src/Spec/hs-src/test/UpdateNonceSpec.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS -Wno-orphans #-}
+module UpdateNonceSpec (spec) where
+
+import Data.Text
+
+import Test.Hspec ( Spec, describe, it )
+import Test.HUnit ( (@?=) )
+
+import Lib
+
+initEnv :: UpdateNonceEnv
+initEnv = MkUpdateNonceEnv { ueΗ = 2 }
+
+initState :: UpdateNonceState
+initState = MkUpdateNonceState { usΗv = 3, usΗc = 4 }
+
+slotBeforeWindow :: Slot
+slotBeforeWindow = 1
+
+testUpdateNonceStepWithSlotBeforeWindow :: ComputationResult Text UpdateNonceState
+testUpdateNonceStepWithSlotBeforeWindow = updnStep dummyExternalFunctions initEnv initState slotBeforeWindow
+
+{-                      window = 20
+                      ______________
+                     /              \
+                     s
+ +---+---+---+---+---+---+---+---+---+---+---+---+
+ 0  1  2 ...         99  100         119 ... 199 200
+ \___________________/   \___________________/
+        epoch 0                 epoch 1
+-}
+
+slotAfterWindow :: Slot
+slotAfterWindow = 99
+
+testUpdateNonceStepWithSlotAfterWindow :: ComputationResult Text UpdateNonceState
+testUpdateNonceStepWithSlotAfterWindow = updnStep dummyExternalFunctions initEnv initState slotAfterWindow
+
+spec :: Spec
+spec = do
+  describe "updnStep" $ do
+    it "updnStep results in the expected state with slot before window" $
+      testUpdateNonceStepWithSlotBeforeWindow @?= Success (MkUpdateNonceState { usΗv = 3 + 2, usΗc = 3 + 2 })
+    it "updnStep results in the expected state with slot after window" $
+      testUpdateNonceStepWithSlotAfterWindow @?= Success (MkUpdateNonceState { usΗv = 3 + 2, usΗc = 4 })
