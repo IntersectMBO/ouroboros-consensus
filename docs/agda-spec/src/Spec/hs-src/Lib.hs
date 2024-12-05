0be49e45e73ddca09242429215ed1097d1d7f050
Javier Díaz <javier.diaz@iohk.io>
2024-12-05 19:33:12 +0000

    Generate Haskell code for `UpdateNonce`

--- docs/agda-spec/src/Spec/hs-src/Lib.hs
+++ docs/agda-spec/src/Spec/hs-src/Lib.hs
@@ -3,9 +3,13 @@ module Lib
   , module Lib
   ) where
 
-import MAlonzo.Code.Spec.Foreign.HSTypes               as X
+import MAlonzo.Code.Spec.Foreign.HSTypes                 as X
   (ComputationResult(..)) -- TODO: Complete
-import MAlonzo.Code.Spec.Foreign.HSConsensus.TickNonce as X
+import MAlonzo.Code.Spec.Foreign.HSConsensus.TickNonce   as X
   (TickNonceEnv(..), TickNonceState(..), ticknStep)
-import MAlonzo.Code.Spec.Foreign.ExternalFunctions     as X
+import MAlonzo.Code.Spec.Foreign.HSConsensus.UpdateNonce as X
+  (UpdateNonceEnv(..), UpdateNonceState(..), updnStep)
+import MAlonzo.Code.Spec.Foreign.HSConsensus.BaseTypes   as X
+  (Slot, Epoch)
+import MAlonzo.Code.Spec.Foreign.ExternalFunctions       as X
   (ExternalFunctions(..), dummyExternalFunctions)
