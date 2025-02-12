272a782ef0664c9892cfcc108a2ad0c32d7e6406
Georgy Lukyanov <georgy.lukyanov@iohk.io>
2025-02-12 11:38:37 +0000

    Add constraint on crypto to `IsShelleyBlock`

--- ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/Block.hs
+++ ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/Block.hs
@@ -139,6 +139,7 @@ mkShelleyBlock raw = ShelleyBlock {
 class
   ( ShelleyCompatible (BlockProtocol blk) (ShelleyBlockLedgerEra blk)
   , blk ~ ShelleyBlock (BlockProtocol blk) (ShelleyBlockLedgerEra blk)
+  , ProtoCrypto (BlockProtocol blk) ~ EraCrypto (ShelleyBlockLedgerEra blk)
   ) => IsShelleyBlock blk
 
 instance ( proto ~ BlockProtocol (ShelleyBlock proto era)
