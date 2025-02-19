7088e8b4a773908de5090cbba5108dd05a16619e
David Eichmann <EichmannD@gmail.com>
2025-02-19 14:58:14 +0100

    Add roundtrip tests for LedgerConfig
    Co-authored-by: Javier Sagredo <javier.sagredo@iohk.io>

--- ouroboros-consensus-cardano/src/unstable-cardano-testlib/Test/Consensus/Cardano/Generators.hs
+++ ouroboros-consensus-cardano/src/unstable-cardano-testlib/Test/Consensus/Cardano/Generators.hs
@@ -21,20 +21,23 @@
 --
 -- We combine the Byron and Shelley-based instances defined elsewhere into
 -- Cardano instances by picking randomly from one of the eras.
-module Test.Consensus.Cardano.Generators (module Test.Consensus.Byron.Generators) where
+module Test.Consensus.Cardano.Generators () where
 
 import qualified Data.Map.Strict as Map
 import           Data.Maybe (catMaybes, fromMaybe)
 import           Data.Proxy
 import           Data.SOP.BasicFunctors
+import           Data.SOP.Counting (Exactly (..))
 import           Data.SOP.Index
 import           Data.SOP.NonEmpty
 import           Data.SOP.Sing
 import           Data.SOP.Strict
 import           Ouroboros.Consensus.Block
+import           Ouroboros.Consensus.Byron.ByronHFC
 import           Ouroboros.Consensus.Byron.Ledger
 import           Ouroboros.Consensus.Cardano.Block
-import           Ouroboros.Consensus.Cardano.Node (CardanoHardForkConstraints)
+import           Ouroboros.Consensus.Cardano.CanHardFork
+import           Ouroboros.Consensus.Cardano.Node ()
 import           Ouroboros.Consensus.HardFork.Combinator
 import           Ouroboros.Consensus.HardFork.Combinator.Serialisation
 import qualified Ouroboros.Consensus.HardFork.History as History
@@ -42,13 +45,15 @@ import           Ouroboros.Consensus.HeaderValidation
 import           Ouroboros.Consensus.Node.NetworkProtocolVersion
 import           Ouroboros.Consensus.Node.Serialisation (Some (..))
 import           Ouroboros.Consensus.Protocol.TPraos (TPraos)
+import           Ouroboros.Consensus.Shelley.HFEras ()
 import           Ouroboros.Consensus.Shelley.Ledger
 import           Ouroboros.Consensus.Shelley.Ledger.Block ()
 import           Ouroboros.Consensus.Shelley.Ledger.SupportsProtocol ()
 import           Ouroboros.Consensus.TypeFamilyWrappers
+import           Test.Cardano.Ledger.Alonzo.Arbitrary ()
 import           Test.Cardano.Ledger.Alonzo.Serialisation.Generators ()
 import           Test.Cardano.Ledger.Conway.Arbitrary ()
-import           Test.Consensus.Byron.Generators
+import           Test.Consensus.Byron.Generators ()
 import           Test.Consensus.Cardano.MockCrypto
 import           Test.Consensus.Protocol.Serialisation.Generators ()
 import           Test.Consensus.Shelley.Generators
@@ -543,19 +548,6 @@ instance Arbitrary History.EraEnd where
       , return History.EraUnbounded
       ]
 
-instance Arbitrary History.SafeZone where
-  arbitrary = oneof
-      [ History.StandardSafeZone <$> arbitrary
-      , return History.UnsafeIndefiniteSafeZone
-      ]
-
-instance Arbitrary History.EraParams where
-  arbitrary = History.EraParams
-      <$> (EpochSize <$> arbitrary)
-      <*> arbitrary
-      <*> arbitrary
-      <*> (GenesisWindow <$> arbitrary)
-
 instance Arbitrary History.EraSummary where
   arbitrary = History.EraSummary
       <$> arbitrary
@@ -705,3 +697,63 @@ instance c ~ MockCryptoCompatByron
               <$> (getHardForkEnabledNodeToClientVersion <$> arbitrary)
               <*> (SomeResult (QueryHardFork GetCurrentEra) <$> arbitrary)
           ]
+
+{------------------------------------------------------------------------------
+  Ledger Config
+------------------------------------------------------------------------------}
+
+
+-- | See 'encodeNodeToClientNP' and 'decodeNodeToClientNP'.
+instance CardanoHardForkConstraints c
+      => Arbitrary (WithVersion
+                     (HardForkNodeToClientVersion (CardanoEras c))
+                     (HardForkLedgerConfig (CardanoEras c))
+                   ) where
+  arbitrary = WithVersion
+      -- Use a version that enables all eras. We assume that all eras are
+      -- enabled in the maximum supported version.
+      (snd $ fromMaybe err $ Map.lookupMax $ supportedNodeToClientVersions (Proxy @(CardanoBlock c)))
+      <$> arbitrary
+    where
+      err = error "Expected at least 1 supported note-to-client version, but `supportedNodeToClientVersions` has none"
+
+instance CardanoHardForkConstraints c
+      => Arbitrary (HardForkLedgerConfig (CardanoEras c)) where
+  arbitrary = HardForkLedgerConfig <$> arbitrary <*> arbitrary
+
+instance SListI xs => Arbitrary (History.Shape xs) where
+  arbitrary = History.Shape . Exactly <$> hsequenceK (hpure (K arbitrary))
+
+instance (CardanoHardForkConstraints c)
+      => Arbitrary (PerEraLedgerConfig (CardanoEras c)) where
+  arbitrary = do
+    byronPLC   <- WrapPartialLedgerConfig <$> arbitrary
+    shelleyPLC <- WrapPartialLedgerConfig <$> arbitrary
+    allegraPLC <- WrapPartialLedgerConfig <$> arbitrary
+    maryPLC    <- WrapPartialLedgerConfig <$> arbitrary
+    alonzoPLC  <- WrapPartialLedgerConfig <$> arbitrary
+    babbagePLC <- WrapPartialLedgerConfig <$> arbitrary
+    conwayPLC  <- WrapPartialLedgerConfig <$> arbitrary
+    return $ PerEraLedgerConfig $
+      byronPLC
+      :* shelleyPLC
+      :* allegraPLC
+      :* maryPLC
+      :* alonzoPLC
+      :* babbagePLC
+      :* conwayPLC
+      :* Nil
+
+instance Arbitrary ByronPartialLedgerConfig where
+  arbitrary = ByronPartialLedgerConfig <$> arbitrary <*> arbitrary
+
+instance Arbitrary (ShelleyLedgerConfig era)
+      => Arbitrary (ShelleyPartialLedgerConfig era) where
+  arbitrary = ShelleyPartialLedgerConfig <$> arbitrary <*> arbitrary
+
+instance Arbitrary TriggerHardFork where
+  arbitrary = oneof [
+      TriggerHardForkAtVersion <$> arbitrary
+    , TriggerHardForkAtEpoch <$> arbitrary
+    , pure TriggerHardForkNotDuringThisExecution
+    ]
