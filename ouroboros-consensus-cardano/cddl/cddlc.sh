91afed36fed170f606a2ffa667827928b7f85e36
Javier Sagredo <javier.sagredo@iohk.io>
2025-03-20 13:14:39 +0100

    Split files into different messages

--- ouroboros-consensus-cardano/cddl/cddlc.sh
+++ ouroboros-consensus-cardano/cddl/cddlc.sh
@@ -1,8 +1,17 @@
 export CDDL_INCLUDE_PATH=ledger-cddls/eras/byron/cddl-spec:ledger-cddls/eras/shelley/impl/cddl-files:ledger-cddls/eras/allegra/impl/cddl-files:ledger-cddls/eras/mary/impl/cddl-files:ledger-cddls/eras/alonzo/impl/cddl-files:ledger-cddls/eras/babbage/impl/cddl-files:ledger-cddls/eras/conway/impl/cddl-files:disk:.:
 
-cddlc -u2tcddl disk/block.cddl > block.compiled.cddl
-cddlc -u2tcddl disk/ledger-state-snapshot.cddl > ledger-state-snapshot.compiled.cddl
-cddlc -u2tcddl node-to-node/blockfetch.cddl > blockfetch.compiled.cddl
-cddlc -u2tcddl node-to-node/txsubmission2.cddl > txsubmission2.compiled.cddl
-cddlc -u2tcddl node-to-node/chainsync.cddl > chainsync.compiled.cddl
-cddlc -u2tcddl node-to-client/localstatequery.cddl > localstatequery.compiled.cddl
+mkdir -p out
+
+gen () {
+    echo -n -e "\t- $1"
+    mkdir -p out/$(echo $1 | rev | cut -d'/' -f2- | rev)
+    cddlc -u2tcddl $1.cddl > "out/$1.compiled.cddl"
+    echo " ok"
+}
+
+IN=$(fd -e cddl -E ledger-cddls -E out -E base.cddl)
+
+echo "Generating complete CDDLs:"
+for f in $IN; do
+    gen $(echo $f | cut -d'.' -f1)
+done
