da74bd633aa29419d597fb777653f72f05116629
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-05-07 11:25:10 +0200

    Implement a first decode/encode of the api example
    This is not working as the decoder used is not including the N2C local
    state query message tag (3), while the api example is using the full
    message as sent by the client.

--- ouroboros-consensus/test/consensus-test/Test/Consensus/Ledger/Query.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/Ledger/Query.hs
@@ -1,8 +1,26 @@
+{-# LANGUAGE TypeApplications #-}
+
 -- | Serialization tests for local state queries and results.
 module Test.Consensus.Ledger.Query (tests) where
 
+import Codec.CBOR.Read (deserialiseFromBytes)
+import Codec.CBOR.Write (toLazyByteString)
+import Control.Monad.Class.MonadThrow (throwIO)
+import qualified Data.ByteString.Base16.Lazy as LBase16
+import qualified Data.ByteString.Lazy as LBS
+import Data.Function ((&))
+import Ouroboros.Consensus.Ledger.Query (
+  QueryVersion (..),
+  queryDecodeNodeToClient,
+  queryEncodeNodeToClient,
+ )
+import Ouroboros.Consensus.Mock.Node.Serialisation (MockBlock)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase)
+
+-- | We only test the latest query version against the API.
+latestVersion :: QueryVersion
+latestVersion = QueryVersion3
 
 tests :: TestTree
 tests =
@@ -12,8 +30,29 @@ tests =
       testGroup
         "getSystemStart"
         [ testCase "Query example" $ do
-            -- TODO: decode blueprint example query with queryDecodeNodeToClient
+            -- Decode blueprint example query with queryDecodeNodeToClient
+            exampleHex <- LBS.readFile "../cardano-blueprint/src/api/examples/getSystemStart/query.cbor"
+            print exampleHex
+            -- FIXME: the example in cardano-blueprint is wrapping the query in the n2c network message
+            let decoder =
+                  queryDecodeNodeToClient
+                    mockCodecConfig
+                    latestVersion
+                    mockBlockNodeToClientVersion
+            someQuery <-
+              LBase16.decodeLenient exampleHex
+                & deserialiseFromBytes decoder
+                & either throwIO (pure . snd)
+            print someQuery
             -- TODO: encode query with queryEncodeNodeToClient
+            let encoded =
+                  toLazyByteString $
+                    queryEncodeNodeToClient @(MockBlock ())
+                      mockCodecConfig
+                      latestVersion
+                      mockBlockNodeToClientVersion
+                      someQuery
+            print $ LBase16.encode encoded
             -- TODO: check whether cbor is equal
             -- TODO: validate cbor against cddl
             fail "TODO"
@@ -49,3 +88,7 @@ tests =
             fail "TODO"
         ]
     ]
+ where
+  mockCodecConfig = error "CodecConfig used unexpectedly"
+
+  mockBlockNodeToClientVersion = error "BlockNodeToClientVersion used unexpectedly"
