8e232a0d6fe7a033f56a8ca05f058060502d6268
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-05-07 10:22:32 +0200

    Draft a first set of tests for query/result serialization

--- ouroboros-consensus-cardano/test/cardano-test/Main.hs
+++ ouroboros-consensus-cardano/test/cardano-test/Main.hs
@@ -10,6 +10,7 @@ import qualified Test.Consensus.Cardano.Show ()
 import qualified Test.Consensus.Cardano.SupportedNetworkProtocolVersion
 import qualified Test.Consensus.Cardano.SupportsSanityCheck
 import qualified Test.Consensus.Cardano.Translation (tests)
+import qualified Test.Consensus.Ledger.Query (tests)
 import Test.Tasty
 import qualified Test.ThreadNet.AllegraMary
 import qualified Test.ThreadNet.Cardano
@@ -44,4 +45,5 @@ tests =
     , Test.ThreadNet.ShelleyAllegra.tests
     , Test.Consensus.Cardano.MiniProtocol.LocalTxSubmission.Server.tests
     , Test.Consensus.Cardano.Translation.tests
+    , Test.Consensus.Ledger.Query.tests
     ]
