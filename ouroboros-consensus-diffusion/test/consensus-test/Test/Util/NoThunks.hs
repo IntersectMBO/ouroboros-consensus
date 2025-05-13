a2b0cb23d65253b13b78542927773c92fdf402c7
Nicolas Frisby <nick.frisby@iohk.io>
2025-05-13 12:29:08 -0700

    TOSQUASH o-c-d:consensus-test:Test.Util.NoThunks

---
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Util/NoThunks.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Test.Util.NoThunks () where
+
+import qualified Data.Strict.Either as Strict
+import qualified Data.Strict.Maybe as Strict
+import           NoThunks.Class (NoThunks)
+
+instance NoThunks x => NoThunks (Strict.Maybe x)
+
+instance (NoThunks x, NoThunks y) => NoThunks (Strict.Either x y)
