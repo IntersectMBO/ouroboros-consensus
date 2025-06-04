3d449ed6ef4be0c97d8440ee24f5923ec5b154f5
Javier Sagredo <javier.sagredo@iohk.io>
2025-06-04 12:47:29 +0200

    Add NodeToNode CDDLs

---
+++ scripts/ci/diff-cddls.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_diff () {
+    diff "ouroboros-consensus-cardano/cddl/node-to-node/$1" "blueprints/src/network/node-to-node/$1"
+    if [ $? -ne 0 ]
+    then
+        echo "::warning ouroboros-consensus-cardano/cddl/node-to-node/$1 differs from blueprints/src/network/node-to-node/$1"
+    else
+        echo "$1 OK"
+    fi
+}
+
+check_diff "blockfetch/block.cddl"
+check_diff "chainsync/header.cddl"
+check_diff "txsubmission2/txId.cddl"
+check_diff "txsubmission2/tx.cddl"
