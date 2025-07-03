7267afdf87120a5fc35dde79765ce4f4e7c0f2dd
Alexander Esgen <alexander.esgen@iohk.io>
2025-07-03 15:58:10 +0200

    Scaffolding for Peras certs and PerasCertDB

---
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/PerasCertDB/API.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Ouroboros.Consensus.Storage.PerasCertDB.API
+  ( PerasCertDB (..)
+  , PerasWeightSnapshot (..)
+  , boostedWeightForPoint
+  , boostedWeightForFragment
+  ) where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import NoThunks.Class
+import Ouroboros.Consensus.Block
+import Ouroboros.Consensus.Util.IOLike
+import Ouroboros.Network.AnchoredFragment (AnchoredFragment)
+import qualified Ouroboros.Network.AnchoredFragment as AF
+
+data PerasCertDB m blk = PerasCertDB
+  { addCert :: PerasCert blk -> m ()
+  , getWeightSnapshot :: STM m (PerasWeightSnapshot blk)
+  , closeDB :: m ()
+  }
+  deriving NoThunks via OnlyCheckWhnfNamed "PerasCertDB" (PerasCertDB m blk)
+
+newtype PerasWeightSnapshot blk = PerasWeightSnapshot
+  { getPerasWeightSnapshot :: Map (Point blk) PerasWeight
+  }
+  deriving stock Show
+  deriving newtype NoThunks
+
+boostedWeightForPoint ::
+  forall blk.
+  StandardHash blk =>
+  PerasWeightSnapshot blk -> Point blk -> PerasWeight
+boostedWeightForPoint (PerasWeightSnapshot weightByPoint) pt =
+  Map.findWithDefault mempty pt weightByPoint
+
+boostedWeightForFragment ::
+  forall blk.
+  HasHeader blk =>
+  PerasWeightSnapshot blk ->
+  AnchoredFragment blk ->
+  PerasWeight
+boostedWeightForFragment weightSnap frag =
+  -- TODO think about whether this could be done in sublinear complexity
+  -- probably should write microbenchmarks at some point to see if this is a bottleneck
+  foldMap
+    (boostedWeightForPoint weightSnap)
+    (blockPoint <$> AF.toOldestFirst frag)
