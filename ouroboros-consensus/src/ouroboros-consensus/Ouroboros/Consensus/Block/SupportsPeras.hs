2f9b26da4aaf13423928ce5c8c3c16769de5b60a
Alexander Esgen <alexander.esgen@iohk.io>
2025-07-03 15:58:10 +0200

    Scaffolding for Peras certs and PerasCertDB

---
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Block/SupportsPeras.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Ouroboros.Consensus.Block.SupportsPeras
+  ( PerasRoundNo (..)
+  , PerasWeight (..)
+  , boostPerCert
+  , BlockSupportsPeras (..)
+  ) where
+
+import Data.Monoid (Sum (..))
+import Data.Word (Word64)
+import GHC.Generics (Generic)
+import NoThunks.Class
+import Ouroboros.Consensus.Block.Abstract
+
+newtype PerasRoundNo = PerasRoundNo {unPerasRoundNo :: Word64}
+  deriving stock Show
+  deriving newtype (Eq, Ord, NoThunks)
+
+newtype PerasWeight = PerasWeight {unPerasWeight :: Word64}
+  deriving stock Show
+  deriving newtype (Eq, Ord, NoThunks)
+  deriving (Semigroup, Monoid) via Sum Word64
+
+-- | TODO this will become a Ledger protocol parameter
+boostPerCert :: PerasWeight
+boostPerCert = PerasWeight 15
+
+class
+  NoThunks (PerasCert blk) =>
+  BlockSupportsPeras blk
+  where
+  data PerasCert blk
+
+  perasCertRound :: PerasCert blk -> PerasRoundNo
+
+  perasCertBoostedBlock :: PerasCert blk -> Point blk
+
+-- TODO degenerate instance for all blks to get things to compile
+instance StandardHash blk => BlockSupportsPeras blk where
+  data PerasCert blk = PerasCert
+    { pcCertRound :: PerasRoundNo
+    , pcCertBoostedBlock :: Point blk
+    }
+    deriving stock Generic
+    deriving anyclass NoThunks
+
+  perasCertRound = pcCertRound
+  perasCertBoostedBlock = pcCertBoostedBlock
