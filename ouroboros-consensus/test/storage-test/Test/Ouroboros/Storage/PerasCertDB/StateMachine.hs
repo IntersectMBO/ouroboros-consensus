8d4634fffb5b2f06d14095cae6b0143f2272ff6e
Alexander Esgen <alexander.esgen@iohk.io>
2025-07-08 13:01:39 +0200

    Pairing

--- ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/StateMachine.hs
+++ ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/StateMachine.hs
@@ -1,36 +1,124 @@
-
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Test.Ouroboros.Storage.PerasCertDB.StateMachine (tests) where
-import Test.Tasty (TestTree)
-import Test.Ouroboros.Storage.TestBlock (TestBlock)
-import Ouroboros.Consensus.Block.SupportsPeras
-import Test.Ouroboros.Storage.PerasCertDB.Model
+
+import Control.Monad.State
+import Control.Tracer (nullTracer)
+import qualified Data.List.NonEmpty as NE
+import Ouroboros.Consensus.Block
+import qualified Ouroboros.Consensus.Storage.PerasCertDB as PerasCertDB
+import Ouroboros.Consensus.Storage.PerasCertDB.API (PerasCertDB, PerasWeightSnapshot)
+import Ouroboros.Consensus.Util.IOLike
+import qualified Test.Ouroboros.Storage.PerasCertDB.Model as Model
+import Test.QuickCheck
+import qualified Test.QuickCheck.Monadic as QC
 import Test.QuickCheck.StateModel
-import Ouroboros.Consensus.Storage.PerasCertDB.API (PerasWeightSnapshot)
-import Ouroboros.Consensus.Storage.PerasCertDB (PerasCertDbError)
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Util.TestBlock (TestBlock, TestHash (..))
+import Test.Util.TestEnv (adjustQuickCheckTests)
 
 tests :: TestTree
-tests = undefined
+tests =
+  testGroup
+    "PerasCertDB"
+    [ adjustQuickCheckTests (* 100) $ testProperty "q-d" $ prop_qd
+    ]
+
+prop_qd :: Actions Model -> Property
+prop_qd actions = QC.monadic f $ property () <$ runActions actions
+ where
+  f :: StateT (PerasCertDB IO TestBlock) IO Property -> Property
+  f = ioProperty . flip evalStateT (error "unreachable")
 
 type Block = TestBlock
-newtype Model = Model (PerasCertDBModel Block) deriving (Show, Generic)
+newtype Model = Model (Model.Model Block) deriving (Show, Generic)
 
 instance StateModel Model where
   data Action Model a where
     OpenDB :: Action Model ()
     CloseDB :: Action Model ()
-    AddCert :: PerasCert Block -> Action Model (Either PerasCertDbError ())
-    GetWeightSnapshot :: Action Model (Either PerasCertDbError (PerasWeightSnapshot Block))
+    AddCert :: PerasCert Block -> Action Model ()
+    GetWeightSnapshot :: Action Model (PerasWeightSnapshot Block)
 
-  arbitraryAction _ _ = error "arbitraryAction not implemented"
-  initialState = error "initialState not implemented"
+  arbitraryAction _ (Model model)
+    | model.open =
+        frequency
+          [ (1, pure $ Some CloseDB)
+          , (20, Some <$> genAddCert)
+          , (20, pure $ Some GetWeightSnapshot)
+          ]
+    | otherwise = pure $ Some OpenDB
+   where
+    genAddCert = do
+      pcCertRound <- PerasRoundNo <$> arbitrary
+      pcCertBoostedBlock <- arbitrary
+      pure $ AddCert PerasCert{pcCertRound, pcCertBoostedBlock}
 
-deriving instance Show (Action Model a)
+  initialState = Model Model.initModel
+
+  nextState (Model model) action _ = Model $ case action of
+    OpenDB -> Model.openDB model
+    CloseDB -> Model.closeDB model
+    AddCert cert -> Model.addCert model cert
+    GetWeightSnapshot -> model
+
+  precondition (Model model) = \case
+    OpenDB -> not model.open
+    action ->
+      model.open && case action of
+        CloseDB -> True
+        AddCert cert -> all p model.certs
+         where
+          p cert' = perasCertRound cert /= perasCertRound cert' || cert == cert'
+        GetWeightSnapshot -> True
+
+deriving stock instance Show (Action Model a)
+deriving stock instance Eq (Action Model a)
 
 instance HasVariables (Action Model a) where
   getAllVariables _ = mempty
 
+instance RunModel Model (StateT (PerasCertDB IO TestBlock) IO) where
+  perform _ action _ = case action of
+    OpenDB -> do
+      perasCertDB <- lift $ PerasCertDB.openDB (PerasCertDB.PerasCertDbArgs nullTracer)
+      put perasCertDB
+    CloseDB -> do
+      perasCertDB <- get
+      lift $ PerasCertDB.closeDB perasCertDB
+    AddCert cert -> do
+      perasCertDB <- get
+      lift $ PerasCertDB.addCert perasCertDB cert
+    GetWeightSnapshot -> do
+      perasCertDB <- get
+      lift $ atomically $ PerasCertDB.getWeightSnapshot perasCertDB
+
+  -- TODO: check open state consistency
+  postcondition (Model model, _) GetWeightSnapshot _ actual = do
+    let expected = Model.getWeightSnapshot model
+    counterexamplePost $ "Model: " <> show expected
+    counterexamplePost $ "SUT: " <> show actual
+    pure $ expected == actual
+  postcondition _ _ _ _ = pure True
+
+-- TODO very ugly
+instance Arbitrary (Point TestBlock) where
+  arbitrary =
+    oneof
+      [ return GenesisPoint
+      , BlockPoint <$> (SlotNo <$> arbitrary) <*> (TestHash . NE.fromList . getNonEmpty <$> arbitrary)
+      ]
