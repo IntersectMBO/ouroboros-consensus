40f65d7010a49efb8c07767b718c19505e207f1b
Javier Sagredo <jasataco@gmail.com>
2025-07-17 13:26:00 +0200

    Reorganize LedgerDB args and define LSM trees arguments
    LSM trees require a Session which we don't want cardano-node to create. We
    therefore define `LedgerDbBackendArgs` which will be given by the node to us.

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/LedgerDB/V2/Args.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/LedgerDB/V2/Args.hs
@@ -1,29 +1,40 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Ouroboros.Consensus.Storage.LedgerDB.V2.Args
   ( FlavorImplSpecificTrace (..)
   , HandleArgs (..)
+  , HandleEnv (..)
   , LedgerDbFlavorArgs (..)
+  , SomeHasFSAndBlockIO (..)
+  , LSMHandleArgs (..)
   ) where
 
-import Data.Void (Void)
-import GHC.Generics
-import NoThunks.Class
+import Control.ResourceRegistry
+import Data.Typeable
+import Database.LSMTree (LSMTreeTrace (..), Salt, Session)
+import Ouroboros.Consensus.Util.Args
+import System.FS.API
+import System.FS.BlockIO.API
 
-data LedgerDbFlavorArgs f m = V2Args HandleArgs
+data LedgerDbFlavorArgs f m = V2Args (HandleArgs f m)
 
-data HandleArgs
+data HandleArgs f m
   = InMemoryHandleArgs
-  | LSMHandleArgs Void
-  deriving (Generic, NoThunks)
+  | LSMHandleArgs (LSMHandleArgs f m)
+
+data LSMHandleArgs f m = LSMArgs
+  { lsmFilePath :: HKD f FilePath
+  , lsmGenSalt :: HKD f (m Salt)
+  , lsmMkFS :: HKD f (ResourceRegistry m -> FilePath -> m (ResourceKey m, SomeHasFSAndBlockIO m))
+  }
+
+data SomeHasFSAndBlockIO m where
+  SomeHasFSAndBlockIO :: (Eq h, Typeable h) => HasFS m h -> HasBlockIO m h -> SomeHasFSAndBlockIO m
+
+data HandleEnv m
+  = InMemoryHandleEnv
+  | LSMHandleEnv (ResourceKey m, Session m) (ResourceKey m)
 
 data FlavorImplSpecificTrace
   = -- | Created a new 'LedgerTablesHandle', potentially by duplicating an
@@ -31,4 +42,5 @@ data FlavorImplSpecificTrace
     TraceLedgerTablesHandleCreate
   | -- | Closed a 'LedgerTablesHandle'.
     TraceLedgerTablesHandleClose
-  deriving (Show, Eq)
+  | LSMTrace LSMTreeTrace
+  deriving Show
