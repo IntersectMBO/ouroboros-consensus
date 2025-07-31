600b1575fc8980497d83bdaf0e3d880c23d63068
Alexander Esgen <alexander.esgen@iohk.io>
2025-07-31 21:07:44 +0200

    PerasCertDB: add simple interface for cert diffusion

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ObjectDiffusion/ObjectPool/Cert.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ObjectDiffusion/ObjectPool/Cert.hs
@@ -1,38 +1,73 @@
 {-# LANGUAGE NamedFieldPuns #-}
+
 module Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.Cert
   ( getCertPoolReaderFromCertDB
   , getCertPoolWriterFromCertDB
   , getCertPoolReaderFromChainDB
   , getCertPoolWriterFromChainDB
   ) where
-import Ouroboros.Consensus.Storage.PerasCertDB.API
-import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.API
+
+import Data.Functor ((<&>))
 import Ouroboros.Consensus.Block
-import Data.Int (Int64)
-import Ouroboros.Consensus.Storage.ChainDB.API
+import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.API
+import Ouroboros.Consensus.Storage.ChainDB.API (ChainDB)
+import qualified Ouroboros.Consensus.Storage.ChainDB.API as ChainDB
+import Ouroboros.Consensus.Storage.PerasCertDB.API
+  ( PerasCertDB
+  , PerasCertSnapshot
+  , PerasCertTicketNo
+  )
+import qualified Ouroboros.Consensus.Storage.PerasCertDB.API as PerasCertDB
+import Ouroboros.Consensus.Util.IOLike
 
-newtype CertTicketNo = TicketNo Int64 deriving (Eq, Ord, Show)
-
-
-getCertPoolReaderFromCertDB :: PerasCertDB m blk -> ObjectPoolReader PerasRoundNo (PerasCert blk) CertTicketNo m
-getCertPoolReaderFromCertDB = undefined
--- TODO: amesgen with fingertrees
-
-getCertPoolWriterFromCertDB :: (StandardHash blk, Monad m) => PerasCertDB m blk -> ObjectPoolWriter PerasRoundNo (PerasCert blk) m
-getCertPoolWriterFromCertDB PerasCertDB{addCert} =
-  ObjectPoolWriter
-    { wrGetObjectId = perasCertRound
-    , objectPoolAddObjects =
-        mapM_ addCert
+mkCertPoolReader ::
+  (IOLike m, StandardHash blk) =>
+  STM m (PerasCertSnapshot blk) ->
+  ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+mkCertPoolReader getCertSnapshot =
+  ObjectPoolReader
+    { rdrGetObjectId = perasCertRound
+    , objectPoolGetSnapshot =
+        getCertSnapshot <&> \snap ->
+          ObjectPoolSnapshot
+            { objectPoolObjectsAfter = \ticketNo ->
+                [ (cert, tno, sz)
+                | (cert, tno) <- PerasCertDB.getCertsAfter snap ticketNo
+                , let sz = 0 -- TODO
+                ]
+            , objectPoolHasObject = PerasCertDB.containsCert snap
+            }
+    , objectPoolZeroIndex = PerasCertDB.zeroPerasCertTicketNo
     }
 
-getCertPoolReaderFromChainDB :: ChainDB m blk -> ObjectPoolReader PerasRoundNo (PerasCert blk) CertTicketNo m
-getCertPoolReaderFromChainDB = undefined
+getCertPoolReaderFromCertDB ::
+  (IOLike m, StandardHash blk) =>
+  PerasCertDB m blk -> ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+getCertPoolReaderFromCertDB perasCertDB =
+  mkCertPoolReader (PerasCertDB.getCertSnapshot perasCertDB)
 
-getCertPoolWriterFromChainDB :: (StandardHash blk, Monad m) => ChainDB m blk -> ObjectPoolWriter PerasRoundNo (PerasCert blk) m
-getCertPoolWriterFromChainDB ChainDB{addPerasCert} =
+getCertPoolWriterFromCertDB ::
+  (StandardHash blk, Monad m) =>
+  PerasCertDB m blk -> ObjectPoolWriter PerasRoundNo (PerasCert blk) m
+getCertPoolWriterFromCertDB perasCertDB =
   ObjectPoolWriter
     { wrGetObjectId = perasCertRound
     , objectPoolAddObjects =
-        mapM_ addPerasCert
+        mapM_ $ PerasCertDB.addCert perasCertDB
+    }
+
+getCertPoolReaderFromChainDB ::
+  (IOLike m, StandardHash blk) =>
+  ChainDB m blk -> ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+getCertPoolReaderFromChainDB chainDB =
+  mkCertPoolReader (ChainDB.getPerasCertSnapshot chainDB)
+
+getCertPoolWriterFromChainDB ::
+  (StandardHash blk, Monad m) =>
+  ChainDB m blk -> ObjectPoolWriter PerasRoundNo (PerasCert blk) m
+getCertPoolWriterFromChainDB chainDB =
+  ObjectPoolWriter
+    { wrGetObjectId = perasCertRound
+    , objectPoolAddObjects =
+        mapM_ $ ChainDB.addPerasCert chainDB
     }
