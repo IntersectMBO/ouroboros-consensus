326297f56e4eb2149cd369d70e66565afc6ade62
Alexander Esgen <alexander.esgen@iohk.io>
2025-08-01 17:47:45 +0200

    ChainSel: make lack of trigger block in the LoE explicit
    Currently, the LoE reports the block that we triggered chain selection for here,
    but this is somewhat misleading.

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/ChainDB/Impl/ChainSel.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/ChainDB/Impl/ChainSel.hs
@@ -583,7 +583,7 @@ chainSelectionForBlock cdb@CDB{..} blockCache hdr punish = electric $ withRegist
             chainSelection chainSelEnv rr chainDiffs' >>= \case
               Just validatedChainDiff ->
                 -- Switch to the new better chain.
-                switchTo cdb p validatedChainDiff
+                switchTo cdb (Just p) validatedChainDiff
               -- No valid candidate better than our chain.
               Nothing -> noChange
           -- No candidate better than our chain.
@@ -789,7 +789,7 @@ switchTo ::
   ) =>
   ChainDbEnv m blk ->
   -- | Which block we performed chain selection for.
-  RealPoint blk ->
+  Maybe (RealPoint blk) ->
   -- | Chain and ledger to switch to
   ValidatedChainDiff (Header blk) (Forker' m blk) ->
   m ()
