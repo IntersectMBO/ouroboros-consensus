4963f4853b2e7373f08d1fe5636427dc40a5d1b3
Thomas BAGREL <thomas.bagrel@tweag.io>
2025-08-06 18:45:49 +0200

    Introduce the PerasCertDiffusion protocol (instance of ObjectDiffusion), and the associated smoke test

---
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ObjectDiffusion/PerasCert.hs
@@ -0,0 +1,36 @@
+-- | This module defines type aliases for the ObjectDiffusion protocol applied
+-- to PerasCert diffusion.
+module Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.PerasCert
+  ( TracePerasCertDiffusionInbound
+  , TracePerasCertDiffusionOutbound
+  , PerasCertPoolReader
+  , PerasCertPoolWriter
+  , PerasCertDiffusionInboundPipelined
+  , PerasCertDiffusionOutbound
+  ) where
+
+import Ouroboros.Consensus.Block
+import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.Inbound
+import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.API
+import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.Outbound
+import Ouroboros.Consensus.Storage.PerasCertDB.API
+import Ouroboros.Network.Protocol.ObjectDiffusion.Inbound (ObjectDiffusionInboundPipelined)
+import Ouroboros.Network.Protocol.ObjectDiffusion.Outbound (ObjectDiffusionOutbound)
+
+type TracePerasCertDiffusionInbound blk =
+  TraceObjectDiffusionInbound PerasRoundNo (PerasCert blk)
+
+type TracePerasCertDiffusionOutbound blk =
+  TraceObjectDiffusionOutbound PerasRoundNo (PerasCert blk)
+
+type PerasCertPoolReader blk m =
+  ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+
+type PerasCertPoolWriter blk m =
+  ObjectPoolWriter PerasRoundNo (PerasCert blk) m
+
+type PerasCertDiffusionInboundPipelined blk m a =
+  ObjectDiffusionInboundPipelined PerasRoundNo (PerasCert blk) m a
+
+type PerasCertDiffusionOutbound blk m a =
+  ObjectDiffusionOutbound PerasRoundNo (PerasCert blk) m a
