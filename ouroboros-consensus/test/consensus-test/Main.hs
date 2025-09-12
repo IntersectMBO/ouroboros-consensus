28b01cb9310d5740bc31c202f852c7208b0a2aca
Thomas BAGREL <thomas.bagrel@tweag.io>
2025-09-12 14:58:12 +0200

    Add smoke tests for PerasCertDiffusion
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Alexander Esgen <alexander.esgen@iohk.io>
    Co-authored-by: Georgy Lukyanov <georgy.lukyanov@iohk.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Nicolas "Niols" Jeannerod <nicolas.jeannerod@moduscreate.com>

--- ouroboros-consensus/test/consensus-test/Main.hs
+++ ouroboros-consensus/test/consensus-test/Main.hs
@@ -18,6 +18,7 @@ import qualified Test.Consensus.MiniProtocol.ChainSync.CSJ (tests)
 import qualified Test.Consensus.MiniProtocol.ChainSync.Client (tests)
 import qualified Test.Consensus.MiniProtocol.LocalStateQuery.Server (tests)
 import qualified Test.Consensus.Peras.Voting.Rules (tests)
+import qualified Test.Consensus.MiniProtocol.ObjectDiffusion.PerasCert.Smoke (tests)
 import qualified Test.Consensus.MiniProtocol.ObjectDiffusion.Smoke (tests)
 import qualified Test.Consensus.Peras.WeightSnapshot (tests)
 import qualified Test.Consensus.Util.MonadSTM.NormalForm (tests)
@@ -43,6 +44,7 @@ tests =
     , Test.Consensus.MiniProtocol.ChainSync.CSJ.tests
     , Test.Consensus.MiniProtocol.ChainSync.Client.tests
     , Test.Consensus.MiniProtocol.ObjectDiffusion.Smoke.tests
+    , Test.Consensus.MiniProtocol.ObjectDiffusion.PerasCert.Smoke.tests
     , Test.Consensus.MiniProtocol.LocalStateQuery.Server.tests
     , testGroup
         "Mempool"
