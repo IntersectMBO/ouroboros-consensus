62ca861dbe303cbcf522dd6ad2a2e2f8f7a3aa07
Nicolas Frisby <nick.frisby@iohk.io>
2025-10-08 14:24:36 -0700

    LeiosDemo202510: draft Leios mini protocols
    I'm intentionally grouping this code poorly and avoiding comments in an attempt
    to emphasize that this code is currently being written to enable
    hacked-together prototypes, not as a contribution towards code that might run
    on mainnet. (Even though these definitions are much more compatible with that
    development work than is other code being written for this prototype.)

---
+++ ouroboros-consensus/src/ouroboros-consensus/LeiosDemoOnlyTestFetch.hs
@@ -0,0 +1,340 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module LeiosDemoOnlyTestFetch
+  ( LeiosFetch (..)
+  , SingLeiosFetch (..)
+  , Message (..)
+  , byteLimitsLeiosFetch
+  , timeLimitsLeiosFetch
+  , codecLeiosFetch
+  , codecLeiosFetchId
+  ) where
+
+import qualified Codec.CBOR.Decoding as CBOR
+import qualified Codec.CBOR.Encoding as CBOR
+import qualified Codec.CBOR.Read as CBOR
+import Control.DeepSeq (NFData (..))
+import Control.Monad.Class.MonadST
+import Data.ByteString.Lazy (ByteString)
+import Data.Kind (Type)
+import Data.Singletons
+import Data.Word (Word16, Word64)
+import Network.TypedProtocol.Codec.CBOR
+import Network.TypedProtocol.Core
+import Ouroboros.Network.Protocol.Limits
+import Ouroboros.Network.Util.ShowProxy (ShowProxy (..))
+import Text.Printf
+
+-----
+
+type LeiosFetch :: Type -> Type -> Type -> Type
+data LeiosFetch point eb tx where
+  StIdle     :: LeiosFetch point eb tx
+  StBlock    :: LeiosFetch point eb tx
+  StBlockTxs :: LeiosFetch point eb tx
+  StDone     :: LeiosFetch point eb tx
+
+instance ( ShowProxy point
+         , ShowProxy eb
+         , ShowProxy tx
+         )
+      => ShowProxy (LeiosFetch point eb tx) where
+  showProxy _ =
+    concat
+      [ "LeiosFetch ",
+        showProxy (Proxy :: Proxy point),
+        " ",
+        showProxy (Proxy :: Proxy eb),
+        " ",
+        showProxy (Proxy :: Proxy tx)
+      ]
+
+instance ShowProxy (StIdle :: LeiosFetch point eb tx) where
+  showProxy _ = "StIdle"
+instance ShowProxy (StBlock :: LeiosFetch point eb tx) where
+  showProxy _ = "StBlock"
+instance ShowProxy (StBlockTxs :: LeiosFetch point eb tx) where
+  showProxy _ = "StBlockTxs"
+instance ShowProxy (StDone :: LeiosFetch point eb tx) where
+  showProxy _ = "StDone"
+
+type SingLeiosFetch
+  :: LeiosFetch point eb tx
+  -> Type
+data SingLeiosFetch st where
+  SingIdle     :: SingLeiosFetch StIdle
+  SingBlock    :: SingLeiosFetch StBlock
+  SingBlockTxs :: SingLeiosFetch StBlockTxs
+  SingDone     :: SingLeiosFetch StDone
+
+deriving instance Show (SingLeiosFetch st)
+
+instance StateTokenI StIdle     where stateToken = SingIdle
+instance StateTokenI StBlock    where stateToken = SingBlock
+instance StateTokenI StBlockTxs where stateToken = SingBlockTxs
+instance StateTokenI StDone     where stateToken = SingDone
+
+-----
+
+instance Protocol (LeiosFetch point eb tx) where
+  data Message (LeiosFetch point eb tx) from to where
+    MsgLeiosBlockRequest
+      :: !point
+      -> Message (LeiosFetch point eb tx) StIdle StBlock
+    MsgLeiosBlock
+      :: !eb
+      -> Message (LeiosFetch point eb tx) StBlock StIdle
+
+    MsgLeiosBlockTxsRequest
+      :: !point
+      -> [(Word16, Word64)]
+      -> Message (LeiosFetch point eb tx) StIdle StBlockTxs
+    MsgLeiosBlockTxs
+      :: ![tx]
+      -> Message (LeiosFetch point eb tx) StBlockTxs StIdle
+
+    -- vote request
+    -- vote reply
+
+    -- range request
+    -- range reply
+
+    MsgDone
+      :: Message (LeiosFetch point eb tx) StIdle StDone
+
+  type StateAgency StIdle     = ClientAgency
+  type StateAgency StBlock    = ServerAgency
+  type StateAgency StBlockTxs = ServerAgency
+  type StateAgency StDone     = NobodyAgency
+
+  type StateToken = SingLeiosFetch
+
+instance NFData (Message (LeiosFetch point eb tx) from to) where
+    rnf = \case
+      MsgLeiosBlockRequest{} -> ()
+      MsgLeiosBlock{} -> ()
+      MsgLeiosBlockTxsRequest _p bitmaps -> rnf bitmaps
+      MsgLeiosBlockTxs{} -> ()
+      -- vote request
+      -- vote reply
+      -- range request
+      -- range reply
+      MsgDone -> ()
+
+deriving instance (Eq point, Eq eb, Eq tx)
+               => Eq (Message (LeiosFetch point eb tx) from to)
+
+deriving instance (Show point, Show eb, Show tx)
+               => Show (Message (LeiosFetch point eb tx) from to)
+
+-----
+
+byteLimitsLeiosFetch
+  :: (bytes -> Word) -> ProtocolSizeLimits (LeiosFetch point eb tx) bytes
+byteLimitsLeiosFetch = ProtocolSizeLimits $ \case
+  SingIdle     -> smallByteLimit
+  SingBlock    -> largeByteLimit
+  SingBlockTxs -> largeByteLimit
+  st@SingDone  -> notActiveState st
+
+timeLimitsLeiosFetch
+  :: ProtocolTimeLimits (LeiosFetch point eb tx)
+timeLimitsLeiosFetch = ProtocolTimeLimits $ \case
+  SingIdle     -> waitForever
+  SingBlock    -> longWait
+  SingBlockTxs -> longWait
+  st@SingDone  -> notActiveState st
+
+-----
+
+codecLeiosFetch
+  :: forall (point :: Type) (eb :: Type) (tx :: Type) m.
+     (MonadST m)
+  => (point -> CBOR.Encoding)
+  -> (forall s. CBOR.Decoder s point)
+  -> (eb -> CBOR.Encoding)
+  -> (forall s. CBOR.Decoder s eb)
+  -> (tx -> CBOR.Encoding)
+  -> (forall s. CBOR.Decoder s tx)
+  -> Codec (LeiosFetch point eb tx) CBOR.DeserialiseFailure m ByteString
+codecLeiosFetch encodeP decodeP encodeEb decodeEb encodeTx decodeTx =
+  mkCodecCborLazyBS
+    (encodeLeiosFetch encodeP encodeEb encodeTx)
+    decode
+    where
+      decode
+        :: forall (st :: LeiosFetch point eb tx).
+           (ActiveState st)
+        => StateToken st
+        -> forall s. CBOR.Decoder s (SomeMessage st)
+      decode stok = do
+        len <- CBOR.decodeListLen
+        key <- CBOR.decodeWord
+        decodeLeiosFetch decodeP decodeEb decodeTx stok len key
+
+encodeLeiosFetch
+  :: forall (point :: Type) (eb :: Type) (tx :: Type)
+            (st  :: LeiosFetch point eb tx)
+            (st' :: LeiosFetch point eb tx).
+     (point -> CBOR.Encoding)
+  -> (eb -> CBOR.Encoding)
+  -> (tx -> CBOR.Encoding)
+  -> Message (LeiosFetch point eb tx) st st'
+  -> CBOR.Encoding
+encodeLeiosFetch encodeP encodeEb encodeTx = encode
+  where
+    encode
+      :: forall st0 st1.
+         Message (LeiosFetch point eb tx) st0 st1
+      -> CBOR.Encoding
+    encode = \case
+      MsgLeiosBlockRequest p ->
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 0
+        <> encodeP p
+      MsgLeiosBlock x -> 
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 1
+        <> encodeEb x
+      MsgLeiosBlockTxsRequest p bitmaps ->
+           CBOR.encodeListLen 3
+        <> CBOR.encodeWord 2
+        <> encodeP p
+        <> encodeBitmaps bitmaps
+      MsgLeiosBlockTxs txs ->
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 3
+        <> CBOR.encodeListLenIndef <> foldr (\tx r -> encodeTx tx <> r) CBOR.encodeBreak txs
+      -- vote request
+      -- vote reply
+      -- range request
+      -- range reply
+      MsgDone ->
+           CBOR.encodeListLen 1
+        <> CBOR.encodeWord 8
+
+decodeLeiosFetch
+  :: forall (point :: Type) (eb :: Type) (tx :: Type)
+            (st :: LeiosFetch point eb tx) s.
+     (ActiveState st)
+  => (forall s'. CBOR.Decoder s' point)
+  -> (forall s'. CBOR.Decoder s' eb)
+  -> (forall s'. CBOR.Decoder s' tx)
+  -> StateToken st
+  -> Int
+  -> Word
+  -> CBOR.Decoder s (SomeMessage st)
+decodeLeiosFetch decodeP decodeEb decodeTx = decode
+  where
+    decode
+      :: forall (st' :: LeiosFetch point eb tx).
+         (ActiveState st')
+      => StateToken st'
+      -> Int
+      -> Word
+      -> CBOR.Decoder s (SomeMessage st')
+    decode stok len key = do
+      case (stok, len, key) of
+        (SingIdle, 1, 0) -> do
+          p <- decodeP
+          return $ SomeMessage $ MsgLeiosBlockRequest p
+        (SingBlock, 2, 1) -> do
+          x <- decodeEb
+          return $ SomeMessage $ MsgLeiosBlock x
+        (SingIdle, 3, 2) -> do
+          p <- decodeP
+          bitmaps <- decodeBitmaps
+          return $ SomeMessage $ MsgLeiosBlockTxsRequest p bitmaps
+        (SingBlockTxs, 2, 3) -> do
+          txs <- CBOR.decodeListLenIndef *> CBOR.decodeSequenceLenIndef (flip (:)) [] reverse decodeTx
+          return $ SomeMessage $ MsgLeiosBlockTxs txs
+        -- vote request
+        -- vote reply
+        -- range request
+        -- range reply
+        (SingIdle, 1, 8) ->
+          return $ SomeMessage MsgDone
+        (SingDone, _, _) -> notActiveState stok
+        -- failures per protocol state
+        (SingIdle, _, _) ->
+          fail $ printf "codecLeiosFetch (%s) unexpected key (%d, %d)" (show stok) key len
+        (SingBlock, _, _) ->
+          fail $ printf "codecLeiosFetch (%s) unexpected key (%d, %d)" (show stok) key len
+        (SingBlockTxs, _, _) ->
+          fail $ printf "codecLeiosFetch (%s) unexpected key (%d, %d)" (show stok) key len
+
+codecLeiosFetchId
+  :: forall (point :: Type) (eb :: Type) (tx :: Type) m.
+     (Monad m)
+  => Codec
+       (LeiosFetch point eb tx)
+       CodecFailure
+       m
+       (AnyMessage (LeiosFetch point eb tx))
+codecLeiosFetchId = Codec {encode, decode}
+  where
+    encode
+      :: forall st st'.
+         ( ActiveState st
+         , StateTokenI st
+         )
+      => Message (LeiosFetch point eb tx) st st'
+      -> AnyMessage (LeiosFetch point eb tx)
+    encode = AnyMessage
+
+    decode
+      :: forall (st :: LeiosFetch point eb tx).
+         (ActiveState st)
+      => StateToken st
+      -> m (DecodeStep
+             (AnyMessage (LeiosFetch point eb tx))
+             CodecFailure
+             m
+             (SomeMessage st)
+           )
+    decode stok = return $ DecodePartial $ \bytes ->
+      return $ case (stok, bytes) of
+        (SingIdle, Just (AnyMessage msg@MsgLeiosBlockRequest{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingBlock, Just (AnyMessage msg@MsgLeiosBlock{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingIdle, Just (AnyMessage msg@MsgLeiosBlockTxsRequest{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingBlockTxs, Just (AnyMessage msg@MsgLeiosBlockTxs{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingIdle, Just (AnyMessage msg@MsgDone{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingDone, _) ->
+          notActiveState stok
+        (_, _) ->
+          DecodeFail $ CodecFailure "codecLeiosFetchId: no matching message"
+
+-----
+
+encodeBitmaps :: [(Word16, Word64)] -> CBOR.Encoding
+encodeBitmaps bitmaps =
+     CBOR.encodeMapLenIndef
+  <> foldr
+       (\(index, bitmap) r -> CBOR.encodeWord16 index <> CBOR.encodeWord64 bitmap <> r)
+       CBOR.encodeBreak
+       bitmaps
+
+decodeBitmaps :: CBOR.Decoder s [(Word16, Word64)]
+decodeBitmaps =
+     CBOR.decodeMapLenIndef
+  *> CBOR.decodeSequenceLenIndef
+       (flip (:))
+       []
+       reverse
+       ((,) <$> CBOR.decodeWord16 <*> CBOR.decodeWord64)
---
+++ ouroboros-consensus/src/ouroboros-consensus/LeiosDemoOnlyTestNotify.hs
@@ -0,0 +1,283 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module LeiosDemoOnlyTestNotify
+  ( LeiosNotify (..)
+  , SingLeiosNotify (..)
+  , Message (..)
+  , byteLimitsLeiosNotify
+  , timeLimitsLeiosNotify
+  , codecLeiosNotify
+  , codecLeiosNotifyId
+  ) where
+
+import qualified Codec.CBOR.Decoding as CBOR
+import qualified Codec.CBOR.Encoding as CBOR
+import qualified Codec.CBOR.Read as CBOR
+import Control.DeepSeq (NFData (..))
+import Control.Monad.Class.MonadST
+import Data.ByteString.Lazy (ByteString)
+import Data.Kind (Type)
+import Data.Singletons
+import Network.TypedProtocol.Codec.CBOR
+import Network.TypedProtocol.Core
+import Ouroboros.Network.Protocol.Limits
+import Ouroboros.Network.Util.ShowProxy (ShowProxy (..))
+import Text.Printf
+
+-----
+
+type LeiosNotify :: Type -> Type -> Type
+data LeiosNotify point announcement where
+  StIdle :: LeiosNotify point announcement
+  StBusy :: LeiosNotify point announcement
+  StDone :: LeiosNotify point announcement
+
+instance ( ShowProxy point
+         , ShowProxy announcement
+         )
+      => ShowProxy (LeiosNotify point announcement) where
+  showProxy _ =
+    concat
+      [ "LeiosNotify ",
+        showProxy (Proxy :: Proxy point),
+        " ",
+        showProxy (Proxy :: Proxy announcement)
+      ]
+
+instance ShowProxy (StIdle :: LeiosNotify point announcement) where
+  showProxy _ = "StIdle"
+instance ShowProxy (StBusy :: LeiosNotify point announcement) where
+  showProxy _ = "StBusy"
+instance ShowProxy (StDone :: LeiosNotify point announcement) where
+  showProxy _ = "StDone"
+
+type SingLeiosNotify
+  :: LeiosNotify point announcement
+  -> Type
+data SingLeiosNotify st where
+  SingIdle :: SingLeiosNotify StIdle
+  SingBusy :: SingLeiosNotify StBusy
+  SingDone :: SingLeiosNotify StDone
+
+deriving instance Show (SingLeiosNotify st)
+
+instance StateTokenI StIdle where stateToken = SingIdle
+instance StateTokenI StBusy where stateToken = SingBusy
+instance StateTokenI StDone where stateToken = SingDone
+
+-----
+
+instance Protocol (LeiosNotify point announcement) where
+  data Message (LeiosNotify point announcement) from to where
+    MsgLeiosNotificationRequestNext
+      :: Message (LeiosNotify point announcement) StIdle StBusy
+
+    MsgLeiosBlockAnnouncement
+      :: !announcement
+      -> Message (LeiosNotify point announcement) StBusy StIdle
+    MsgLeiosBlockOffer
+      :: !point
+      -> Message (LeiosNotify point announcement) StBusy StIdle
+    MsgLeiosBlockTxsOffer
+      :: !point
+      -> Message (LeiosNotify point announcement) StBusy StIdle
+    -- votes offer
+
+    MsgDone
+      :: Message (LeiosNotify point announcement) StIdle StDone
+
+  type StateAgency StIdle = ClientAgency
+  type StateAgency StBusy = ServerAgency
+  type StateAgency StDone = NobodyAgency
+
+  type StateToken = SingLeiosNotify
+
+instance NFData (Message (LeiosNotify point announcement) from to) where
+    rnf = \case
+        MsgLeiosNotificationRequestNext -> ()
+        MsgLeiosBlockAnnouncement{} -> ()
+        MsgLeiosBlockOffer{} -> ()
+        MsgLeiosBlockTxsOffer{} -> ()
+        -- votes offer
+        MsgDone -> ()
+
+deriving instance (Eq point, Eq announcement)
+               => Eq (Message (LeiosNotify point announcement) from to)
+
+deriving instance (Show point, Show announcement)
+               => Show (Message (LeiosNotify point announcement) from to)
+
+-----
+
+byteLimitsLeiosNotify
+  :: (bytes -> Word) -> ProtocolSizeLimits (LeiosNotify point announcement) bytes
+byteLimitsLeiosNotify = ProtocolSizeLimits $ \case
+  SingIdle    -> smallByteLimit
+  SingBusy    -> smallByteLimit
+  st@SingDone -> notActiveState st
+
+timeLimitsLeiosNotify
+  :: ProtocolTimeLimits (LeiosNotify point announcement)
+timeLimitsLeiosNotify = ProtocolTimeLimits $ \case
+  SingIdle    -> waitForever
+  SingBusy    -> waitForever
+  st@SingDone -> notActiveState st
+
+-----
+
+codecLeiosNotify
+  :: forall (point :: Type) (announcement :: Type) m.
+     (MonadST m)
+  => (point -> CBOR.Encoding)
+  -> (forall s. CBOR.Decoder s point)
+  -> (announcement -> CBOR.Encoding)
+  -> (forall s. CBOR.Decoder s announcement)
+  -> Codec (LeiosNotify point announcement) CBOR.DeserialiseFailure m ByteString
+codecLeiosNotify encodeP decodeP encodeA decodeA =
+  mkCodecCborLazyBS
+    (encodeLeiosNotify encodeP encodeA)
+    decode
+    where
+      decode
+        :: forall (st :: LeiosNotify point announcement).
+           (ActiveState st)
+        => StateToken st
+        -> forall s. CBOR.Decoder s (SomeMessage st)
+      decode stok = do
+        len <- CBOR.decodeListLen
+        key <- CBOR.decodeWord
+        decodeLeiosNotify decodeP decodeA stok len key
+
+encodeLeiosNotify
+  :: forall (point :: Type) (announcement :: Type)
+            (st  :: LeiosNotify point announcement)
+            (st' :: LeiosNotify point announcement).
+     (point -> CBOR.Encoding)
+  -> (announcement -> CBOR.Encoding)
+  -> Message (LeiosNotify point announcement) st st'
+  -> CBOR.Encoding
+encodeLeiosNotify encodeP encodeA = encode
+  where
+    encode
+      :: forall st0 st1.
+         Message (LeiosNotify point announcement) st0 st1
+      -> CBOR.Encoding
+    encode = \case
+      MsgLeiosNotificationRequestNext ->
+           CBOR.encodeListLen 1
+        <> CBOR.encodeWord 0
+      MsgLeiosBlockAnnouncement x -> 
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 1
+        <> encodeA x
+      MsgLeiosBlockOffer p ->
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 2
+        <> encodeP p
+      MsgLeiosBlockTxsOffer p ->
+           CBOR.encodeListLen 2
+        <> CBOR.encodeWord 3
+        <> encodeP p
+        -- votes offer
+      MsgDone ->
+           CBOR.encodeListLen 1
+        <> CBOR.encodeWord 5
+
+decodeLeiosNotify
+  :: forall (point :: Type) (announcement :: Type)
+            (st :: LeiosNotify point announcement) s.
+     (ActiveState st)
+  => (forall s'. CBOR.Decoder s' point)
+  -> (forall s'. CBOR.Decoder s' announcement)
+  -> StateToken st
+  -> Int
+  -> Word
+  -> CBOR.Decoder s (SomeMessage st)
+decodeLeiosNotify decodeP decodeA = decode
+  where
+    decode
+      :: forall (st' :: LeiosNotify point announcement).
+         (ActiveState st')
+      => StateToken st'
+      -> Int
+      -> Word
+      -> CBOR.Decoder s (SomeMessage st')
+    decode stok len key = do
+      case (stok, len, key) of
+        (SingIdle, 1, 0) ->
+          return $ SomeMessage MsgLeiosNotificationRequestNext
+        (SingBusy, 2, 1) -> do
+          x <- decodeA
+          return $ SomeMessage $ MsgLeiosBlockAnnouncement x
+        (SingBusy, 2, 2) -> do
+          p <- decodeP
+          return $ SomeMessage $ MsgLeiosBlockOffer p
+        (SingBusy, 2, 3) -> do
+          p <- decodeP
+          return $ SomeMessage $ MsgLeiosBlockTxsOffer p
+        -- votes offer
+        (SingIdle, 1, 5) ->
+          return $ SomeMessage MsgDone
+        (SingDone, _, _) -> notActiveState stok
+        -- failures per protocol state
+        (SingIdle, _, _) ->
+          fail $ printf "codecLeiosNotify (%s) unexpected key (%d, %d)" (show stok) key len
+        (SingBusy, _, _) ->
+          fail $ printf "codecLeiosNotify (%s) unexpected key (%d, %d)" (show stok) key len
+
+codecLeiosNotifyId
+  :: forall (point :: Type) (announcement :: Type) m.
+     (Monad m)
+  => Codec
+       (LeiosNotify point announcement)
+       CodecFailure
+       m
+       (AnyMessage (LeiosNotify point announcement))
+codecLeiosNotifyId = Codec {encode, decode}
+  where
+    encode
+      :: forall st st'.
+         ( ActiveState st
+         , StateTokenI st
+         )
+      => Message (LeiosNotify point announcement) st st'
+      -> AnyMessage (LeiosNotify point announcement)
+    encode = AnyMessage
+
+    decode
+      :: forall (st :: LeiosNotify point announcement).
+         (ActiveState st)
+      => StateToken st
+      -> m (DecodeStep
+             (AnyMessage (LeiosNotify point announcement))
+             CodecFailure
+             m
+             (SomeMessage st)
+           )
+    decode stok = return $ DecodePartial $ \bytes ->
+      return $ case (stok, bytes) of
+        (SingIdle, Just (AnyMessage msg@MsgLeiosNotificationRequestNext)) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingBusy, Just (AnyMessage msg@MsgLeiosBlockAnnouncement{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingBusy, Just (AnyMessage msg@MsgLeiosBlockOffer{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingBusy, Just (AnyMessage msg@MsgLeiosBlockTxsOffer{})) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingIdle, Just (AnyMessage msg@MsgDone)) ->
+          DecodeDone (SomeMessage msg) Nothing
+        (SingDone, _) ->
+          notActiveState stok
+        (_, _) ->
+          DecodeFail $ CodecFailure "codecLeiosNotifyId: no matching message"
