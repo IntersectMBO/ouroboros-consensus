2cb6afadcfb46cf74bae9a08083998a72e7a862f
Drazen Popovic <drazen.popovic@iohk.io>
2025-10-15 17:12:33 +0200

    Add README.md for Leios MVD and an actual test scenario condition

--- nix/leios-mvd/test.nix
+++ nix/leios-mvd/test.nix
@@ -24,7 +24,17 @@
   testScript = ''
     start_all()
 
+    # Wait until the respective services are up
     immdb_node.wait_for_unit("immdb-server.service")
     leios_node.wait_for_unit("cardano-node.service")
+
+    # Wait until leios-node synced with immdb-node
+    # NOTE(bladyjoker): Block 51 is the tip
+    # [0.139717s] BlockNo 51  SlotNo 994      6685f44f32433d0817b6edf5f9e00aaaa3c4986524b8b453a620825747a936cc
+    leios_node.wait_until_succeeds("cardano-cli query tip | grep hash | grep -q '6685f4'")
+
+    # Collect logs from leios-node (read them in result/cardano.logs)
+    leios_node.execute("journalctl -u cardano-node --no-pager > cardano-node.logs")
+    leios_node.copy_from_vm("cardano-node.logs", "")
   '';
 }
