f78487be0a6663d412de1eb76e093e8b766113fe
Drazen Popovic <drazen.popovic@iohk.io>
2025-10-15 11:31:58 +0200

    Add `--address` flag to `immdb-server` executable

--- ouroboros-consensus-cardano/app/immdb-server.hs
+++ ouroboros-consensus-cardano/app/immdb-server.hs
@@ -7,27 +7,35 @@ import           Cardano.Crypto.Init (cryptoInit)
 import qualified Cardano.Tools.DBAnalyser.Block.Cardano as Cardano
 import           Cardano.Tools.DBAnalyser.HasAnalysis (mkProtocolInfo)
 import qualified Cardano.Tools.ImmDBServer.Diffusion as ImmDBServer
-import           Data.Void
+import           Data.Void (absurd)
 import           Main.Utf8 (withStdTerminalHandles)
+import           Network.Socket (AddrInfo (addrFlags, addrSocketType))
 import qualified Network.Socket as Socket
-import           Options.Applicative
+import           Options.Applicative (ParserInfo, execParser, fullDesc, help,
+                     helper, info, long, metavar, progDesc, showDefault,
+                     strOption, value)
 import           Ouroboros.Consensus.Node.ProtocolInfo (ProtocolInfo (..))
 
 main :: IO ()
 main = withStdTerminalHandles $ do
     cryptoInit
-    Opts {immDBDir, port, configFile} <- execParser optsParser
-    let sockAddr = Socket.SockAddrInet port hostAddr
-          where
-            -- could also be passed in
-            hostAddr = Socket.tupleToHostAddress (127, 0, 0, 1)
-        args = Cardano.CardanoBlockArgs configFile Nothing
+    Opts {immDBDir, port, address, configFile} <- execParser optsParser
+
+    let hints = Socket.defaultHints { addrFlags = [Socket.AI_NUMERICHOST], addrSocketType = Socket.Stream}
+    addrInfo <- do
+      addrInfos <- Socket.getAddrInfo (Just hints) (Just address) (Just port)
+      case addrInfos of
+        []         -> error "Invalid address or port"
+        addrInfo:_ -> return addrInfo
+
+    let args = Cardano.CardanoBlockArgs configFile Nothing
     ProtocolInfo{pInfoConfig} <- mkProtocolInfo args
-    absurd <$> ImmDBServer.run immDBDir sockAddr pInfoConfig
+    absurd <$> ImmDBServer.run immDBDir (Socket.addrAddress addrInfo) pInfoConfig
 
 data Opts = Opts {
     immDBDir   :: FilePath
-  , port       :: Socket.PortNumber
+  , port       :: String
+  , address    :: String
   , configFile :: FilePath
   }
 
@@ -43,10 +51,16 @@ optsParser =
         , help "Path to the ImmutableDB"
         , metavar "PATH"
         ]
-      port <- option auto $ mconcat
+      port <- strOption $ mconcat
         [ long "port"
         , help "Port to serve on"
-        , value 3001
+        , value "3001"
+        , showDefault
+        ]
+      address <- strOption $ mconcat
+        [ long "address"
+        , help "Address to serve on"
+        , value "127.0.0.1"
         , showDefault
         ]
       configFile <- strOption $ mconcat
@@ -54,4 +68,4 @@ optsParser =
         , help "Path to config file, in the same format as for the node or db-analyser"
         , metavar "PATH"
         ]
-      pure Opts {immDBDir, port, configFile}
+      pure Opts {immDBDir, port, address, configFile}
