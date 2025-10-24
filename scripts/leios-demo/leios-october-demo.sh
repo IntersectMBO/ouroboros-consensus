bf9731e218778942d0fccfb45a5f6173a6ebdefb
Nicolas Frisby <nick.frisby@iohk.io>
2025-10-24 15:47:09 -0700

    Revert "WIP the script"
    This reverts commit 9571dbf669465d9ced67a8174c4845087d2fdee4.

--- scripts/leios-demo/leios-october-demo.sh
+++ scripts/leios-demo/leios-october-demo.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# The only parameter should be the path to the folder
+# The first parameter should be the path to the local checkout of
+# cardano-node.
+#
+# The second parameter should be the path to the folder
 # where the data of a benchmarking cluster run is stored
 # (CLUSTER_RUN_DATA directory).
 
-# P&T cluster run data
+# Local checkout path of the cardano-node repository
 # Safely remove trailing slash if present
-CLUSTER_RUN_DATA="${1%/}"
+CARDANO_NODE_PATH="${1%/}"
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: Please provide one parameter, the path to the cluster run data directory." >&2
+# P&T cluster run data
+CLUSTER_RUN_DATA="${2%/}"
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Please provide two parameters: <cardano-node path> and <CLUSTER_RUN_DATA directory path>." >&2
     exit 1
 fi
 
@@ -23,26 +29,6 @@ if [ ! -d "$CLUSTER_RUN_DATA" ]; then
     exit 1
 fi
 
-if [[ -z "${cardano_node}" ]]; then
-    echo "Error: Please set \${cardano_node} to the path to the cardano-node exe." >&2
-    exit 1
-fi
-
-if [[ -z "${immdb_server}" ]]; then
-    echo "Error: Please set \${immdb_server} to the path to the immdb-server exe." >&2
-    exit 1
-fi
-
-if [[ -z "${REF_SLOT}" ]]; then
-    echo "Error: Please set \${REF_SLOT} to the reference slot." >&2
-    exit 1
-fi
-
-if [[ -z "$LEIOS_SCHEDULE_PATH" ]]; then
-    echo "Error: Please set \${LEIOS_SCHEDULE_PATH} to path to the Leios schedule file." >&2
-    exit 1
-fi
-
 TMP_DIR=$(mktemp -d)
 echo "Using temporary directory for DB and logs: $TMP_DIR"
 
@@ -52,8 +38,8 @@ pushd "$CARDANO_NODE_PATH" > /dev/null
 ## Run cardano-node (node-0)
 ##
 
-echo "Creating topology-node-0.json in $(TMP_DIR)"
-cat << EOF > "${TMP_DIR}/topology-node-0.json"
+echo "Creating topology-node-0.json in $(pwd)"
+cat << EOF > topology-node-0.json
 {
   "bootstrapPeers": [],
   "localRoots": [
@@ -75,11 +61,11 @@ EOF
 
 mkdir -p "$TMP_DIR/node-0/db"
 
-CARDANO_NODE_CMD="${cardano_node} run \
+CARDANO_NODE_CMD="cabal run -- cardano-node run \
     --config $CLUSTER_RUN_DATA/node-0/config.json \
-    --topology ${TMP_DIR}/topology-node-0.json \
+    --topology topology-node-0.json \
     --database-path $TMP_DIR/node-0/db \
-    --socket-path $TMP_DIR/node-0.socket \
+    --socket-path node-0.socket \
     --host-addr 0.0.0.0 --port 3002"
 
 echo "Command: $CARDANO_NODE_CMD &> $TMP_DIR/cardano-node-0.log &"
@@ -94,7 +80,7 @@ echo "Cardano node 0 started with PID: $CARDANO_NODE_0_PID"
 ## Run a second Cardano-node (To be eventually replaced by a mocked downstream node)
 ##
 
-cat << EOF > "${TMP_DIR}/topology-node-1.json"
+cat << EOF > topology-node-1.json
 {
   "bootstrapPeers": [],
   "localRoots": [
@@ -116,11 +102,11 @@ EOF
 
 mkdir -p "$TMP_DIR/node-1/db"
 
-MOCKED_PEER_CMD="${cardano-node} run \
+MOCKED_PEER_CMD="cabal run -- cardano-node run \
     --config $CLUSTER_RUN_DATA/node-0/config.json \
-    --topology ${TMP_DIR}/topology-node-1.json \
+    --topology topology-node-1.json \
     --database-path $TMP_DIR/node-1/db \
-    --socket-path $TMP_DIR/node-1.socket \
+    --socket-path node-1.socket \
     --host-addr 0.0.0.0 --port 3003"
 
 echo "Command (Node 1): $MOCKED_PEER_CMD &> $TMP_DIR/cardano-node-1.log &"
@@ -139,16 +125,14 @@ popd > /dev/null
 ##
 
 ## TODO: we should find a better way to wait for the nodes to be started
-# Calculate the POSIX time 5 seconds from now.
-REF_TIME_FOR_SLOT=$(( $(date +%s) + 5 ))
+# Calculate the POSIX time 60 seconds from now.
+REF_TIME_FOR_SLOT=$(( $(date +%s) + 60 ))
 
-IMMDB_CMD_CORE="${immdb_server} \
+IMMDB_CMD_CORE="cabal run immdb-server \
     -- --db $CLUSTER_RUN_DATA/node-0/db/immutable/ \
     --config $CLUSTER_RUN_DATA/node-0/config.json \
-    --initial-slot $REF_SLOT \
-    --initial-time $REF_TIME_FOR_SLOT \
-    --leios-schedule $LEIOS_SCHEDULE_PATH \
-"
+    --initial-slot 80 \
+    --initial-time $REF_TIME_FOR_SLOT"
 
 echo "Command: $IMMDB_CMD_CORE &> $TMP_DIR/immdb-server.log &"
 
@@ -173,22 +157,22 @@ kill -9 -"$MOCKED_PEER_PID" 2>/dev/null || true
 
 echo "Temporary data stored at: $TMP_DIR"
 
-# # Log analysis
+# Log analysis
 
-# VENV_PATH="./scripts/leios-demo/venv"
+VENV_PATH="./scripts/leios-demo/venv"
 
-# # 1. Activate the Python Virtual Environment
-# if [ -f "$VENV_PATH/bin/activate" ]; then
-#     echo "Activating virtual environment..."
-#     # 'source' must be used for activation to modify the current shell environment
-#     source "$VENV_PATH/bin/activate"
-# else
-#     echo "Error: Virtual environment activation script not found at $VENV_PATH/bin/activate." >&2
-# fi
+# 1. Activate the Python Virtual Environment
+if [ -f "$VENV_PATH/bin/activate" ]; then
+    echo "Activating virtual environment..."
+    # 'source' must be used for activation to modify the current shell environment
+    source "$VENV_PATH/bin/activate"
+else
+    echo "Error: Virtual environment activation script not found at $VENV_PATH/bin/activate." >&2
+fi
 
-# python3 scripts/leios-demo/log_parser.py $TMP_DIR/cardano-node-0.log $TMP_DIR/cardano-node-1.log
+python3 scripts/leios-demo/log_parser.py $TMP_DIR/cardano-node-0.log $TMP_DIR/cardano-node-1.log
 
-# # 2. Deactivate the Python Virtual Environment before exiting
-# deactivate 2>/dev/null || true
+# 2. Deactivate the Python Virtual Environment before exiting
+deactivate 2>/dev/null || true
 
-# exit 0
+exit 0
