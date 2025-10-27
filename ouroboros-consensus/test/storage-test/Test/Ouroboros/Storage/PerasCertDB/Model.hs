5da78b61cb76623b5ea1fe43cca994e2cb5bc47d
Agustin Mista <agustin.mista@moduscreate.com>
2025-10-27 09:58:29 +0100

    Simplify HasPerasCertX field accessors
    Conflicts:
      ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ObjectDiffusion/ObjectPool/PerasCert.hs
      ouroboros-consensus/test/consensus-test/Test/Consensus/MiniProtocol/ObjectDiffusion/PerasCert/Smoke.hs

--- ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/Model.hs
+++ ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/Model.hs
@@ -50,7 +50,6 @@ addCert model@Model{certs} cert
   | otherwise = model{certs = Set.insert cert certs}
 
 hasRoundNo ::
-  StandardHash blk =>
   Set (WithArrivalTime (ValidatedPerasCert blk)) ->
   WithArrivalTime (ValidatedPerasCert blk) ->
   Bool
@@ -66,7 +65,7 @@ getWeightSnapshot Model{certs} =
     | cert <- Set.toList certs
     ]
 
-garbageCollect :: StandardHash blk => SlotNo -> Model blk -> Model blk
+garbageCollect :: SlotNo -> Model blk -> Model blk
 garbageCollect slot model@Model{certs} =
   model{certs = Set.filter keepCert certs}
  where
