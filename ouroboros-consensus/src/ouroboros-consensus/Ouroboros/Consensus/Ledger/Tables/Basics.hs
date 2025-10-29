e2dd9b11c8b3b651153e6191c74f5766d09dddf4
Javier Sagredo <javier.sagredo@iohk.io>
2025-10-29 16:15:48 +0100

    WIP

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Ledger/Tables/Basics.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Ledger/Tables/Basics.hs
@@ -2,38 +2,54 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 module Ouroboros.Consensus.Ledger.Tables.Basics
   ( -- * Kinds
 
   --
-
-    -- | For convenience' sake, we define these kinds which convey the intended
-    -- instantiation for the type variables.
+  --  For convenience' sake, we define these kinds which convey the intended
+  -- instantiation for the type variables.
     LedgerStateKind
   , MapKind
 
     -- * Ledger tables
   , LedgerTables (..)
-  , MemPackIdx
-  , SameUtxoTypes
+  , Table (..)
+  , TAG (..)
+  , TableIndices
   , TxIn
   , TxOut
+  , AccountState
+  , Credential
+
+    -- * Helpers
+  , MemPackIdx
+
+    -- * Casting
+  , SameUtxoTypes
   , castLedgerTables
   ) where
 
-import Data.Coerce (coerce)
 import Data.Kind (Type)
+import Data.SOP.Constraint
+import Data.SOP.Strict
 import GHC.Generics (Generic)
-import NoThunks.Class (NoThunks)
+import NoThunks.Class
 import Ouroboros.Consensus.Ticked (Ticked)
+import Ouroboros.Consensus.Util.TypeLevel
 
 {-------------------------------------------------------------------------------
   Kinds
@@ -41,7 +57,7 @@ import Ouroboros.Consensus.Ticked (Ticked)
 
 -- | Something that holds two types, which intend to represent /keys/ and
 -- /values/.
-type MapKind {- key -} = Type {- value -} -> Type -> Type
+type MapKind = KV
 
 type LedgerStateKind = MapKind -> Type
 
@@ -70,37 +86,68 @@ type LedgerStateKind = MapKind -> Type
 -- two type parameters, the key and the value.
 type LedgerTables :: LedgerStateKind -> MapKind -> Type
 newtype LedgerTables l mk = LedgerTables
-  { getLedgerTables :: mk (TxIn l) (TxOut l)
+  { getLedgerTables :: NP (Table mk l) (TableIndices l)
   }
-  deriving stock Generic
+  deriving Generic
 
-deriving stock instance
-  Show (mk (TxIn l) (TxOut l)) =>
-  Show (LedgerTables l mk)
-deriving stock instance
-  Eq (mk (TxIn l) (TxOut l)) =>
-  Eq (LedgerTables l mk)
 deriving newtype instance
-  NoThunks (mk (TxIn l) (TxOut l)) =>
+  All
+    (Compose NoThunks (Table mk l))
+    (TableIndices l) =>
   NoThunks (LedgerTables l mk)
 
--- | Each @LedgerState@ instance will have the notion of a @TxIn@ for the tables.
---
--- This will change once there is more than one table.
+type Table :: KV -> LedgerStateKind -> TAG -> Type
+newtype Table mk l tag = Table (mk (Fst (KVs tag l)) (Snd (KVs tag l)))
+
+type data TAG = UTxOTable | AccountsTable
+
+type KVs :: TAG -> LedgerStateKind -> (Type, Type)
+type family KVs (tag :: TAG) l where
+  KVs UTxOTable l = '(TxIn l, TxOut l)
+  KVs AccountsTable l = '(Credential l, AccountState l)
+
+type TableIndices :: LedgerStateKind -> [TAG]
+type family TableIndices l
+
+-- type LS :: Type -> LedgerStateKind
+-- data family LS blk mk
+
+-- data Byron
+-- type instance TableIndices (LS Byron) = '[]
+
+-- data BlkA
+-- type instance TableIndices (LS BlkA) = '[UTxOTable]
+
+-- data BlkB
+-- type instance TableIndices (LS BlkB) = '[UTxOTable (LS BlkB), AccountsTable (LS BlkB)]
+
+-- type HFBlock = '[Byron, BlkA, BlkB]
+
+-- type instance TableIndices (LS HFBlk) = '[UTxOTable (LS HFBlk), AccountsTable (LS BlkB)]
+
+type Credential :: LedgerStateKind -> Type
+type family Credential l
+
+type AccountState :: LedgerStateKind -> Type
+type family AccountState l
+
 type TxIn :: LedgerStateKind -> Type
 type family TxIn l
 
--- | Each @LedgerState@ instance will have the notion of a @TxOut@ for the
--- tables.
---
--- This will change once there is more than one table.
 type TxOut :: LedgerStateKind -> Type
 type family TxOut l
 
 type instance TxIn (LedgerTables l) = TxIn l
 type instance TxOut (LedgerTables l) = TxOut l
+type instance Credential (LedgerTables l) = Credential l
+type instance AccountState (LedgerTables l) = AccountState l
+type instance TableIndices (LedgerTables l) = TableIndices l
+
 type instance TxIn (Ticked l) = TxIn l
 type instance TxOut (Ticked l) = TxOut l
+type instance Credential (Ticked l) = Credential l
+type instance AccountState (Ticked l) = AccountState l
+type instance TableIndices (Ticked l) = TableIndices l
 
 -- | Auxiliary information for @IndexedMemPack@.
 type MemPackIdx :: LedgerStateKind -> MapKind -> Type
@@ -109,10 +156,17 @@ type family MemPackIdx l mk where
   MemPackIdx (Ticked l) mk = MemPackIdx l mk
   MemPackIdx l mk = l mk
 
-type SameUtxoTypes l l' = (TxIn l ~ TxIn l', TxOut l ~ TxOut l')
+type SameUtxoTypes l l' =
+  (SListI (TableIndices l), SListI (TableIndices l'), TableIndices l ~ TableIndices l')
 
 castLedgerTables ::
-  SameUtxoTypes l l' =>
+  ( SameShapeAs (TableIndices l) (TableIndices l')
+  , AllZipF
+      (LiftedCoercible (Table mk l) (Table mk l'))
+      (TableIndices l)
+      (TableIndices l')
+  , SameUtxoTypes l l'
+  ) =>
   LedgerTables l mk ->
   LedgerTables l' mk
-castLedgerTables = coerce
+castLedgerTables (LedgerTables l1) = LedgerTables (hcoerce l1)
