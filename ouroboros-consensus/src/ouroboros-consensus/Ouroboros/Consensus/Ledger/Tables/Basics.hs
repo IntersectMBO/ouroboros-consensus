a06f39336aa9bad89d6619372de29c10fae97433
Javier Sagredo <javier.sagredo@iohk.io>
2025-10-31 17:31:59 +0100

    WIP

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Ledger/Tables/Basics.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Ledger/Tables/Basics.hs
@@ -4,12 +4,14 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeData #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
@@ -29,11 +31,15 @@ module Ouroboros.Consensus.Ledger.Tables.Basics
   , LedgerTables (..)
   , Table (..)
   , TAG (..)
-  , TableIndices
+  , STAG (..)
+  , TablesForBlock
   , TxIn
   , TxOut
   , AccountState
   , Credential
+  , onUTxOTable
+  , onAccountsTable
+  , onTable
 
     -- * Helpers
   , MemPackIdx
@@ -41,15 +47,27 @@ module Ouroboros.Consensus.Ledger.Tables.Basics
     -- * Casting
   , SameUtxoTypes
   , castLedgerTables
+  , Sing
+  , sing
+  , SingI
   ) where
 
+import Data.Eq.Singletons
 import Data.Kind (Type)
+import Data.Proxy
 import Data.SOP.Constraint
+import Data.SOP.Sing
 import Data.SOP.Strict
+import Data.Singletons (Sing, SingI (..), singByProxy)
+import Data.Singletons.Decide
+import Data.Type.Equality (TestEquality (testEquality), (:~:) (..))
+import Data.Void
 import GHC.Generics (Generic)
+import Lens.Micro
 import NoThunks.Class
 import Ouroboros.Consensus.Ticked (Ticked)
 import Ouroboros.Consensus.Util.TypeLevel
+import qualified Prelude.Singletons as S
 
 {-------------------------------------------------------------------------------
   Kinds
@@ -59,11 +77,58 @@ import Ouroboros.Consensus.Util.TypeLevel
 -- /values/.
 type MapKind = KV
 
-type LedgerStateKind = MapKind -> Type
+type LedgerStateKind = MapKind -> Type -- LedgerState ByronBlock
 
 {-------------------------------------------------------------------------------
   Ledger tables
 -------------------------------------------------------------------------------}
+
+-- | The possible tables that Consensus is aware of.
+type data TAG = UTxOTable | AccountsTable
+
+------------------------------------------------------------
+-- START Singletons
+------------------------------------------------------------
+data STAG a where
+  SUTxOTable :: STAG UTxOTable
+  SAccountsTable :: STAG AccountsTable
+
+type instance Sing = STAG
+
+instance SingI UTxOTable where
+  sing = SUTxOTable
+instance SingI AccountsTable where
+  sing = SAccountsTable
+
+instance TestEquality STAG where
+  testEquality SUTxOTable SUTxOTable = Just Refl
+  testEquality SUTxOTable _ = Nothing
+  testEquality SAccountsTable SAccountsTable = Just Refl
+  testEquality SAccountsTable _ = Nothing
+
+------------------------------------------------------------
+-- END Singletons
+------------------------------------------------------------
+
+-- | The table keys and values for each table.
+type TableKV :: TAG -> LedgerStateKind -> (Type, Type)
+type family TableKV tag l where
+  TableKV UTxOTable l = '(TxIn l, TxOut l)
+  TableKV AccountsTable l = '(Credential l, AccountState l)
+
+-- | A table value
+type Table :: KV -> LedgerStateKind -> TAG -> Type
+newtype Table mk l tag = Table {getTable :: mk (Fst (TableKV tag l)) (Snd (TableKV tag l))}
+
+-- | Each block will declare its tables.
+--
+-- > TablesForBlock (LedgerState Byron) = '[]
+--
+-- > TablesForBlock (LedgerState Shelley) = '[UTxOTable]
+--
+-- > TablesForBlock (LedgerState Conway) = '[UTxOTable, AccountsTable]
+type TablesForBlock :: LedgerStateKind -> [TAG]
+type family TablesForBlock l
 
 -- | The Ledger Tables represent the portion of the data on disk that has been
 -- pulled from disk and attached to the in-memory Ledger State or that will
@@ -86,44 +151,78 @@ type LedgerStateKind = MapKind -> Type
 -- two type parameters, the key and the value.
 type LedgerTables :: LedgerStateKind -> MapKind -> Type
 newtype LedgerTables l mk = LedgerTables
-  { getLedgerTables :: NP (Table mk l) (TableIndices l)
+  { getLedgerTables :: NP (Table mk l) (TablesForBlock l)
   }
   deriving Generic
 
 deriving newtype instance
   All
     (Compose NoThunks (Table mk l))
-    (TableIndices l) =>
+    (TablesForBlock l) =>
   NoThunks (LedgerTables l mk)
 
-type Table :: KV -> LedgerStateKind -> TAG -> Type
-newtype Table mk l tag = Table (mk (Fst (KVs tag l)) (Snd (KVs tag l)))
+------------------------------------------------------------
+-- Type proof of inclusion
+------------------------------------------------------------
 
-type data TAG = UTxOTable | AccountsTable
+data Membership (xs :: [TAG]) (x :: TAG) where
+  MZ :: Membership (x ': xs) x
+  MS :: Membership xs x -> Membership (y ': xs) x
 
-type KVs :: TAG -> LedgerStateKind -> (Type, Type)
-type family KVs (tag :: TAG) l where
-  KVs UTxOTable l = '(TxIn l, TxOut l)
-  KVs AccountsTable l = '(Credential l, AccountState l)
+deriving instance Show (Membership xs x)
 
-type TableIndices :: LedgerStateKind -> [TAG]
-type family TableIndices l
+findMembership ::
+  forall (xs :: [TAG]) (x :: TAG).
+  S.SList xs ->
+  S.Sing x ->
+  Maybe (Membership xs x)
+findMembership S.SNil _ = Nothing
+findMembership (S.SCons sy sxs) sx =
+  case testEquality sx sy of
+    Just Refl -> Just MZ
+    Nothing -> MS <$> findMembership sxs sx
 
--- type LS :: Type -> LedgerStateKind
--- data family LS blk mk
+------------------------------------------------------------
+-- Lenses
+------------------------------------------------------------
 
--- data Byron
--- type instance TableIndices (LS Byron) = '[]
+-- | Given a proof of inclusion, create a lens for the particular item in the NP
+ixNP :: Membership xs x -> Lens' (NP f xs) (f x)
+ixNP MZ f (x :* xs) = (:* xs) <$> f x
+ixNP (MS m) f (x :* xs) = (x :*) <$> ixNP m f xs
+ixNP _ _ Nil = error "impossible: membership longer than NP"
 
--- data BlkA
--- type instance TableIndices (LS BlkA) = '[UTxOTable]
+setterForSing ::
+  forall l mk tag.
+  S.SList (TablesForBlock l) -> -- '[UTxOtable, AccountsTable]
+  S.Sing tag -> -- SUTxoTable
+  Maybe (ASetter' (LedgerTables l mk) (Table mk l tag))
+setterForSing sxs sx =
+  (\mbm -> \f -> fmap LedgerTables . ixNP mbm f . getLedgerTables) <$> findMembership sxs sx
 
--- data BlkB
--- type instance TableIndices (LS BlkB) = '[UTxOTable (LS BlkB), AccountsTable (LS BlkB)]
+onUTxOTable ::
+  SLIST (TablesForBlock l) =>
+  Proxy l -> ASetter' (LedgerTables l mk) (Table mk l UTxOTable)
+onUTxOTable p = onTable p (Proxy @UTxOTable)
 
--- type HFBlock = '[Byron, BlkA, BlkB]
+onAccountsTable ::
+  SLIST (TablesForBlock l) =>
+  Proxy l -> ASetter' (LedgerTables l mk) (Table mk l AccountsTable)
+onAccountsTable p = onTable p (Proxy @AccountsTable)
 
--- type instance TableIndices (LS HFBlk) = '[UTxOTable (LS HFBlk), AccountsTable (LS BlkB)]
+onTable ::
+  forall l (tag :: TAG) mk.
+  (SingI tag, SLIST (TablesForBlock l)) =>
+  Proxy l ->
+  Proxy tag ->
+  ASetter' (LedgerTables l mk) (Table mk l tag)
+onTable _ _ =
+  case setterForSing sListt (sing :: Sing tag) of
+    Nothing -> \_ s -> pure s
+    Just setter -> setter
+
+class SLIST xs where
+  sListt :: S.SList xs
 
 type Credential :: LedgerStateKind -> Type
 type family Credential l
@@ -141,13 +240,13 @@ type instance TxIn (LedgerTables l) = TxIn l
 type instance TxOut (LedgerTables l) = TxOut l
 type instance Credential (LedgerTables l) = Credential l
 type instance AccountState (LedgerTables l) = AccountState l
-type instance TableIndices (LedgerTables l) = TableIndices l
+type instance TablesForBlock (LedgerTables l) = TablesForBlock l
 
 type instance TxIn (Ticked l) = TxIn l
 type instance TxOut (Ticked l) = TxOut l
 type instance Credential (Ticked l) = Credential l
 type instance AccountState (Ticked l) = AccountState l
-type instance TableIndices (Ticked l) = TableIndices l
+type instance TablesForBlock (Ticked l) = TablesForBlock l
 
 -- | Auxiliary information for @IndexedMemPack@.
 type MemPackIdx :: LedgerStateKind -> MapKind -> Type
@@ -157,14 +256,14 @@ type family MemPackIdx l mk where
   MemPackIdx l mk = l mk
 
 type SameUtxoTypes l l' =
-  (SListI (TableIndices l), SListI (TableIndices l'), TableIndices l ~ TableIndices l')
+  (SListI (TablesForBlock l), SListI (TablesForBlock l'), TablesForBlock l ~ TablesForBlock l')
 
 castLedgerTables ::
-  ( SameShapeAs (TableIndices l) (TableIndices l')
+  ( SameShapeAs (TablesForBlock l) (TablesForBlock l')
   , AllZipF
       (LiftedCoercible (Table mk l) (Table mk l'))
-      (TableIndices l)
-      (TableIndices l')
+      (TablesForBlock l)
+      (TablesForBlock l')
   , SameUtxoTypes l l'
   ) =>
   LedgerTables l mk ->
