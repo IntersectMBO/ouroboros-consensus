3c282fbcd33f39b7483885cbabebd6a208de9e35
Agustin Mista <agustin.mista@moduscreate.com>
2025-11-03 10:26:31 +0100

    Store most recent cert in the PerasCertDB
    This commit adds a method to the PerasCertDB API to retrieve the latest
    certificate seen. This is certificate needed to implement the Peras voting
    and must be kept around even after garbage collection. Because of this,
    we extend the internal state of the PerasCertDB to store this special
    certificate on the side, and (potentially) update it after new
    certificates are added to the database.
    
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Alexander Esgen <alexander.esgen@iohk.io>
    Co-authored-by: Georgy Lukyanov <georgy.lukyanov@iohk.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>

--- ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/Model.hs
+++ ouroboros-consensus/test/storage-test/Test/Ouroboros/Storage/PerasCertDB/Model.hs
@@ -11,6 +11,7 @@ module Test.Ouroboros.Storage.PerasCertDB.Model
   , closeDB
   , addCert
   , getWeightSnapshot
+  , getLatestCertSeen
   , garbageCollect
   , hasRoundNo
   ) where
@@ -19,14 +20,16 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import GHC.Generics (Generic)
 import Ouroboros.Consensus.Block
-import Ouroboros.Consensus.BlockchainTime.WallClock.Types (WithArrivalTime)
+import Ouroboros.Consensus.BlockchainTime.WallClock.Types (WithArrivalTime, forgetArrivalTime)
 import Ouroboros.Consensus.Peras.Weight
   ( PerasWeightSnapshot
   , mkPerasWeightSnapshot
   )
+import Ouroboros.Consensus.Util (safeMaximumOn)
 
 data Model blk = Model
   { certs :: Set (WithArrivalTime (ValidatedPerasCert blk))
+  , latestCertSeen :: Maybe (WithArrivalTime (ValidatedPerasCert blk))
   , open :: Bool
   }
   deriving Generic
@@ -34,20 +37,23 @@ data Model blk = Model
 deriving instance StandardHash blk => Show (Model blk)
 
 initModel :: Model blk
-initModel = Model{open = False, certs = Set.empty}
+initModel = Model{open = False, certs = Set.empty, latestCertSeen = Nothing}
 
 openDB :: Model blk -> Model blk
 openDB model = model{open = True}
 
 closeDB :: Model blk -> Model blk
-closeDB _ = Model{open = False, certs = Set.empty}
+closeDB _ = Model{open = False, certs = Set.empty, latestCertSeen = Nothing}
 
 addCert ::
   StandardHash blk =>
   Model blk -> WithArrivalTime (ValidatedPerasCert blk) -> Model blk
 addCert model@Model{certs} cert
   | certs `hasRoundNo` cert = model
-  | otherwise = model{certs = Set.insert cert certs}
+  | otherwise = model{certs = certs', latestCertSeen = safeMaximumOn roundNo (Set.toList certs')}
+ where
+  certs' = Set.insert cert certs
+  roundNo = getPerasCertRound . forgetArrivalTime
 
 hasRoundNo ::
   Set (WithArrivalTime (ValidatedPerasCert blk)) ->
@@ -64,6 +70,11 @@ getWeightSnapshot Model{certs} =
     [ (getPerasCertBoostedBlock cert, getPerasCertBoost cert)
     | cert <- Set.toList certs
     ]
+
+getLatestCertSeen ::
+  Model blk -> Maybe (WithArrivalTime (ValidatedPerasCert blk))
+getLatestCertSeen Model{latestCertSeen} =
+  latestCertSeen
 
 garbageCollect :: SlotNo -> Model blk -> Model blk
 garbageCollect slot model@Model{certs} =
