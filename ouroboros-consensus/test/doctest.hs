431506b550b5e85dd5c56b6a5d733741568131e1
Onyinyechi Eze <onyinyechieze46@gmail.com>
2025-11-26 07:32:10 +0100

    Update doctest.hs

--- ouroboros-consensus/test/doctest.hs
+++ ouroboros-consensus/test/doctest.hs
@@ -2,7 +2,11 @@ module Main (main) where
 
 main :: IO ()
 main = do
-  putStrLn "This test-suite exists only to add dependencies"
-  putStrLn "To run doctests: "
-  putStrLn "    cabal build all --enable-tests"
-  putStrLn "    cabal-docspec"
+  putStrLn "Test Suite Placeholder"
+  putStrLn "This test suite only exists to pull in the required dependencies."
+
+  putStrLn "\nTo run doctests:"
+  putStrLn "  cabal build all --enable-tests"
+  putStrLn "  cabal-docspec"
+
+  putStrLn "\n(No tests actually run from this file.)"
