2fbb62bf0b6b07269edac196cd36f5dc21e51805
Agustin Mista <agustin.mista@moduscreate.com>
2026-01-26 13:12:22 +0100

    Implement test model for wFA^LS committee selection
    This commits provides a model implementation for the weighted Fait
    Accompli committee selection scheme that will be used for Peras. This
    comes with a pure model of the local sortition scheme that will be used
    as a fallback for non-deterministic non-persistent voters.
    
    In addition, it provides some general QuickCheck properties to verify
    the validity of the deterministic parts of the algorithm.
    
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>

---
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/Committee/WFALS/Conformance.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.Consensus.Committee.WFALS.Conformance
+  ( conformsToRustImplementation
+  )
+where
+
+import Data.Aeson
+  ( FromJSON (..)
+  , eitherDecodeStrict
+  , withObject
+  , (.:)
+  )
+import Data.Array (Array)
+import qualified Data.Array as Array
+import qualified Data.FileEmbed as FileEmbed
+import Data.Map.Strict (Map)
+import System.FilePath ((</>))
+import Test.QuickCheck (Property)
+import Test.QuickCheck.Gen (Gen, choose)
+import Test.QuickCheck.Property (counterexample, forAll, (===))
+
+type CommitteeSize = Int
+type NumPersistent = Int
+type NumNonPersistent = Int
+type PoolId = String
+type Stake = Rational
+type StakeDistr = Map PoolId Stake
+
+-- | A single result of the Rust implementation
+data RustResult = RustResult
+  { targetCommitteeSize :: CommitteeSize
+  , numPersistent :: NumPersistent
+  , numNonPersistent :: NumNonPersistent
+  }
+  deriving (Show, Eq)
+
+instance FromJSON RustResult where
+  parseJSON = withObject "RustResult" $ \obj ->
+    RustResult
+      <$> obj .: "target"
+      <*> obj .: "persistent"
+      <*> obj .: "nonpersistent"
+
+-- | Embedded Rust implementation results
+rustResults :: Array Int RustResult
+rustResults =
+  either error toArray $
+    eitherDecodeStrict $
+      $( FileEmbed.embedFile $
+           "ouroboros-consensus"
+             </> "test"
+             </> "consensus-test"
+             </> "data"
+             </> "rust_results.json"
+       )
+ where
+  toArray pairs =
+    Array.listArray (0, length pairs - 1) pairs
+
+-- | Embedded stake distribution
+stakeDistr :: StakeDistr
+stakeDistr =
+  either error id $
+    eitherDecodeStrict $
+      $( FileEmbed.embedFile $
+           "ouroboros-consensus"
+             </> "test"
+             </> "consensus-test"
+             </> "data"
+             </> "stake_distr.json"
+       )
+
+-- | Sample a value from an array
+sampleArray :: Array Int a -> Gen a
+sampleArray array = do
+  i <- choose (Array.bounds array)
+  pure $ (Array.!) array i
+
+-- | Check that a weighted fait accompli committee selection implementation
+-- conforms to the Rust implementation by comparing the number persistent and
+-- non-persistent committee members it selects for a given target committee size.
+conformsToRustImplementation ::
+  ( Map PoolId Stake ->
+    CommitteeSize ->
+    ( NumPersistent
+    , NumNonPersistent
+    )
+  ) ->
+  Property
+conformsToRustImplementation wfals = do
+  forAll (sampleArray rustResults) $
+    \RustResult
+       { targetCommitteeSize
+       , numPersistent
+       , numNonPersistent
+       } -> do
+        let (actualNumPersistent, actualNumNonPersistent) =
+              wfals stakeDistr targetCommitteeSize
+        counterexample
+          ( unlines
+              [ "Target committee size: "
+                  <> show targetCommitteeSize
+              , "Expected (persistent, non-persistent): "
+                  <> show (numPersistent, numNonPersistent)
+              , "Actual (persistent, non-persistent): "
+                  <> show (actualNumPersistent, actualNumNonPersistent)
+              ]
+          )
+          $ (actualNumPersistent, actualNumNonPersistent)
+            === (numPersistent, numNonPersistent)
