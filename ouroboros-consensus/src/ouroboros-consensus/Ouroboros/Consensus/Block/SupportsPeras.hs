3a59a8a551141a5999f57e86b909bca6d6d6f1ff
Agustin Mista <agustin.mista@moduscreate.com>
2026-02-06 11:25:57 +0100

    Add block certificate projection to BlockSupportsPeras
    This commit extends the BlockSupportsPeras type class with a method
    getPerasCertInBlock to (potentially) extract Peras certificates from
    blocks when they are optionally included to orchestrate the end of a
    cooldown period.
    
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Block/SupportsPeras.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Block/SupportsPeras.hs
@@ -274,6 +274,14 @@ class
     ValidatedPerasVotesWithQuorum blk ->
     Either (PerasForgeErr blk) (ValidatedPerasCert blk)
 
+  -- | Extract a Peras certificate optionally stored in a block.
+  --
+  -- Returns 'Nothing' if the block does not contain a Peras certificate, or
+  -- if the block is from an era that does not support Peras certificates.
+  getPerasCertInBlock ::
+    blk ->
+    Maybe (PerasCert blk)
+
 -- TODO: degenerate instance for all blks to get things to compile
 -- see https://github.com/tweag/cardano-peras/issues/73
 instance StandardHash blk => BlockSupportsPeras blk where
@@ -342,6 +350,10 @@ instance StandardHash blk => BlockSupportsPeras blk where
               }
         , vpcCertBoost = perasWeight params
         }
+
+  -- TODO: extract actual Peras certificates from blocks when the HFC plumbing
+  -- is in place.
+  getPerasCertInBlock _ = Nothing
 
 instance ShowProxy blk => ShowProxy (PerasCert blk) where
   showProxy _ = "PerasCert " <> showProxy (Proxy @blk)
