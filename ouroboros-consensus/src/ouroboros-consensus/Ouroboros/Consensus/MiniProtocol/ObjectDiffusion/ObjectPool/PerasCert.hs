7a606f5b2b6ad1800db0d520551b03d7f388e56b
Thomas BAGREL <thomas.bagrel@tweag.io>
2026-02-13 15:41:00 +0100

    Introduce `ObjectPool{Reader,Writer}` interfaces and implem of it for `PerasCert(DB)`
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Alexander Esgen <alexander.esgen@iohk.io>
    Co-authored-by: Georgy Lukyanov <georgy.lukyanov@iohk.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Nicolas "Niols" Jeannerod <nicolas.jeannerod@moduscreate.com>

---
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/MiniProtocol/ObjectDiffusion/ObjectPool/PerasCert.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+-- | Instantiate 'ObjectPoolReader' and 'ObjectPoolWriter' using Peras
+-- certificates from the 'PerasCertDB' (or the 'ChainDB' which is wrapping the
+-- 'PerasCertDB').
+module Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.PerasCert
+  ( makePerasCertPoolReaderFromCertDB
+  , makePerasCertPoolWriterFromCertDB
+  , makePerasCertPoolReaderFromChainDB
+  , makePerasCertPoolWriterFromChainDB
+  ) where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import GHC.Exception (throw)
+import Ouroboros.Consensus.Block
+import Ouroboros.Consensus.BlockchainTime.WallClock.Types
+  ( SystemTime
+  , WithArrivalTime (..)
+  , addArrivalTime
+  )
+import Ouroboros.Consensus.MiniProtocol.ObjectDiffusion.ObjectPool.API
+import Ouroboros.Consensus.Storage.ChainDB.API (ChainDB)
+import qualified Ouroboros.Consensus.Storage.ChainDB.API as ChainDB
+import Ouroboros.Consensus.Storage.PerasCertDB.API
+  ( PerasCertDB
+  , PerasCertSnapshot
+  , PerasCertTicketNo
+  )
+import qualified Ouroboros.Consensus.Storage.PerasCertDB.API as PerasCertDB
+import Ouroboros.Consensus.Util.IOLike
+
+-- | TODO: replace by `Data.Map.take` as soon as we move to GHC 9.8
+takeAscMap :: Int -> Map k v -> Map k v
+takeAscMap n = Map.fromDistinctAscList . take n . Map.toAscList
+
+makePerasCertPoolReaderFromSnapshot ::
+  IOLike m =>
+  STM m (PerasCertSnapshot blk) ->
+  ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+makePerasCertPoolReaderFromSnapshot getCertSnapshot =
+  ObjectPoolReader
+    { oprObjectId = getPerasCertRound
+    , oprZeroTicketNo = PerasCertDB.zeroPerasCertTicketNo
+    , oprObjectsAfter = \lastKnown limit -> do
+        certSnapshot <- getCertSnapshot
+        let certsAfterLastKnown =
+              PerasCertDB.getCertsAfter certSnapshot lastKnown
+        let loadCertsAfterLastKnown =
+              pure $
+                fmap
+                  (vpcCert . forgetArrivalTime)
+                  (takeAscMap (fromIntegral limit) certsAfterLastKnown)
+        pure $
+          if Map.null certsAfterLastKnown
+            then Nothing
+            else Just loadCertsAfterLastKnown
+    }
+
+makePerasCertPoolReaderFromCertDB ::
+  IOLike m =>
+  PerasCertDB m blk ->
+  ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+makePerasCertPoolReaderFromCertDB perasCertDB =
+  makePerasCertPoolReaderFromSnapshot (PerasCertDB.getCertSnapshot perasCertDB)
+
+makePerasCertPoolWriterFromCertDB ::
+  (StandardHash blk, IOLike m) =>
+  SystemTime m ->
+  PerasCertDB m blk ->
+  ObjectPoolWriter PerasRoundNo (PerasCert blk) m
+makePerasCertPoolWriterFromCertDB systemTime perasCertDB =
+  ObjectPoolWriter
+    { opwObjectId = getPerasCertRound
+    , opwAddObjects = \certs -> do
+        addPerasVotes systemTime (PerasCertDB.addCert perasCertDB) certs
+    , opwHasObject = do
+        certSnapshot <- PerasCertDB.getCertSnapshot perasCertDB
+        pure $ PerasCertDB.containsCert certSnapshot
+    }
+
+makePerasCertPoolReaderFromChainDB ::
+  IOLike m =>
+  ChainDB m blk ->
+  ObjectPoolReader PerasRoundNo (PerasCert blk) PerasCertTicketNo m
+makePerasCertPoolReaderFromChainDB chainDB =
+  makePerasCertPoolReaderFromSnapshot (ChainDB.getPerasCertSnapshot chainDB)
+
+makePerasCertPoolWriterFromChainDB ::
+  (StandardHash blk, IOLike m) =>
+  SystemTime m ->
+  ChainDB m blk ->
+  ObjectPoolWriter PerasRoundNo (PerasCert blk) m
+makePerasCertPoolWriterFromChainDB systemTime chainDB =
+  ObjectPoolWriter
+    { opwObjectId = getPerasCertRound
+    , opwAddObjects = \certs -> do
+        addPerasVotes systemTime (ChainDB.addPerasCertAsync chainDB) certs
+    , opwHasObject = do
+        certSnapshot <- ChainDB.getPerasCertSnapshot chainDB
+        pure $ PerasCertDB.containsCert certSnapshot
+    }
+
+data PerasCertInboundException
+  = forall blk. PerasCertValidationError (PerasValidationErr blk)
+
+deriving instance Show PerasCertInboundException
+
+instance Exception PerasCertInboundException
+
+-- | Validate a list of 'PerasCert's, throwing a 'PerasCertInboundException' if
+-- any of them are invalid.
+validatePerasCerts ::
+  (StandardHash blk, MonadThrow m) =>
+  [PerasCert blk] ->
+  m [ValidatedPerasCert blk]
+validatePerasCerts certs = do
+  let perasCfg = mkPerasParams
+  -- TODO pass down 'BlockConfig' when all the plumbing is in place
+  -- see https://github.com/tweag/cardano-peras/issues/73
+  -- see https://github.com/tweag/cardano-peras/issues/120
+  case traverse (validatePerasCert perasCfg) certs of
+    Left validationErr -> throw (PerasCertValidationError validationErr)
+    Right validatedCerts -> return validatedCerts
+
+-- | Add a list of 'PerasCert's into an object pool.
+--
+-- NOTE: we first validate the certificates, throwing an exception if any of
+-- them are invalid. We then wrap them with their arrival time, and finally add
+-- them to the pool using the provided adder function.
+--
+-- The order of the first two operations (i.e., validation and timestamping) are
+-- rather arbitrary, and the abstract Peras protocol just assumes it can happen
+-- "within" a slot.
+addPerasVotes ::
+  (StandardHash blk, MonadSTM m, MonadThrow m) =>
+  SystemTime m ->
+  (WithArrivalTime (ValidatedPerasCert blk) -> m a) ->
+  [PerasCert blk] ->
+  m ()
+addPerasVotes systemTime adder votes = do
+  validatePerasCerts votes
+    >>= mapM (addArrivalTime systemTime)
+    >>= mapM_ adder
