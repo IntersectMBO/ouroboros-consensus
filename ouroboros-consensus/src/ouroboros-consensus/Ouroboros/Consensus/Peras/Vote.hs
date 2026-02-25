186382294030649c77bbf10b75756104d52dc051
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2026-02-25 14:59:50 +0100

    Add minimal voting thread and tracers

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Peras/Vote.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Peras/Vote.hs
@@ -1,3 +1,23 @@
-module Ouroboros.Consensus.Peras.Vote (module X) where
+{-# LANGUAGE LambdaCase #-}
+
+module Ouroboros.Consensus.Peras.Vote
+ ( module X
+ , TraceVotingEvent (..)
+ ) where
 
 import Ouroboros.Consensus.Peras.Vote.Aggregation as X
+import Ouroboros.Consensus.Peras.Voting.Rules (PerasVotingRulesDecision)
+import Ouroboros.Consensus.Util.Pred (Explainable (..), ExplanationMode (..))
+
+-- | TODO: remove type parameter when actual types are known for committee selection
+data TraceVotingEvent electionEvt
+  = TraceCommitteeSelectionEvent electionEvt
+  | TraceVotingRuleEvent PerasVotingRulesDecision
+
+instance Explainable electionEvt => Explainable (TraceVotingEvent electionEvt) where
+  explain mode = \case
+    TraceCommitteeSelectionEvent evt -> "CommitteeSelection(" <> explain mode evt <> ")"
+    TraceVotingRuleEvent evt -> "VotingRule(" <> explain mode evt <> ")"
+
+instance Explainable electionEvt => Show (TraceVotingEvent electionEvt) where
+  show = explain Shallow
