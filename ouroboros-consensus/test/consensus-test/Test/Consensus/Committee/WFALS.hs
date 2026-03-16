bca6699f7ce098975cf5c787f8019e0155da6af5
Agustin Mista <agustin.mista@moduscreate.com>
2026-03-16 10:02:06 +0100

    Refactor WFA conformance tests and instantiate real implementation
    This commit refactors the weighted Fait-Accompli conformance test suite
    into a composite unit test that accepts a function to preprocess the stake
    distribution into a value that gets shared accross individual tests.
    
    This is useful to instantiate this test suite against the real implementation,
    which relies on a precomputed stake distribution array that can be
    shared across all 3000 individual test steps.
    
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>

--- ouroboros-consensus/test/consensus-test/Test/Consensus/Committee/WFALS.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/Committee/WFALS.hs
@@ -3,13 +3,17 @@ module Test.Consensus.Committee.WFALS
   )
 where
 
+import qualified Cardano.Crypto.DSIGN.Class as SL
+import qualified Cardano.Crypto.Seed as SL
+import qualified Cardano.Ledger.Keys as SL
 import qualified Data.Map.Strict as Map
+import Data.String (IsString (..))
+import qualified Ouroboros.Consensus.Committee.Types as WFA
+import qualified Ouroboros.Consensus.Committee.WFA as WFA
 import Test.Consensus.Committee.WFALS.Conformance (conformsToRustImplementation)
 import qualified Test.Consensus.Committee.WFALS.Model as Model
 import qualified Test.Consensus.Committee.WFALS.Model.Test as Model
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.QuickCheck (testProperty)
-import Test.Util.TestEnv (adjustQuickCheckTests)
 
 tests :: TestTree
 tests =
@@ -17,20 +21,67 @@ tests =
     "weighted Fait-Accompli committee selection tests"
     [ Model.tests
     , modelConformsToRustImplementation
+    , realImplementationConformsToRustImplementation
     ]
 
 -- | Check that the model implementation matches the Rust one
 modelConformsToRustImplementation :: TestTree
 modelConformsToRustImplementation =
-  adjustQuickCheckTests (* 10) $
-    testProperty "model conforms to Rust implementation" $
-      conformsToRustImplementation model
+  conformsToRustImplementation
+    "model conforms to Rust implementation"
+    mkStakeDistr
+    model
  where
+  mkStakeDistr =
+    Map.map Model.rationalToStake
+
   model stakeDistr targetCommitteeSize =
     let (persistentSeats, numNonPersistentSeats, _) =
           Model.weightedFaitAccompliPersistentSeats
             (fromIntegral targetCommitteeSize)
-            (Map.map Model.rationalToStake stakeDistr)
+            stakeDistr
      in ( Map.size persistentSeats
         , fromIntegral numNonPersistentSeats
         )
+
+-- | Check that the real implementation matches the Rust one
+realImplementationConformsToRustImplementation :: TestTree
+realImplementationConformsToRustImplementation =
+  conformsToRustImplementation
+    "real implementation conforms to Rust implementation"
+    mkStakeDistr
+    impl
+ where
+  -- NOTE: we don't seem to have an easy way to convert the input hash into its
+  -- corresponding 'KeyHash StakePool', so here we are just recreating a new one
+  -- derived from the input string. This is fine for our purposes since we don't
+  -- inspect the actual pool IDs in the implementation, and we only rely on them
+  -- being unique, which they should be as long as the input strings are unique.
+  mkStakeDistr =
+    WFA.mkExtWFAStakeDistr
+      . Map.mapKeys
+        ( \str ->
+            WFA.PoolId
+              . SL.hashKey
+              . SL.VKey
+              . SL.deriveVerKeyDSIGN
+              . SL.genKeyDSIGN
+              . SL.mkSeedFromBytes
+              . fromString
+              $ str
+        )
+      . Map.map
+        ( \stake ->
+            (WFA.LedgerStake stake, ())
+        )
+
+  impl stakeDistr targetCommitteeSize =
+    let
+      (persistentSeats, nonPersistentSeats, _, _) =
+        WFA.weightedFaitAccompliSplitSeats
+          stakeDistr
+          (WFA.TargetCommitteeSize (fromIntegral targetCommitteeSize))
+     in
+      ( fromIntegral (WFA.unPersistentCommitteeSize persistentSeats)
+      , fromIntegral (WFA.unNonPersistentCommitteeSize nonPersistentSeats)
+      )
