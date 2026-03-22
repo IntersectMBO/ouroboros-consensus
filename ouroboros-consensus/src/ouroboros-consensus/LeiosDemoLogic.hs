8fd6bdca3f663c471ca4a44174328303b2c1d422
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-03-22 07:26:23 +0100

    Keep a note about a raise database error

--- ouroboros-consensus/src/ouroboros-consensus/LeiosDemoLogic.hs
+++ ouroboros-consensus/src/ouroboros-consensus/LeiosDemoLogic.hs
@@ -573,6 +573,8 @@ msgLeiosBlock ktracer tracer (writeLock, outstandingVar, readyVar) db peerId req
         -- the fetching logic does download it. This results in a duplicate
         -- insert here. We can of course make the database ignore the
         -- duplicates, but we should have not even fetched it.
+        -- REVIEW: ^^^^ this should be resolved
+        -- TODO: This was encountered again, but likely because of a race on two fetches.
         leiosDbInsertEbBody db point eb
         traceWith ktracer $ TraceLeiosBlockAcquired point
     -- update NodeKernel state
