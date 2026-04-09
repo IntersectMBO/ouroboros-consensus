b02526df8c78d157597cb47082a6b7edbc84f5c7
Agustin Mista <agustin.mista@moduscreate.com>
2026-04-09 11:21:31 +0200

    Tweak existing WFALS model and conformance tests
    This commit reorganizes the existing model and conformance test
    infrastructure for WFALS in anticipation to the the tests for the real
    implementation implemented in the next commit.
    
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>

--- ouroboros-consensus/test/consensus-test/Test/Consensus/Committee/WFALS.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/Committee/WFALS.hs
@@ -1,20 +1,17 @@
-module Test.Consensus.Committee.WFALS
-  ( tests
-  )
-where
+{-# LANGUAGE RankNTypes #-}
+
+module Test.Consensus.Committee.WFALS (tests) where
 
 import qualified Data.Map.Strict as Map
 import Test.Consensus.Committee.WFALS.Conformance (conformsToRustImplementation)
 import qualified Test.Consensus.Committee.WFALS.Model as Model
-import qualified Test.Consensus.Committee.WFALS.Model.Test as Model
+import qualified Test.Consensus.Committee.WFALS.Model.Tests as Model
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.QuickCheck (testProperty)
-import Test.Util.TestEnv (adjustQuickCheckTests)
 
 tests :: TestTree
 tests =
   testGroup
-    "weighted Fait-Accompli committee selection tests"
+    "WFALS"
     [ Model.tests
     , modelConformsToRustImplementation
     ]
@@ -22,15 +19,26 @@ tests =
 -- | Check that the model implementation matches the Rust one
 modelConformsToRustImplementation :: TestTree
 modelConformsToRustImplementation =
-  adjustQuickCheckTests (* 10) $
-    testProperty "model conforms to Rust implementation" $
-      conformsToRustImplementation model
+  conformsToRustImplementation
+    "Model conforms to Rust implementation"
+    mkStakeDistr
+    model
  where
-  model stakeDistr targetCommitteeSize =
-    let (persistentSeats, numNonPersistentSeats, _) =
-          Model.weightedFaitAccompliPersistentSeats
-            (fromIntegral targetCommitteeSize)
-            (Map.map Model.rationalToStake stakeDistr)
-     in ( Map.size persistentSeats
+  -- NOTE: this is not a good tiebreaker for real-world use, but it is
+  -- sufficient here because the actual order of pools with the same stake does
+  -- not matter when computing the persistent vs. non-persistent seat /counts/.
+  tiebreaker =
+    compare
+
+  mkStakeDistr =
+    Map.map Model.rationalToStake
+
+  model stakeDistr targetCommitteeSize = do
+    let totalSeats = fromIntegral targetCommitteeSize
+    case Model.weightedFaitAccompliPersistentSeats tiebreaker totalSeats stakeDistr of
+      Left err ->
+        error $ "Model implementation failed with error: " <> show err
+      Right (persistentSeats, numNonPersistentSeats, _) ->
+        ( fromIntegral (Map.size persistentSeats)
         , fromIntegral numNonPersistentSeats
         )
