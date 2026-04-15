b05e17e1ea5a274e772608df6bb0148dda938c99
Drazen Popovic <drazen.popovic@iohk.io>
2026-04-15 11:25:13 +0200

    Bind nix fmt to hydraJobs.native.formattingLinting.all

--- flake.nix
+++ flake.nix
@@ -95,6 +95,7 @@
         } // leiosDemo.devShells;
         inherit hydraJobs;
         legacyPackages = pkgs;
+        formatter = hydraJobs.native.formattingLinting.all;
         packages =
           hydraJobs.native.haskell96.exesNoAsserts.ouroboros-consensus-cardano // {
             leios-mvd-test = pkgs.testers.nixosTest (import ./nix/leios-mvd/test.nix { inherit inputs pkgs; });
