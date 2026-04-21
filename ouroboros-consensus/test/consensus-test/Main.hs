b746d9b77fea5b5467679519fef31b1dba4a5299
Agustin Mista <agustin.mista@moduscreate.com>
2026-04-21 17:25:41 +0200

    Add property tests for Bitmap library
    This commit adds property tests for the newly added bitmap library
    asserting that both conversion from and/to indices as well as
    serialization roundtrip correctly.
    
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>

--- ouroboros-consensus/test/consensus-test/Main.hs
+++ ouroboros-consensus/test/consensus-test/Main.hs
@@ -24,6 +24,7 @@ import qualified Test.Consensus.MiniProtocol.LocalStateQuery.Server (tests)
 import qualified Test.Consensus.Peras.Cert.Inclusion (tests)
 import qualified Test.Consensus.Peras.Voting.Rules (tests)
 import qualified Test.Consensus.Peras.WeightSnapshot (tests)
+import qualified Test.Consensus.Util.Bitmap (tests)
 import qualified Test.Consensus.Util.MonadSTM.NormalForm (tests)
 import qualified Test.Consensus.Util.Pred (tests)
 import qualified Test.Consensus.Util.Versioned (tests)
@@ -67,6 +68,7 @@ tests =
         , Test.Consensus.Peras.Voting.Rules.tests
         , Test.Consensus.Peras.WeightSnapshot.tests
         ]
+    , Test.Consensus.Util.Bitmap.tests
     , Test.Consensus.Util.MonadSTM.NormalForm.tests
     , Test.Consensus.Util.Versioned.tests
     , Test.Consensus.Util.Pred.tests
