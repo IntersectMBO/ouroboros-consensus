8ea0e91674383a3e9a0de1561a91bc9a6c3be369
Georgy Lukyanov <georgy.lukyanov@iohk.io>
2026-04-21 11:21:50 +0200

    [wip] disable Peras in ChainDB q-s-m

--- ouroboros-consensus/src/unstable-consensus-testlib/Test/Ouroboros/Storage/TestBlock.hs
+++ ouroboros-consensus/src/unstable-consensus-testlib/Test/Ouroboros/Storage/TestBlock.hs
@@ -786,7 +786,7 @@ mkTestConfig k ChunkSize{chunkCanContainEBB, numRegularBlocks} =
       , eraSlotLength = slotLength
       , eraSafeZone = HardFork.StandardSafeZone (unNonZero (maxRollbacks k) * 2)
       , eraGenesisWin = GenesisWindow (unNonZero (maxRollbacks k) * 2)
-      , eraPerasRoundLength = HardFork.PerasEnabled (perasRoundLength mkPerasParams)
+      , eraPerasRoundLength = HardFork.NoPerasEnabled
       }
 
 instance ImmutableEraParams TestBlock where
