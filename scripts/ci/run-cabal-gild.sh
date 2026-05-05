d928258c8145949d32aa7ebcc05a63cc426764bf
Javier Sagredo <javier.sagredo@iohk.io>
2026-05-05 08:21:48 +0200

    Update cabal-gild

--- scripts/ci/run-cabal-gild.sh
+++ scripts/ci/run-cabal-gild.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cabal-gild --io ouroboros-consensus.cabal
+cabal-gild ouroboros-consensus.cabal
 
 case "$(uname -s)" in
     MINGW*) git ls-files --eol | grep "w/crlf" | awk '{print $4}' | xargs dos2unix;;
