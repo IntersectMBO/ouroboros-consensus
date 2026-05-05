fa5e00b4b91113c3698548a8cf5d4b73e26c5724
Agustin Mista <agustin.mista@moduscreate.com>
2026-05-05 15:30:39 +0200

    Remove PerasCfg type family
    Co-authored-by: Agustin Mista <agustin.mista@moduscreate.com>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/PerasVoteDB/Impl.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Storage/PerasVoteDB/Impl.hs
@@ -33,7 +33,6 @@ import GHC.Generics (Generic)
 import NoThunks.Class
 import Ouroboros.Consensus.Block
 import Ouroboros.Consensus.BlockchainTime (WithArrivalTime (..))
-import Ouroboros.Consensus.Peras.Types (PerasRoundNo, PerasVoteId (..))
 import Ouroboros.Consensus.Peras.Vote.Aggregation
 import Ouroboros.Consensus.Storage.PerasVoteDB.API
 import Ouroboros.Consensus.Util.Args
@@ -125,14 +124,14 @@ data TraceEvent blk
 type PerasVoteDbArgs :: (Type -> Type) -> (Type -> Type) -> Type -> Type
 data PerasVoteDbArgs f m blk = PerasVoteDbArgs
   { pvdbaTracer :: Tracer m (TraceEvent blk)
-  , pvdbaPerasCfg :: HKD f (PerasCfg blk)
+  , pvdbaPerasParams :: HKD f PerasParams
   }
 
 defaultArgs :: Monad m => Incomplete PerasVoteDbArgs m blk
 defaultArgs =
   PerasVoteDbArgs
     { pvdbaTracer = nullTracer
-    , pvdbaPerasCfg = noDefault
+    , pvdbaPerasParams = noDefault
     }
 
 createDB ::
@@ -143,7 +142,7 @@ createDB ::
   ) =>
   Complete PerasVoteDbArgs m blk ->
   m (PerasVoteDB m blk)
-createDB args@PerasVoteDbArgs{pvdbaPerasCfg} = do
+createDB args@PerasVoteDbArgs{pvdbaPerasParams} = do
   pvdeState <-
     newTVarWithInvariantIO
       (either Just (const Nothing) . invariantForPerasVoteDbState)
@@ -155,7 +154,7 @@ createDB args@PerasVoteDbArgs{pvdbaPerasCfg} = do
           }
   pure
     PerasVoteDB
-      { addVote = implAddVote pvdbaPerasCfg env
+      { addVote = implAddVote pvdbaPerasParams env
       , getVoteIds = implGetVoteIds env
       , getVotesAfter = implGetVotesAfter env
       , getForgedCertForRound = implGetForgedCertForRound env
@@ -177,11 +176,11 @@ implAddVote ::
   , StandardHash blk
   , Typeable blk
   ) =>
-  PerasCfg blk ->
+  PerasParams ->
   PerasVoteDbEnv m blk ->
   WithArrivalTime (ValidatedPerasVote blk) ->
   STM m (m (AddPerasVoteResult blk))
-implAddVote perasCfg PerasVoteDbEnv{pvdeTracer, pvdeState} vote = do
+implAddVote params PerasVoteDbEnv{pvdeTracer, pvdeState} vote = do
   let voteId = getPerasVoteId vote
   addPerasVoteRes <- do
     WithFingerprint pvds fp <- readTVar pvdeState
@@ -206,7 +205,7 @@ implAddVote perasCfg PerasVoteDbEnv{pvdeTracer, pvdeState} vote = do
         pvsVotesByTicket' = Map.insert pvsLastTicketNo' vote (pvdsVotesByTicket pvds)
 
     (addPerasVoteRes, pvsRoundVoteStates') <-
-      case updatePerasRoundVoteStates vote perasCfg (pvdsRoundVoteStates pvds) of
+      case updatePerasRoundVoteStates vote params (pvdsRoundVoteStates pvds) of
         -- Added vote and reached a quorum, forging a new certificate
         Right (VoteGeneratedNewCert cert, pvsRoundVoteStates') ->
           pure (AddedPerasVoteAndGeneratedNewCert cert, pvsRoundVoteStates')
