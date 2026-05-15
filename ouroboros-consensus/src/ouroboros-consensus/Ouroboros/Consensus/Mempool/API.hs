58882a471668cc17319ba6cdd61e93ab029882e2
Javier Sagredo <javier.sagredo@iohk.io>
2026-05-15 10:47:21 +0200

    Remove mk from Mempool API

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/API.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/API.hs
@@ -54,6 +54,7 @@ import Ouroboros.Consensus.Ledger.Abstract
 import Ouroboros.Consensus.Ledger.SupportsMempool
 import qualified Ouroboros.Consensus.Mempool.Capacity as Cap
 import Ouroboros.Consensus.Mempool.TxSeq (TicketNo, zeroTicketNo)
+import Ouroboros.Consensus.Ticked
 import Ouroboros.Consensus.Util.IOLike
 import Ouroboros.Network.Protocol.TxSubmission2.Type (SizeInBytes)
 
@@ -182,8 +183,7 @@ data Mempool m blk = Mempool
   -- This doesn't look at the ledger state at all.
   , getSnapshotFor ::
       SlotNo ->
-      TickedLedgerState blk DiffMK ->
-      (LedgerTables blk KeysMK -> m (LedgerTables blk ValuesMK)) ->
+      StateRef m (Ticked LedgerState) blk ->
       m (MempoolSnapshot blk)
   -- ^ Get a snapshot of the mempool state that is valid with respect to
   -- the given ledger state
@@ -309,7 +309,7 @@ data MempoolTimeoutConfig = MempoolTimeoutConfig
 -- | The result of attempting to add a transaction to the mempool.
 data MempoolAddTxResult blk
   = -- | The transaction was added to the mempool.
-    MempoolTxAdded !(Validated (GenTx blk)) !(LedgerTables blk DiffMK)
+    MempoolTxAdded !(Validated (GenTx blk))
   | -- | The transaction was rejected and could not be added to the mempool
     -- for the specified reason.
     MempoolTxRejected !(GenTx blk) !(ApplyTxErr blk)
@@ -318,21 +318,17 @@ deriving instance
   ( Eq (GenTx blk)
   , Eq (Validated (GenTx blk))
   , Eq (ApplyTxErr blk)
-  , Eq (TxIn blk)
-  , Eq (TxOut blk)
   ) =>
   Eq (MempoolAddTxResult blk)
 deriving instance
   ( Show (GenTx blk)
   , Show (Validated (GenTx blk))
   , Show (ApplyTxErr blk)
-  , Show (TxIn blk)
-  , Show (TxOut blk)
   ) =>
   Show (MempoolAddTxResult blk)
 
 mempoolTxAddedToMaybe :: MempoolAddTxResult blk -> Maybe (Validated (GenTx blk))
-mempoolTxAddedToMaybe (MempoolTxAdded vtx _) = Just vtx
+mempoolTxAddedToMaybe (MempoolTxAdded vtx) = Just vtx
 mempoolTxAddedToMaybe _ = Nothing
 
 isMempoolTxAdded :: MempoolAddTxResult blk -> Bool
@@ -402,14 +398,14 @@ data ForgeLedgerState blk
     -- This will only be the case when we realized that we are the slot leader
     -- and we are actually producing a block. It is the caller's responsibility
     -- to call 'applyChainTick' and produce the ticked ledger state.
-    ForgeInKnownSlot SlotNo (TickedLedgerState blk DiffMK)
+    ForgeInKnownSlot SlotNo (TickedLedgerState blk)
   | -- | The slot number of the block is not yet known
     --
     -- When we are validating transactions before we know in which block they
     -- will end up, we have to make an assumption about which slot number to use
     -- for 'applyChainTick' to prepare the ledger state; we will assume that
     -- they will end up in the slot after the slot at the tip of the ledger.
-    ForgeInUnknownSlot (LedgerState blk EmptyMK)
+    ForgeInUnknownSlot (LedgerState blk)
 
 {-------------------------------------------------------------------------------
   Snapshot of the mempool
