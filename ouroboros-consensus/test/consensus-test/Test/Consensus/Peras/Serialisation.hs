0d4f6a94915a765fb0d1571938c3c2b1fa3e8848
Agustin Mista <agustin.mista@moduscreate.com>
2026-05-20 12:42:10 +0200

    Revert "Re-change Peras target to use `Point blk` instead of `PerasBoostedBlock`"
    This reverts commit e2f6e656fa3dc56b4f59f176fc0b3039bfd0edb5.

--- ouroboros-consensus/test/consensus-test/Test/Consensus/Peras/Serialisation.hs
+++ ouroboros-consensus/test/consensus-test/Test/Consensus/Peras/Serialisation.hs
@@ -7,10 +7,7 @@ module Test.Consensus.Peras.Serialisation
   ) where
 
 import Cardano.Binary (FromCBOR (..), ToCBOR (..), decodeFull, serialize)
-import Cardano.Ledger.BaseTypes (SlotNo (..))
 import qualified Data.ByteString.Lazy as LazyByteString
-import Ouroboros.Consensus.Block (Point)
-import Ouroboros.Consensus.Block.RealPoint (RealPoint (..), realPointToPoint)
 import Test.Consensus.Peras.Util
   ( genPerasCert
   , genPerasVote
@@ -18,10 +15,8 @@ import Test.Consensus.Peras.Util
   , tabulatePerasCert
   , tabulatePerasVote
   )
-import Test.Ouroboros.Storage.TestBlock (TestBlock, TestHeaderHash (..))
 import Test.QuickCheck
-  ( Arbitrary (..)
-  , Gen
+  ( Gen
   , Property
   , counterexample
   , forAll
@@ -32,15 +27,6 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
 import Test.Util.TestEnv (adjustQuickCheckTests)
 
--- | Generate an arbitrary 'Point' for 'TestBlock'.
--- We reuse 'TestBlock' from "Test.Ouroboros.Storage.TestBlock" which already
--- provides 'ConvertRawHash' (needed by the CBOR instances of Peras types).
-genTestPoint :: Gen (Point TestBlock)
-genTestPoint = do
-  slotNo <- SlotNo <$> arbitrary
-  hash <- TestHeaderHash <$> arbitrary
-  pure $ realPointToPoint $ RealPoint slotNo hash
-
 tests :: TestTree
 tests =
   testGroup
@@ -49,13 +35,13 @@ tests =
         testProperty "Roundtrip for PerasVote" $
           prop_roundtrip
             -- Generate both persistent and non-persistent votes
-            (genPerasVote genTestPoint True)
+            (genPerasVote True)
             tabulatePerasVote
     , adjustQuickCheckTests (* 10) $
         testProperty "Roundtrip for PerasCert" $
           prop_roundtrip
             -- Generate certs with both persistent and non-persistent votes
-            (genPerasCert genTestPoint True)
+            (genPerasCert True)
             tabulatePerasCert
     ]
 
