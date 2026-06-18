8b0681d3dae23d5e24e18f5c122e05bdabbfdf8a
Javier Sagredo <javier.sagredo@iohk.io>
2026-06-18 11:00:39 +0200

    [UTxO-HD][Mempool] mk-free MempoolAcc port
    Port the mempool to the mk-free MempoolAcc representation: API,
    Impl/Common, Query, Capacity, then Update and Init. The mempool no longer
    threads a MapKind; it carries the accumulated UTxO values explicitly.

--- ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/API.hs
+++ ouroboros-consensus/src/ouroboros-consensus/Ouroboros/Consensus/Mempool/API.hs
@@ -183,8 +183,9 @@ data Mempool m blk = Mempool
   -- This doesn't look at the ledger state at all.
   , getSnapshotFor ::
       SlotNo ->
-      TickedLedgerState blk DiffMK ->
-      (LedgerTables blk KeysMK -> m (LedgerTables blk ValuesMK)) ->
+      TickedLedgerState blk ->
+      Diff blk ->
+      (Keys blk -> m (Values blk)) ->
       m (MempoolSnapshot blk)
   -- ^ Get a snapshot of the mempool state that is valid with respect to
   -- the given ledger state
@@ -310,7 +311,7 @@ data MempoolTimeoutConfig = MempoolTimeoutConfig
 -- | The result of attempting to add a transaction to the mempool.
 data MempoolAddTxResult blk
   = -- | The transaction was added to the mempool.
-    MempoolTxAdded !(Validated (GenTx blk)) !(LedgerTables blk DiffMK)
+    MempoolTxAdded !(Validated (GenTx blk)) !(Diff blk)
   | -- | The transaction was rejected and could not be added to the mempool
     -- for the specified reason.
     MempoolTxRejected !(GenTx blk) !(ApplyTxErr blk)
@@ -319,16 +320,14 @@ deriving instance
   ( Eq (GenTx blk)
   , Eq (Validated (GenTx blk))
   , Eq (ApplyTxErr blk)
-  , Eq (TxIn blk)
-  , Eq (TxOut blk)
+  , Eq (Diff blk)
   ) =>
   Eq (MempoolAddTxResult blk)
 deriving instance
   ( Show (GenTx blk)
   , Show (Validated (GenTx blk))
   , Show (ApplyTxErr blk)
-  , Show (TxIn blk)
-  , Show (TxOut blk)
+  , Show (Diff blk)
   ) =>
   Show (MempoolAddTxResult blk)
 
@@ -403,14 +402,14 @@ data ForgeLedgerState blk
     -- This will only be the case when we realized that we are the slot leader
     -- and we are actually producing a block. It is the caller's responsibility
     -- to call 'applyChainTick' and produce the ticked ledger state.
-    ForgeInKnownSlot SlotNo (TickedLedgerState blk DiffMK)
+    ForgeInKnownSlot SlotNo (TickedLedgerState blk) (Diff blk)
   | -- | The slot number of the block is not yet known
     --
     -- When we are validating transactions before we know in which block they
     -- will end up, we have to make an assumption about which slot number to use
     -- for 'applyChainTick' to prepare the ledger state; we will assume that
     -- they will end up in the slot after the slot at the tip of the ledger.
-    ForgeInUnknownSlot (LedgerState blk EmptyMK)
+    ForgeInUnknownSlot (LedgerState blk)
 
 {-------------------------------------------------------------------------------
   Snapshot of the mempool
