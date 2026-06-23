d6c2df4cd0ad645b035722b735cbebcf8d11585a
Agustin Mista <agustin.mista@moduscreate.com>
2026-06-23 09:07:17 +0200

    Add PerasParams to BlockConfig and enable them conditionally for Dijkstra

--- ouroboros-consensus-cardano/src/unstable-cardano-testlib/Test/ThreadNet/Infra/ShelleyBasedHardFork.hs
+++ ouroboros-consensus-cardano/src/unstable-cardano-testlib/Test/ThreadNet/Infra/ShelleyBasedHardFork.hs
@@ -70,6 +70,7 @@ import Data.Void (Void)
 import Lens.Micro ((%~), (&), (.~), (^.))
 import NoThunks.Class (NoThunks)
 import Ouroboros.Consensus.Block.Forging (MkBlockForging)
+import Ouroboros.Consensus.Block.SupportsPeras (PerasParams (..), defaultPerasParams)
 import Ouroboros.Consensus.Cardano.CanHardFork
   ( crossEraForecastAcrossShelley
   , translateChainDepStateAcrossShelley
@@ -80,6 +81,10 @@ import Ouroboros.Consensus.HardFork.Combinator.Embed.Binary
 import Ouroboros.Consensus.HardFork.Combinator.Serialisation
 import Ouroboros.Consensus.HardFork.Combinator.State.Types as HFC
 import qualified Ouroboros.Consensus.HardFork.History as History
+import Ouroboros.Consensus.HardFork.History.EraParams
+  ( pattern NoPerasEnabled
+  , pattern PerasEnabled
+  )
 import Ouroboros.Consensus.Ledger.Abstract
 import Ouroboros.Consensus.Ledger.Extended
 import Ouroboros.Consensus.Ledger.SupportsMempool
@@ -487,7 +492,10 @@ protocolInfoShelleyBasedHardFork
     genesis = transCfg2 ^. L.tcShelleyGenesisL
 
     eraParams1 :: History.EraParams
-    eraParams1 = shelleyEraParams genesis
+    eraParams1 =
+      shelleyEraParams
+        genesis
+        NoPerasEnabled
 
     toPartialLedgerConfig1 ::
       LedgerConfig (ShelleyBlock proto1 era1) ->
@@ -501,7 +509,10 @@ protocolInfoShelleyBasedHardFork
     -- Era 2
 
     eraParams2 :: History.EraParams
-    eraParams2 = shelleyEraParams genesis
+    eraParams2 =
+      shelleyEraParams
+        genesis
+        (PerasEnabled (perasRoundLength defaultPerasParams))
 
     toPartialLedgerConfig2 ::
       LedgerConfig (ShelleyBlock proto2 era2) ->
