40b0321324ee58f125478361b9301122067b3f62
kolam <kolam@posteo.me>
2026-08-09 21:54:53 -0400

    Remove EncCBORGroup from ShelleyCompatible; Dijkstra no longer has EncCBORGroup instance

--- ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/Block.hs
+++ ouroboros-consensus-cardano/src/shelley/Ouroboros/Consensus/Shelley/Ledger/Block.hs
@@ -46,7 +46,6 @@ import Cardano.Ledger.Binary
   , FullByteString (..)
   , serialize
   )
-import Cardano.Ledger.Binary.Group (EncCBORGroup)
 import qualified Cardano.Ledger.Binary.Plain as Plain
 import qualified Cardano.Ledger.Block as SL (EraBlockHeader)
 import Cardano.Ledger.Core as SL
@@ -54,7 +53,7 @@ import Cardano.Ledger.Core as SL
   , eraProtVerLow
   , toEraCBOR
   )
-import qualified Cardano.Ledger.Core as SL (BlockBody, TranslationContext, hashBlockBody)
+import qualified Cardano.Ledger.Core as SL (TranslationContext, hashBlockBody)
 import Cardano.Ledger.Hashes (HASH)
 import qualified Cardano.Ledger.Shelley.API as SL
 import Cardano.Protocol.Crypto (Crypto)
@@ -109,7 +108,6 @@ type instance BlockProtocol (ShelleyBlock proto era) = proto
 class
   ( ShelleyBasedEra era
   , ShelleyProtocol proto
-  , EncCBORGroup (SL.BlockBody era)
   , EncCBOR (SL.Block (ShelleyProtocolHeader proto) era)
   , DecCBOR (Annotator (SL.Block (ShelleyProtocolHeader proto) era))
   , -- Header constraints
