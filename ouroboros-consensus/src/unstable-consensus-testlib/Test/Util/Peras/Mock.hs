1fb52ef559541f571c8eaf4c46f94689d8739efc
Agustin Mista <agustin.mista@tweag.io>
2026-08-11 12:14:21 +0200

    Refactor Peras test utilities into reusable modules
    This commit moves the shared Peras generators, mocks, predicates,
    and tabulation helpers from the consensus test suite into exposed
    unstable-consensus-testlib modules. In addition, it provides a couple
    of extra helpers that will be needed used soon.
    
    Moreover, it refactors the smoke, serialisation, and voting-adapter
    tests to use the new utilities.
    
    Co-authored-by: Agustin Mista <agustin.mista@tweag.io>
    Co-authored-by: Thomas BAGREL <thomas.bagrel@tweag.io>
    Co-authored-by: Nicolas BACQUEY <nicolas.bacquey@tweag.io>

---
+++ ouroboros-consensus/src/unstable-consensus-testlib/Test/Util/Peras/Mock.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
+
+-- | Test utilities for the mock Peras implementation.
+module Test.Util.Peras.Mock
+  ( genMockPerasVotingCommitteeInput
+  , genMockPerasVotingCommittee
+  , genMockPerasVote
+  , genMockPerasCert
+  , genMockPerasCertFullCommittee
+  , genMockPerasVoterIndices
+  , pickSeatIndexFromCommittee
+  , genVotersSubset
+  ) where
+
+import Data.Containers.NonEmpty (NE)
+import Data.Either (fromRight)
+import qualified Data.List.NonEmpty as NonEmpty
+import Data.Set (Set)
+import qualified Data.Set.NonEmpty as NESet
+import Ouroboros.Consensus.Committee.Class
+import Ouroboros.Consensus.Peras.Cert.Mock (MockPerasCert (..))
+import Ouroboros.Consensus.Peras.Crypto.Mock
+  ( MockPerasCrypto
+  , MockPerasVotingCommitteeScheme
+  , VotingCommittee (..)
+  , VotingCommitteeInput (..)
+  , unsafeIntToSeatIndex
+  )
+import Ouroboros.Consensus.Peras.Types (PerasSeatIndex (..))
+import Ouroboros.Consensus.Peras.Vote.Mock (MockPerasVote (..))
+import Test.QuickCheck (Gen, choose)
+import Test.Util.Peras.Common
+  ( NonEmptyListWithUniqueIds (..)
+  , genLedgerStake
+  , genNonEmptyListWithUniqueIds
+  , genPointTestBlock
+  , genPoolId
+  , genRoundNo
+  )
+import Test.Util.TestBlock (TestBlock)
+
+genMockPerasVotingCommitteeInput ::
+  Gen
+    ( VotingCommitteeInput
+        (MockPerasCrypto TestBlock)
+        (MockPerasVotingCommitteeScheme TestBlock)
+    )
+genMockPerasVotingCommitteeInput = do
+  NonEmptyListWithUniqueIds poolIds <- genNonEmptyListWithUniqueIds id genPoolId
+  poolIdsWithStakes <- traverse (\poolId -> (poolId,) <$> genLedgerStake) poolIds
+  pure (MockPerasVotingCommitteeInput poolIdsWithStakes)
+
+genMockPerasVotingCommittee ::
+  Gen
+    ( VotingCommittee
+        (MockPerasCrypto TestBlock)
+        (MockPerasVotingCommitteeScheme TestBlock)
+    )
+genMockPerasVotingCommittee =
+  fromRight (error "mkVotingCommittee cannot fail for the mock committee")
+    . mkVotingCommittee
+    <$> genMockPerasVotingCommitteeInput
+
+pickSeatIndexFromCommittee ::
+  VotingCommittee
+    (MockPerasCrypto TestBlock)
+    (MockPerasVotingCommitteeScheme TestBlock) ->
+  Gen PerasSeatIndex
+pickSeatIndexFromCommittee committee = do
+  let maxIndex = length (weightDistr committee) - 1
+  unsafeIntToSeatIndex <$> choose (0, maxIndex)
+
+genVotersSubset ::
+  VotingCommittee
+    (MockPerasCrypto TestBlock)
+    (MockPerasVotingCommitteeScheme TestBlock) ->
+  Gen (NE (Set PerasSeatIndex))
+genVotersSubset committee = do
+  NonEmptyListWithUniqueIds seatIndices <-
+    genNonEmptyListWithUniqueIds id $
+      pickSeatIndexFromCommittee committee
+  pure (NESet.fromList seatIndices)
+
+genMockPerasVoterIndices :: Gen (NE (Set PerasSeatIndex))
+genMockPerasVoterIndices = do
+  NonEmptyListWithUniqueIds seatIndices <-
+    genNonEmptyListWithUniqueIds id
+      . fmap unsafeIntToSeatIndex
+      $ choose (0, 100)
+  pure (NESet.fromList seatIndices)
+
+genMockPerasVote ::
+  VotingCommittee
+    (MockPerasCrypto TestBlock)
+    (MockPerasVotingCommitteeScheme TestBlock) ->
+  Gen (MockPerasVote TestBlock)
+genMockPerasVote committee = do
+  seatIndex <- pickSeatIndexFromCommittee committee
+  roundNo <- genRoundNo
+  block <- genPointTestBlock
+  pure
+    MockPerasVote
+      { mockVoteSeatIndex = seatIndex
+      , mockVoteRound = roundNo
+      , mockVoteBlock = block
+      }
+
+genMockPerasCert ::
+  VotingCommittee
+    (MockPerasCrypto TestBlock)
+    (MockPerasVotingCommitteeScheme TestBlock) ->
+  Gen (MockPerasCert TestBlock)
+genMockPerasCert committee = do
+  votersSubset <- genVotersSubset committee
+  roundNo <- genRoundNo
+  block <- genPointTestBlock
+  pure
+    MockPerasCert
+      { mockCertVoters = votersSubset
+      , mockCertRound = roundNo
+      , mockCertBlock = block
+      }
+
+genMockPerasCertFullCommittee ::
+  VotingCommittee
+    (MockPerasCrypto TestBlock)
+    (MockPerasVotingCommitteeScheme TestBlock) ->
+  Gen (MockPerasCert TestBlock)
+genMockPerasCertFullCommittee committee = do
+  let maxIndex =
+        length (weightDistr committee) - 1
+  let voters =
+        NESet.fromList
+          . NonEmpty.fromList
+          . fmap unsafeIntToSeatIndex
+          $ [0 .. maxIndex]
+  roundNo <- genRoundNo
+  block <- genPointTestBlock
+  pure
+    MockPerasCert
+      { mockCertVoters = voters
+      , mockCertRound = roundNo
+      , mockCertBlock = block
+      }
