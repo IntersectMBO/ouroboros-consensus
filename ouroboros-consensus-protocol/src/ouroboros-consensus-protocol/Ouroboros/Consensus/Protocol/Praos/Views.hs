73f96c4d1f1b5e86ac3ba264c5ce0a0ce847dd6f
Nicolas Frisby <nicolas.frisby@moduscreate.com>
2026-09-08 17:23:05 -0400

    WIP

--- ouroboros-consensus-protocol/src/ouroboros-consensus-protocol/Ouroboros/Consensus/Protocol/Praos/Views.hs
+++ ouroboros-consensus-protocol/src/ouroboros-consensus-protocol/Ouroboros/Consensus/Protocol/Praos/Views.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Ouroboros.Consensus.Protocol.Praos.Views
-  ( HeaderView (..)
+  ( BaseHeaderView (..)
+  , BaseHeaderBody
   , PraosLedgerView (..)
   , forecastToPraosLedgerView
   ) where
@@ -15,16 +19,20 @@ import Cardano.Ledger.Chain (ChainChecksPParams (..))
 import Cardano.Ledger.Keys (KeyRole (BlockIssuer), VKey)
 import qualified Cardano.Ledger.Shelley.API as SL
 import Cardano.Protocol.Crypto (KES, VRF)
+import qualified Cardano.Protocol.Leios.BlockHeader as LeiosCodec
+import qualified Cardano.Protocol.Praos.BlockHeader as PraosCodec
 import Cardano.Protocol.TPraos.BlockHeader (PrevHash)
 import Cardano.Protocol.TPraos.OCert (OCert)
 import Cardano.Slotting.Slot (SlotNo)
+import Data.Kind (Type)
 import Data.Word (Word16, Word32)
 import Lens.Micro ((^.))
-import Ouroboros.Consensus.Protocol.Praos.Header (HeaderBody)
+import Ouroboros.Consensus.Protocol.Praos.Common
 import Ouroboros.Consensus.Protocol.Praos.VRF (InputVRF)
 
+type BaseHeaderView :: PraosExtension -> Type -> Type
 -- | View of the block header required by the Praos protocol.
-data HeaderView crypto = HeaderView
+data BaseHeaderView pext crypto = HeaderView
   { hvPrevHash :: !PrevHash
   -- ^ Hash of the previous block
   , hvVK :: !(VKey BlockIssuer)
@@ -37,11 +45,18 @@ data HeaderView crypto = HeaderView
   -- ^ operational certificate
   , hvSlotNo :: !SlotNo
   -- ^ Slot
-  , hvSigned :: !(HeaderBody crypto)
+  , hvSigned :: !(BaseHeaderBody pext crypto)
   -- ^ Header which must be signed
-  , hvSignature :: !(SignedKES (KES crypto) (HeaderBody crypto))
+  , hvSignature :: !(SignedKES (KES crypto) (BaseHeaderBody pext crypto))
   -- ^ KES Signature of the header
   }
+
+type BaseHeaderBody :: PraosExtension -> Type -> Type
+type family BaseHeaderBody pext :: Type -> Type where
+  BaseHeaderBody PextNone  = PraosCodec.HeaderBody
+  BaseHeaderBody PextLeios = LeiosCodec.HeaderBody
+
+-----
 
 data PraosLedgerView = PraosLedgerView
   { plvPoolDistr :: SL.PoolDistr
